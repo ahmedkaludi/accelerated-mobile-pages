233296692aa6de30d716677168df41845b28cde0
Nicholas Griffin <nicholasgriffin@technutty.co.uk>
2016-09-18 21:07:29 +0100

    Sync with site's content
    Syncing data from site.

--- themes/default/page.php
+++ themes/default/page.php
@@ -2,20 +2,56 @@
 
 	<div id="contentwrap">
  
-
+    <?php $access_key = 1; ?>
 		<?php if ( have_posts() ): while ( have_posts() ): the_post(); ?>
+
+   
+<?php if (is_front_page()): ?>
+
+<?php
+$args = array( 'numberposts' => 12, 'post_status'=>"publish",'post_type'=>"post",'orderby'=>"post_date");
+$postslist = get_posts( $args );
+ foreach ($postslist as $post) :  setup_postdata($post); ?>
+ <div class="post">
+	 <?php if ( has_post_thumbnail() ) { ?>
+			<?php
+			$thumb_id = get_post_thumbnail_id();
+			$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail', true);
+			$thumb_url = $thumb_url_array[0];
+			?>
+			<div class="post_image"><a href="<?php the_permalink(); ?>"><amp-img src=<?php echo $thumb_url ?> width=100 height=75></amp-img></a></div>
+		<?php } ?>
+		<h2 class="title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" accesskey="<?php echo $access_key; $access_key++; ?>"><?php the_title(); ?></a></h2>
+		<?php $content = get_the_content();?>
+		<p><?php echo wp_trim_words( $content , '15' ); ?></p>
+</li>
+</div>
+<?php endforeach; ?>
+
+<div id="pagination">
+	<div class="next"><?php next_posts_link( 'Next &raquo;', 0 ) ?></div>
+	<div class="prev"><?php previous_posts_link( '&laquo; Previous' ); ?></div>
+	<div class="clearfix"></div>
+</div>
+
+ <?php else: ?>
+
+
+		<div class="post">
 
 		<div id="title">
 			<h2><?php the_title(); ?></h2>
 		</div>
 
-		<div class="post">
 			<?php if ( has_post_thumbnail() ) : $thumbnail_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' ); ?>
 			<a href="<?php the_permalink(); ?>" class="thumbnail"> </a>
 			<?php endif; ?>
 
 			<?php the_content(); ?>
 			<?php wp_link_pages( 'before=<p>&after=</p>&next_or_number=number&pagelink=Page %' ); ?>
+
+<?php endif; ?>
+
 		</div>
 
 		<?php endwhile; ?>
@@ -63,7 +99,8 @@ $thumb_url = $thumb_url_array[0];
   "description": "A most wonderful article"
 }
 
- 
+
 
 </script>
+
 <?php get_footer(); ?>
