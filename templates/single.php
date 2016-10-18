4fce46b2c53d42a42e2fc0527d03d4367cdeb044
Ahmed Kaludi <ahmed@magazine3.com>
2016-10-18 22:45:57 +0530

    Removed carousel.js file because it is added already
    Removed carousel.js file because it is added already

--- templates/single.php
+++ templates/single.php
@@ -5,7 +5,6 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no">
 	<?php do_action( 'amp_post_template_head', $this ); ?>
-	<script async custom-element="amp-carousel" src="https://cdn.ampproject.org/v0/amp-carousel-0.1.js"></script>
 	<style amp-custom>
 	<?php $this->load_parts( array( 'style' ) ); ?>
 	<?php do_action( 'amp_post_template_css', $this ); ?>
