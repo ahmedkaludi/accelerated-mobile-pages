86433f67118f2b863e395af724665aa223bd7055
Mohammed Kaludi <mohammed@magazine3.com>
2016-11-07 15:06:56 +0530

    Footer texrt connected with new layout
    Footer texrt connected with new layout

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,8 @@
+<?php global $redux_builder_amp; ?>
 <footer class="amp-wp-footer">
 	<div>
 		<h2><?php echo esc_html( $this->get( 'blog_name' ) ); ?></h2>
-		<p>
+		<p> <?php echo $redux_builder_amp['amp-footer-text']; ?>
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'amp' ) ); ?>"><?php printf( __( 'Powered by %s', 'amp' ), 'WordPress' ); ?></a>
 		</p>
 		<a href="#top" class="back-to-top"><?php _e( 'Back to top', 'amp' ); ?></a>
