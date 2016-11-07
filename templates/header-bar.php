e6ea872cabb925d8e2edb0f956fc8897d38185a7
Mohammed Kaludi <mohammed@magazine3.com>
2016-11-07 15:06:36 +0530

    Logo area added
    Logo area added with new design

--- templates/header-bar.php
+++ templates/header-bar.php
@@ -1,12 +1,13 @@
 <?php global $redux_builder_amp; ?>
 <header id="#top" class="amp-wp-header">
   <div class="ampforwp-logo-area" >
+
     <a href="<?php echo esc_url( trailingslashit( $this->get( 'home_url' ) ) ) . '?' .AMP_QUERY_VAR; ?>">
-      <?php $site_icon_url = $this->get( 'site_icon_url' );
-        if ( $site_icon_url ) : ?>
-        <amp-img src="<?php echo esc_url( $site_icon_url ); ?>" width="32" height="32" class="amp-wp-site-icon"></amp-img>
-      <?php endif; ?>
-      <?php echo esc_html( $this->get( 'blog_name' ) ); ?>
+      <?php if (true == ($redux_builder_amp['opt-media']['url'])) {  ?>
+        <amp-img src="<?php echo $redux_builder_amp['opt-media']['url']; ?>" width="190" height="36" alt="logo" class="amp-logo"></amp-img>
+      <?php } else {
+        echo esc_html( $this->get( 'blog_name' ) ); 
+      } ?>
     </a>
   
   <div on='tap:sidebar.toggle' role="button" tabindex="0" class="nav_container">
