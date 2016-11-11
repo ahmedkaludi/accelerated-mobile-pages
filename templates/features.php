41c58e0038eca6dc7e9f37baf55dbf4a347bbb11
Mohammed Kaludi <mohammed@magazine3.com>
2016-11-11 17:51:34 +0530

    Double link was added Non-amp button
    Double link was added Non-amp button

--- templates/features.php
+++ templates/features.php
@@ -176,7 +176,7 @@
 							//24. Added an options button for switching on/off link to non amp page
 							if($redux_builder_amp['amp-footer-link-non-amp-page']=='1'){ if ( $ampforwp_backto_nonamp ) { ?>
 								 |
-	            	<a href="?<?php echo $ampforwp_backto_nonamp; ?>"><?php _e('View Non-AMP Version','ampforwp');?></a> <?php  } }?>
+	            	<a href="<?php echo $ampforwp_backto_nonamp; ?>"><?php _e('View Non-AMP Version','ampforwp');?></a> <?php  } }?>
 	            </p>
 	            <p><?php echo $redux_builder_amp['amp-footer-text']; ?> </p>
 	        </div>
