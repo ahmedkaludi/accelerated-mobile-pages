b10ab58c2225946834b215a0e89cee611bc5964a
Mohammed Kaludi <mohammed@magazine3.com>
2016-11-16 15:23:55 +0530

    Realigned the code
    Realigned the code

--- templates/meta-taxonomy.php
+++ templates/meta-taxonomy.php
@@ -1,27 +1,20 @@
 <?php
-$ampforwp_categories = get_the_terms( $this->ID, 'category' );
-
- ?>
-
-<?php if ( $ampforwp_categories ) : ?>
-	<div class="amp-wp-meta amp-wp-tax-category ampforwp-tax-category">
-		<?php global $redux_builder_amp; printf( __($redux_builder_amp['amp-translator-categories-text'] .': ', 'amp' ));
-    foreach ($ampforwp_categories as $cat ) {
-      # code...
-    echo ('<a href="'.get_site_url().'/category/' . $cat->slug .'/?amp" >'.$cat->name .'</a>');
-    }
-?>
-	</div>
-<?php endif; ?>
-
-<?php
-$ampforwp_tags=  get_the_terms( $this->ID, 'post_tag' );
- if ( $ampforwp_tags && ! is_wp_error( $ampforwp_tags ) ) : ?>
-	<div class="amp-wp-meta amp-wp-tax-tag ampforwp-tax-tag">
-		<?php  global $redux_builder_amp; printf( __($redux_builder_amp['amp-translator-tags-text'] .': ', 'amp' ));
-     foreach ($ampforwp_tags as $tag) {
-      # code...
-    echo ('<a href="'.get_site_url().'/tag/' . $tag->slug .'/?amp" >'.$tag->name .'</a>');
-    }  ?>
-	</div>
-<?php endif; ?>
+  $ampforwp_categories = get_the_terms( $this->ID, 'category' );
+  if ( $ampforwp_categories ) : ?>
+  	<div class="amp-wp-meta amp-wp-tax-category ampforwp-tax-category">
+  		<?php global $redux_builder_amp; printf( __($redux_builder_amp['amp-translator-categories-text'] .': ', 'amp' ));
+      foreach ($ampforwp_categories as $cat ) {
+          echo ('<a href="'.get_site_url().'/category/' . $cat->slug .'/?amp" >'.$cat->name .'</a>');
+      } ?>
+  	</div>
+  <?php endif;
+  
+  $ampforwp_tags=  get_the_terms( $this->ID, 'post_tag' );
+  if ( $ampforwp_tags && ! is_wp_error( $ampforwp_tags ) ) : ?>
+    <div class="amp-wp-meta amp-wp-tax-tag ampforwp-tax-tag">
+    	<?php  global $redux_builder_amp; printf( __($redux_builder_amp['amp-translator-tags-text'] .': ', 'amp' ));
+        foreach ($ampforwp_tags as $tag) {
+            echo ('<a href="'.get_site_url().'/tag/' . $tag->slug .'/?amp" >'.$tag->name .'</a>');
+        } ?>
+    </div>
+  <?php endif; ?>
