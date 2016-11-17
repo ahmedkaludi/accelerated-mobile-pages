38cee9da0ad306e0c5913668f0bd6057680ea065
Khaled Mohammed <khaled.mak94@gmail.com>
2016-11-17 15:22:11 +0530

    Dynamic links for tags and categories Meta links on a post

--- templates/meta-taxonomy.php
+++ templates/meta-taxonomy.php
@@ -4,7 +4,7 @@
   	<div class="amp-wp-meta amp-wp-tax-category ampforwp-tax-category">
   		<span><?php global $redux_builder_amp; printf( __($redux_builder_amp['amp-translator-categories-text'] .': ', 'amp' )); ?></span>
       <?php foreach ($ampforwp_categories as $cat ) {
-          echo ('<a href="'.get_site_url().'/category/' . $cat->slug .'/?amp" >'.$cat->name .'</a>');
+        echo ('<a href="'.get_category_link($cat->term_taxonomy_id).'?amp" >'.$cat->name .'</a>');
       } ?>
   	</div>
   <?php endif; ?>
--- templates/single.php
+++ templates/single.php
@@ -57,7 +57,7 @@
 				<div class="amp-wp-meta amp-wp-tax-tag ampforwp-tax-tag">
 					<?php  global $redux_builder_amp; printf( __($redux_builder_amp['amp-translator-tags-text'] .': ', 'amp' ));
 						foreach ($ampforwp_tags as $tag) {
-								echo ('<a href="'.get_site_url().'/tag/' . $tag->slug .'/?amp" >'.$tag->name .'</a>');
+								 echo ('<a href="'.get_tag_link($tag->term_taxonomy_id).'?amp" >'.$tag->name .'</a>');
 						} ?>
 				</div>
 			<?php endif;
