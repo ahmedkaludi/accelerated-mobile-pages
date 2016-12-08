6aafeb561175d3a1bb46eb6f33fb3bad420ef6fb
Khaled Mohammed <khaled.mak94@gmail.com>
2016-12-08 18:25:40 +0530

    issue fixed #259

--- templates/design-manager/design-1/elements/meta-info.php
+++ templates/design-manager/design-1/elements/meta-info.php
@@ -1,11 +1,17 @@
 <div class="amp-wp-article-header ampforwp-meta-info">
 
 	<?php $post_author = $this->get( 'post_author' ); ?>
-	<?php if ( $post_author ) : ?>
-		<?php $author_avatar_url = get_avatar_url( $post_author->user_email, array( 'size' => 24 ) ); ?>
+	<?php
+	if ( $post_author ) : ?>
+		<?php
+		$string = get_avatar($post_author->user_email);
+		$token = strtok($string, " ");
+		$token = strtok(" ");
+		$token = strtok(" ");
+	?>
 		<div class="amp-wp-meta amp-wp-byline">
 			<?php if ( function_exists( 'get_avatar_url' ) ) : ?>
-				<amp-img src="<?php echo esc_url( $author_avatar_url ); ?>" width="24" height="24" layout="fixed"></amp-img>
+				<amp-img <?php echo $token; ?>" width="24" height="24" layout="fixed"></amp-img>
 			<?php endif; ?>
 			<span class="amp-wp-author author vcard"><?php echo esc_html( $post_author->display_name ); ?></span>
 		</div>
