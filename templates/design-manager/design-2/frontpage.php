331f303440985616135e30f02f109b1faf744047
Mohammed Kaludi <mohammed@magazine3.com>
2016-12-30 13:24:40 +0530

    FrontPage properly added
    FrontPage properly added code was removed by @khaledMohammed000 , I
    don't know why ?

--- templates/design-manager/design-2/frontpage.php
+++ templates/design-manager/design-2/frontpage.php
@@ -1,11 +1,17 @@
-<?php global $redux_builder_amp;  ?>
+<?php global $redux_builder_amp;  
+$post_id = $redux_builder_amp['amp-frontpage-select-option-pages'];
+$template = new AMP_Post_Template( $post_id );?>
 <!doctype html>
 <html amp>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no">
 	<?php do_action( 'amp_post_template_head', $this ); ?>
-
+	<?php 	
+		$amp_component_scripts = $template->data['amp_component_scripts'];
+		foreach ($amp_component_scripts as $ampforwp_service => $ampforwp_js_file) { ?>
+			<script custom-element="<?php echo $ampforwp_service; ?>"  src="<?php echo $ampforwp_js_file; ?>" async></script> <?php	
+		} ?>		
 	<style amp-custom>
 	<?php $this->load_parts( array( 'style' ) ); ?>
 	<?php do_action( 'amp_post_template_css', $this ); ?>
@@ -17,27 +23,19 @@
 <?php do_action( 'ampforwp_after_header', $this ); ?>
 
 <main>
-	<div class="amp-wp-content the_content">
-		<?php do_action( 'ampforwp_before_post_content', $this ); ?>
+	<div class="amp-wp-content the_content"> <?php 
 
-<?php
-global $redux_demo;
-//echo 'Multi Select value: '  . $redux_builder_amp['amp-frontpage-select-option-pages'];
-?>
+		do_action( 'ampforwp_before_post_content', $this );
+		// Front Page Content
+		$ampforwp_post_content = $template->data['post_amp_content'];
+		echo $ampforwp_post_content;
 
-<?php $the_query = new WP_Query( 'page_id=' . $redux_builder_amp['amp-frontpage-select-option-pages'] . '' ); ?>
-<?php while ($the_query -> have_posts()) : $the_query -> the_post();  ?>
-<?php the_content(); ?>
-<?php endwhile;?>
+		do_action( 'ampforwp_after_post_content', $this ); ?>
 
-<?php do_action( 'ampforwp_after_post_content', $this ); ?>
 	</div>
 
 	<div class="amp-wp-content post-pagination-meta">
 		<?php $this->load_parts( apply_filters( 'amp_post_template_meta_parts', array( 'meta-taxonomy' ) ) ); ?>
-
-
-
 	</div>
 
 	<?php if($redux_builder_amp['enable-single-social-icons'] == true)  { ?>
