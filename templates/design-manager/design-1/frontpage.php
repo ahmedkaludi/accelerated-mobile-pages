7e4d7b696aed98feb841ab6e12aec699fc3e4f8d
Khaled Mohammed <khaled.mak94@gmail.com>
2017-01-03 16:28:34 +0300

    issue #358

--- templates/design-manager/design-1/frontpage.php
+++ templates/design-manager/design-1/frontpage.php
@@ -18,9 +18,6 @@ $template = new AMP_Post_Template( $post_id );?>
 	<style amp-custom>
 	<?php $this->load_parts( array( 'style' ) ); ?>
 	<?php do_action( 'amp_post_template_css', $this ); ?>
-	#title{
-		text-align: center;
-	}
 	</style>
 </head>
 <body class="single-post amp-wp-frontpage">
