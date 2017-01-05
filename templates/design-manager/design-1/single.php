972de3a73ece96bed76d3c4c21176c1c69ae91dc
Khaled Mohammed <khaled.mak94@gmail.com>
2017-01-05 18:13:49 +0300

    error for doublecomments

--- templates/design-manager/design-1/single.php
+++ templates/design-manager/design-1/single.php
@@ -25,8 +25,8 @@
 	$data = get_option( 'ampforwp_design' );
 	if( isset( $data['elements'] ) || ! empty( $data['elements'] ) ){
 		$options = explode( ',', $data['elements'] );
-
 	}
+	if( $options ) {
 	$values = array_values($options );
 	global $post;
 	$comments_count = wp_count_comments($post->ID);
@@ -34,7 +34,8 @@
 			<div class="comment-button-wrapper">
 			    <a href="<?php echo get_permalink().'#commentform' ?>"><?php esc_html_e( $redux_builder_amp['amp-translator-leave-a-comment-text']  ); ?></a>
 			</div>
-	<?php } ?>
+	<?php }
+	}?>
 	<?php do_action('ampforwp_post_after_design_elements') ?>
 </article>
 
--- templates/design-manager/design-2/single.php
+++ templates/design-manager/design-2/single.php
@@ -26,8 +26,8 @@ global $redux_builder_amp;
 $data = get_option( 'ampforwp_design' );
 if( isset( $data['elements'] ) || ! empty( $data['elements'] ) ){
 	$options = explode( ',', $data['elements'] );
-
 }
+if( $options ) {
 $values = array_values($options );
 global $post;
 $comments_count = wp_count_comments($post->ID);
@@ -36,7 +36,8 @@ if( in_array('comments:0',$values) || $comments_count->approved == 0  ) {
 		<div class="comment-button-wrapper">
 		    <a href="<?php echo get_permalink().'#commentform' ?>"><?php esc_html_e( $redux_builder_amp['amp-translator-leave-a-comment-text']  ); ?></a>
 		</div>
-<?php } ?>
+<?php }
+} ?>
 		<?php do_action('ampforwp_post_after_design_elements') ?>
 	</article>
 </main>
