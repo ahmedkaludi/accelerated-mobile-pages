2b731bc9371819ebef39700d2df3185b816e7c69
Khaled Mohammed <khaled.mak94@gmail.com>
2017-01-05 18:34:53 +0300

    button issue solved

--- templates/design-manager/design-1/single.php
+++ templates/design-manager/design-1/single.php
@@ -26,16 +26,20 @@
 	if( isset( $data['elements'] ) || ! empty( $data['elements'] ) ){
 		$options = explode( ',', $data['elements'] );
 	}
+	$count = 0;
 	if( $options ) {
-	$values = array_values($options );
-	global $post;
-	$comments_count = wp_count_comments($post->ID);
-	if( in_array('comments:0',$values) || $comments_count->approved == 0  ) { ?>
-			<div class="comment-button-wrapper">
-			    <a href="<?php echo get_permalink().'#commentform' ?>"><?php esc_html_e( $redux_builder_amp['amp-translator-leave-a-comment-text']  ); ?></a>
-			</div>
-	<?php }
-	}?>
+		$values = array_values($options );
+		global $post;
+		$comments_count = wp_count_comments($post->ID);
+		if( in_array('comments:0',$values) ) {
+			button_code();
+			$count++;
+			}
+	}
+	if($count === 0 && $comments_count->approved == 0) {
+		button_code();
+	}
+	?>
 	<?php do_action('ampforwp_post_after_design_elements') ?>
 </article>
 
@@ -45,3 +49,10 @@
 
 </body>
 </html>
+
+<?php
+function button_code() { global $redux_builder_amp ;?>
+	<div class="comment-button-wrapper">
+			<a href="<?php echo get_permalink().'#commentform' ?>"><?php esc_html_e( $redux_builder_amp['amp-translator-leave-a-comment-text']  ); ?></a>
+	</div>
+<?php }
--- templates/design-manager/design-2/single.php
+++ templates/design-manager/design-2/single.php
@@ -26,18 +26,20 @@ global $redux_builder_amp;
 $data = get_option( 'ampforwp_design' );
 if( isset( $data['elements'] ) || ! empty( $data['elements'] ) ){
 	$options = explode( ',', $data['elements'] );
-}
+}$count = 0;
 if( $options ) {
-$values = array_values($options );
-global $post;
-$comments_count = wp_count_comments($post->ID);
-if( in_array('comments:0',$values) || $comments_count->approved == 0  ) {
- ?>
-		<div class="comment-button-wrapper">
-		    <a href="<?php echo get_permalink().'#commentform' ?>"><?php esc_html_e( $redux_builder_amp['amp-translator-leave-a-comment-text']  ); ?></a>
-		</div>
-<?php }
-} ?>
+	$values = array_values($options );
+	global $post;
+	$comments_count = wp_count_comments($post->ID);
+	if( in_array('comments:0',$values) ) {
+		button_code();
+		$count++;
+		}
+}
+if($count === 0 && $comments_count->approved == 0) {
+	button_code();
+}
+?>
 		<?php do_action('ampforwp_post_after_design_elements') ?>
 	</article>
 </main>
@@ -45,3 +47,10 @@ if( in_array('comments:0',$values) || $comments_count->approved == 0  ) {
 <?php do_action( 'amp_post_template_footer', $this ); ?>
 </body>
 </html>
+
+<?php
+function button_code() { global $redux_builder_amp ;?>
+	<div class="comment-button-wrapper">
+			<a href="<?php echo get_permalink().'#commentform' ?>"><?php esc_html_e( $redux_builder_amp['amp-translator-leave-a-comment-text']  ); ?></a>
+	</div>
+<?php }
