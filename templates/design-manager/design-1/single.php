41e0f4d21ddf0ec270020f5d964a353af5abeb4a
Khaled Mohammed <khaled.mak94@gmail.com>
2017-01-19 16:31:42 +0530

    customizer button issue solved
    when comments uncheked we add a button that does not live render with
    its new position

--- templates/design-manager/design-1/single.php
+++ templates/design-manager/design-1/single.php
@@ -19,27 +19,7 @@
 	<?php do_action('ampforwp_post_before_design_elements') ?>
 
 	<?php $this->load_parts( apply_filters( 'ampforwp_design_elements', array( 'empty-filter' ) ) ); ?>
-	<?php
-	global $redux_builder_amp;
 
-	$data = get_option( 'ampforwp_design' );
-	if( isset( $data['elements'] ) || ! empty( $data['elements'] ) ){
-		$options = explode( ',', $data['elements'] );
-	}
-	$count = 0;
-	if( $options ) {
-		$values = array_values($options );
-		global $post;
-		$comments_count = wp_count_comments($post->ID);
-		if( in_array('comments:0',$values) ) {
-			button_code();
-			$count++;
-			}
-	}
-	if($count === 0 && $comments_count->approved == 0) {
-		button_code();
-	}
-	?>
 	<?php do_action('ampforwp_post_after_design_elements') ?>
 </article>
 
@@ -50,10 +30,3 @@
 </body>
 </html>
 
-<?php
-function button_code() { global $redux_builder_amp ;?>
-	<?php if( !comments_open() ) { return; } ?>
-	<div class="comment-button-wrapper">
-			<a href="<?php echo get_permalink().'#commentform' ?>"><?php esc_html_e( $redux_builder_amp['amp-translator-leave-a-comment-text']  ); ?></a>
-	</div>
-<?php }
