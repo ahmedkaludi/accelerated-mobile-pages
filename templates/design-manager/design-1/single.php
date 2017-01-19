89c2004869187b514f82ab6e88c669dc1151f907
Khaled Mohammed <khaled.mak94@gmail.com>
2017-01-19 12:07:53 +0530

    comments button

--- templates/design-manager/design-1/single.php
+++ templates/design-manager/design-1/single.php
@@ -52,6 +52,7 @@
 
 <?php
 function button_code() { global $redux_builder_amp ;?>
+	<?php if( !comments_open() ) { return; } ?>
 	<div class="comment-button-wrapper">
 			<a href="<?php echo get_permalink().'#commentform' ?>"><?php esc_html_e( $redux_builder_amp['amp-translator-leave-a-comment-text']  ); ?></a>
 	</div>
