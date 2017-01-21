092e5e0565d498e81d535d907694d93b3c7a9783
Mohammed Kaludi <mohammed@magazine3.com>
2017-01-21 15:42:13 +0530

    Remove Extra styling added by other Themes/ Plugins
    Remove Extra styling added by other Themes/ Plugins

--- templates/features.php
+++ templates/features.php
@@ -971,9 +971,12 @@ function ampforwp_remove_title_tags(){
 		function ampforwp_the_content_filter_full( $content_buffer ) {
             $ampforwp_is_amp_endpoint = ampforwp_is_amp_endpoint();
 			if ( $ampforwp_is_amp_endpoint ) {
-				 $content_buffer = preg_replace("/' defer='defer/", "", $content_buffer);
-				 $content_buffer = preg_replace("/onclick=[^>]*/", "", $content_buffer);
-                 $content_buffer = preg_replace("/<\\/?thrive_headline(.|\\s)*?>/",'',$content_buffer);
+				$content_buffer = preg_replace("/' defer='defer/", "", $content_buffer);
+				$content_buffer = preg_replace("/onclick=[^>]*/", "", $content_buffer);
+                $content_buffer = preg_replace("/<\\/?thrive_headline(.|\\s)*?>/",'',$content_buffer);
+                // Remove Extra styling added by other Themes/ Plugins
+               	$content_buffer = preg_replace('/(<style(.*?)>(.*?)<\/style>)<!doctype html>/','<!doctype html>',$content_buffer);
+               	$content_buffer = preg_replace('/(<style(.*?)>(.*?)<\/style>)(\/\*)/','$4',$content_buffer);
             }
             return $content_buffer;
 		}
