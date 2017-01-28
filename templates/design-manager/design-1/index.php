273a7b063a9d72a55f40ace9f474a6d98b0816c5
Mohammed Kaludi <mohammed@magazine3.com>
2017-01-28 15:47:55 +0530

    code refined
    code refined

--- templates/design-manager/design-1/index.php
+++ templates/design-manager/design-1/index.php
@@ -4,13 +4,12 @@
 <head>
 	<meta charset="utf-8">
     <link rel="dns-prefetch" href="https://cdn.ampproject.org">
-<?php
-global $redux_builder_amp;
-if ( is_home() || is_front_page() || ( is_archive() && $redux_builder_amp['ampforwp-archive-support'] ) ){
-	global $wp;
-	$current_archive_url = home_url( $wp->request );
-	$amp_url = trailingslashit($current_archive_url);
-} ?>
+	<?php global $redux_builder_amp;
+		if ( is_home() || is_front_page() || ( is_archive() && $redux_builder_amp['ampforwp-archive-support'] ) ){
+			global $wp;
+			$current_archive_url = home_url( $wp->request );
+			$amp_url = trailingslashit($current_archive_url);
+		} ?>
 	<link rel="canonical" href="<?php echo $amp_url ?>">
 	<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no">
 	<?php do_action( 'amp_post_template_head', $this ); ?>
