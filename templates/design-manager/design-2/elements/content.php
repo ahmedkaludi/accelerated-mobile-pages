14a4c4ca8702a4ee8f09da4db180df2e2f614bd2
Khaled Mohammed <khaled.mak94@gmail.com>
2017-01-30 21:23:00 +0530

    filters added for next and previous text

--- templates/design-manager/design-2/elements/content.php
+++ templates/design-manager/design-2/elements/content.php
@@ -15,7 +15,7 @@
 				// Custom/Alternative AMP content added through post meta
 				echo $this->get( 'ampforwp_amp_content' );
 			}
-			
+
 			// echo $this->get( 'post_amp_content' ); // amphtml content; no kses
 			?>
 
@@ -34,8 +34,10 @@
 					<!--Next Link code-->
 					<div class="next">
 						<?php $next_post = get_next_post();
-							if (!empty( $next_post )) { ?>
-									<a href="<?php echo trailingslashit(get_permalink( $next_post->ID )) . AMP_QUERY_VAR; ?>"><?php echo $next_post->post_title; ?> &raquo;</a> <?php
+							if (!empty( $next_post )) {
+								$next_text = $next_post->post_title;
+								?>
+									<a href="<?php echo trailingslashit(get_permalink( $next_post->ID )) . AMP_QUERY_VAR; ?>"><?php echo apply_filters('ampforwp_next_link',$next_text ); ?> &raquo;</a> <?php
 								} ?>
 					</div>
 					<!--Next Link code-->
@@ -43,8 +45,10 @@
 					<!--Prev Link code-->
 					<div class="prev">
 							<?php $prev_post = get_previous_post();
-								 if (!empty( $prev_post )) { ?>
-								   <a href="<?php echo trailingslashit(get_permalink( $prev_post->ID )). AMP_QUERY_VAR; ?>"> &laquo; <?php echo $prev_post->post_title ?></a> <?php
+								 if (!empty( $prev_post )) {
+									 $prev_text = $prev_post->post_title;
+									  ?>
+								   <a href="<?php echo trailingslashit(get_permalink( $prev_post->ID )). AMP_QUERY_VAR; ?>"> &laquo; <?php echo apply_filters('ampforwp_prev_link',$prev_text ); ?></a> <?php
 								 } ?>
 					</div>
 					<!--Prev Link code-->
