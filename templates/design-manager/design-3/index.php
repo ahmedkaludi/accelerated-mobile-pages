d4b61c47f9a4498c7da1a99c4ccae6b59941baf7
Mohammed Kaludi <mohammed@magazine3.com>
2017-02-08 21:11:26 +0530

    Proper Rel Canonical Added #494
    Proper Rel Canonical Added #494

--- templates/design-manager/design-3/index.php
+++ templates/design-manager/design-3/index.php
@@ -6,10 +6,12 @@
   <link rel="dns-prefetch" href="https://cdn.ampproject.org">
 	<?php
 	global $redux_builder_amp;
-	if ( is_home() || is_front_page()  ){
+	if ( is_home() || is_front_page()  || ( is_archive() && $redux_builder_amp['ampforwp-archive-support'] ) ){
 		global $wp;
 		$current_archive_url = home_url( $wp->request );
-		$amp_url = trailingslashit($current_archive_url);
+		$amp_url 	= trailingslashit($current_archive_url);
+		$remove 	= '/'. AMP_QUERY_VAR;
+		$amp_url 	= str_replace($remove, '', $amp_url) ;
 	} ?>
 	<link rel="canonical" href="<?php echo $amp_url ?>">
 	<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no">
