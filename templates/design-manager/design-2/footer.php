631ff4e3cfa25c1cf9147ec640ec0c16abc39cb8
Nicholas Griffin <nicholasgriffin@technutty.co.uk>
2017-02-08 22:48:14 +0000

    Okay I messed a few things up :P

--- templates/design-manager/design-2/footer.php
+++ templates/design-manager/design-2/footer.php
@@ -21,8 +21,8 @@
 
 <footer class="container">
         <div id="footer">
-        <a class="non-amp-link full-footer-link" href="<?php echo $ampforwp_backto_nonamp; ?>"></a>
-        <a class="to-top-link full-footer-link" href="#header"></a>
+        <a class="non-amp-link full-footer-link" href="<?php echo $ampforwp_backto_nonamp; ?>">View the original article</a>
+        <a class="to-top-link full-footer-link" href="#header">Go back to the top</a>
           <a href="https://technutty.co.uk">
 <amp-img src="https://technutty.co.uk/TechNuttyAmpLogo.png" width="600" height="60" alt="logo" class="amp-logo" layout=responsive id="AMP_1">
 
@@ -45,15 +45,7 @@
 
             <div class="meta-footer">
             <a href="https://technutty.co.uk/about/'">About</a> / <a href="https://technutty.co.uk/contact/'">Contact</a> / <a href="https://technutty.co.uk/terms-and-conditions/'">Terms and Conditions</a> / <a href="https://technutty.co.uk/privacy-policy/'">Privacy Policy</a> 
-                                             <ul>
-<button onclick="window.location.href='<?php echo $ampforwp_backto_nonamp; ?>'" class="bttn-footer-link bttn-tn bttn-non-amp">View the Non-AMP version</button>                                      
-<button onclick="window.location.href='#header'" class="bttn-footer-link bttn-tn bttn-primary">Top</button>
-<button onclick="window.location.href='https://technutty.co.uk/about/'" class="bttn-footer-link bttn-tn bttn-primary">About</button>
-<button onclick="window.location.href='https://technutty.co.uk/contact/'" class="bttn-footer-link bttn-tn bttn-primary">Contact</button>
-<button onclick="window.location.href='https://technutty.co.uk/newsletter/'" class="bttn-footer-link bttn-tn bttn-primary">Newsletter</button>
-<button onclick="window.location.href='https://technutty.co.uk/terms-and-conditions/'" class="bttn-footer-link bttn-tn bttn-primary">T&Cs</button>
-<button onclick="window.location.href='https://technutty.co.uk/privacy-policy/'" class="bttn-footer-link bttn-tn bttn-primary">Privacy Policy</button>
-                                               </ul>
+                                             
 
             																                       </div>
         </div>
