b53e87fd887830293502757b543386a370069228
Nicholas Griffin <nicholasgriffin@technutty.co.uk>
2017-02-09 02:27:34 +0000

    share commit, again..

--- templates/design-manager/design-2/elements/social-icons.php
+++ templates/design-manager/design-2/elements/social-icons.php
@@ -1,6 +1,8 @@
 <?php global $redux_builder_amp;  ?>
-<div class="amp-share">'
- <a href="https://twitter.com/share?text=' . urlencode( $title ) . '&amp;url=' . $encoded_url . '" target="_blank" class="as-twitter" title="Tweet">Tweet</a>
- <a target="_blank" href="http://www.facebook.com/sharer.php?u=' . $encoded_url . '" class="as-facebook" title="Share on Facebook">Share</a>
+<div class="amp-wp-content post-pagination-meta ampforwp-social-icons-wrapper ampforwp-social-icons">
+<div class="amp-share">
+ <a href="http://twitter.com/home/?status=<?php the_title(); ?> - <?php the_permalink(); ?> via @TheTechNuttyUK" target="_blank" class="as-twitter" title="Tweet">Tweet</a>
+ <a target="_blank" href="http://www.facebook.com/sharer.php?u=<?php the_permalink();?>&amp;t=<?php the_title(); ?>" class="as-facebook" title="Share on Facebook">Share</a>
+</div>
 </div>
 
