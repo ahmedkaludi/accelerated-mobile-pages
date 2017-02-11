60b832ba74ffbbd0849f6ca80d5e1f09d02fbd52
Nicholas Griffin <nicholasgriffin@technutty.co.uk>
2017-02-11 00:35:16 +0000

    Changed the single social buttons to use Font Awesome, probably should have done that before....

--- templates/design-manager/design-2/elements/social-icons.php
+++ templates/design-manager/design-2/elements/social-icons.php
@@ -1,8 +1,8 @@
 <?php global $redux_builder_amp;  ?>
 <div class="amp-wp-content post-pagination-meta ampforwp-social-icons-wrapper ampforwp-social-icons">
 <div class="amp-share">
- <a href="http://twitter.com/home/?status=<?php the_title(); ?> - <?php the_permalink(); ?> via @TheTechNuttyUK" target="_blank" class="as-twitter" title="Tweet">Tweet</a>
- <a target="_blank" href="http://www.facebook.com/sharer.php?u=<?php the_permalink();?>&amp;t=<?php the_title(); ?>" class="as-facebook" title="Share on Facebook">Share</a>
+ <a href="http://twitter.com/home/?status=<?php the_title(); ?> - <?php the_permalink(); ?> via @TheTechNuttyUK" target="_blank" class="as-twitter" title="Tweet"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+ <a target="_blank" href="http://www.facebook.com/sharer.php?u=<?php the_permalink();?>&amp;t=<?php the_title(); ?>" class="as-facebook" title="Share on Facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
 </div>
 </div>
 
