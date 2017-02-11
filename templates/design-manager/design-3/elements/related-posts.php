99cb0ba22ab1641813a0e4bcfd2e9e945013624e
Khaled Mohammed <khaled.mak94@gmail.com>
2017-02-11 13:23:08 +0530

    related posts exclude private/protected posts

--- templates/design-manager/design-3/elements/related-posts.php
+++ templates/design-manager/design-3/elements/related-posts.php
@@ -14,7 +14,9 @@
 							    'category__in' => $category_ids,
 							    'post__not_in' => array($post->ID),
 							    'posts_per_page'=> $int_number_of_related_posts,
-							    'ignore_sticky_posts'=>1
+							    'ignore_sticky_posts'=>1,
+									'has_password' => false ,
+									'post_status'=> 'publish'
 							);
 						}
 			} //end of block for categories
