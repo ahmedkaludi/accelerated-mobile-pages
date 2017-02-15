c4ef541262cf8f97d5001162a5f7eafa61f2b161
Khaled Mohammed <khaled.mak94@gmail.com>
2017-02-15 12:10:42 +0530

    FIxed short codes issue in related posts #543

--- templates/design-manager/design-2/elements/related-posts.php
+++ templates/design-manager/design-2/elements/related-posts.php
@@ -69,7 +69,7 @@
 																			$content = get_the_content();
 																		}
 																		?>
-								                    <p><?php echo wp_trim_words( $content , '15' ); ?></p>
+								                    <p><?php echo wp_trim_words( strip_shortcodes($content) , '15' ); ?></p>
 								                </div>
 								            </li>
 														<?php
