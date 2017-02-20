50bb2860502dba4e5f6ddf7772f1816455135618
Ahmed Kaludi <ahmed@magazine3.com>
2017-02-21 00:31:56 +0530

    Featured image was having issues in the design 2
    Featured image was having issues in the design 2

--- templates/design-manager/design-2/elements/featured-image.php
+++ templates/design-manager/design-2/elements/featured-image.php
@@ -1,4 +1,3 @@
-<div class="amp-wp-article-featured-image amp-wp-content featured-image-content">
 <?php
 $featured_image = $this->get( 'featured_image' );
 
@@ -8,7 +7,8 @@ if ( empty( $featured_image ) ) {
 
 $amp_html = $featured_image['amp_html'];
 $caption = $featured_image['caption'];
-?>
+?><div class="amp-wp-article-featured-image amp-wp-content featured-image-content">
+
 <figure class="amp-wp-article-featured-image wp-caption">
 	<?php echo $amp_html; // amphtml content; no kses ?>
 	<?php if ( $caption ) : ?>
