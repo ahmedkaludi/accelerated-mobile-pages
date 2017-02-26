a2987cfb64c3be10b00b4382cc0218a01ef6ead0
Ahmed Kaludi <ahmed@magazine3.com>
2017-02-26 23:10:30 +0530

    Report an issue final done.
    Report an issue final done.

--- templates/report-bugs.php
+++ templates/report-bugs.php
@@ -3,21 +3,29 @@ if ( ! function_exists( 'ampforwp_reporting_bugs' ) ) {
 	function ampforwp_reporting_bugs($sections){
 
 $sections[] = array(
-      'title'      => __( 'Report Bugs', 'redux-framework-demo' ),
+      'title'      => __( 'Report an Issue', 'redux-framework-demo' ),
       // 'id'         => 'opt-structured-data',
       // 'subsection' => true,
       'icon' => 'el el-warning-sign ',
-      'desc'  => "<p>What is Lorem Ipsum?
-									Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-									</p>
-									<p>
-									Why do we use it?
-									It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). </p>
-
-									<p>
-									<a href=#>Click here to report the Bugs</a>
-									</p>
-									",
+      'desc'  => "<p><br /><h3>Tell Us What's Happening</h3><strong>We need your help in improving this plugin!</strong></p>
+<p>We take every issue and bug report very seriously. Me and my team personally goes through your feedback and works hard on solving them.</p>
+<p>
+<a href='https://goo.gl/forms/zIks2sTbhBZK0A3L2' style='background: #E91E63;
+    padding: 10px 16px;
+    text-decoration: none;
+    color: #fff;
+    margin-top: 10px;
+    display: inline-block;
+    font-size: 16px;
+    border-radius: 3px;' target='_blank'>Report an Error</a>
+<a href='https://goo.gl/forms/GVeHSzpZVWBok2oo2' style='background: #4CAF50;
+    padding: 10px 16px;
+    text-decoration: none;margin-left:9px;
+    color: #fff;
+    margin-top: 10px;
+    display: inline-block;
+    font-size: 16px;
+    border-radius: 3px;' target='_blank'>Request a Feature</a></p><br />",
 );
 
 	    return $sections;
