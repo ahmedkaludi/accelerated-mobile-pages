fe8e7f44075efed1a93a01832544b0aa45e77faf
Mohammed Kaludi <mohammed@magazine3.com>
2017-02-27 15:20:28 +0530

    Disqus iframe script loading on homepage fixed
    Disqus iframe script loading on homepage fixed

--- templates/features.php
+++ templates/features.php
@@ -1338,7 +1338,7 @@ function ampforwp_add_disqus_support() {
  add_filter( 'amp_post_template_data', 'ampforwp_add_disqus_scripts' );
 function ampforwp_add_disqus_scripts( $data ) {
 	global $redux_builder_amp;
-	if ( $redux_builder_amp['ampforwp-disqus-comments-support'] ) {
+	if ( $redux_builder_amp['ampforwp-disqus-comments-support'] && is_single() ) {
 		if ( empty( $data['amp_component_scripts']['amp-iframe'] ) ) {
 			$data['amp_component_scripts']['amp-iframe'] = 'https://cdn.ampproject.org/v0/amp-iframe-0.1.js';
 		}
