8538681da501fe4caeb31629c6d77fe4ed2ff00a
Nicholas Griffin <nicholasgriffin@technutty.co.uk>
2017-03-01 17:16:22 +0000

    Remove stray conflict tag

--- templates/features.php
+++ templates/features.php
@@ -277,12 +277,9 @@
 		if( is_page() ) { ?>
 <script async custom-element="amp-form" src="https://cdn.ampproject.org/v0/amp-form-0.1.js"></script>
 		<?php } ?>
-<<<<<<< HEAD
 	<?php if($redux_builder_amp['amp-enable-notifications'] == true)  { ?>
 			<script async custom-element="amp-user-notification" src="https://cdn.ampproject.org/v0/amp-user-notification-0.1.js"></script>
 		<?php } ?>
-
-=======
 <script async custom-element="amp-sidebar" src="https://cdn.ampproject.org/v0/amp-sidebar-0.1.js"></script>
 		<?php if($redux_builder_amp['amp-enable-notifications'] == true)  { ?>
 <script async custom-element="amp-user-notification" src="https://cdn.ampproject.org/v0/amp-user-notification-0.1.js"></script>
@@ -292,7 +289,6 @@
 <script async custom-element="amp-social-share" src="https://cdn.ampproject.org/v0/amp-social-share-0.1.js"></script>
 			<?php }
 		} ?>
->>>>>>> ahmedkaludi/master
 		<?php if($redux_builder_amp['amp-frontpage-select-option'] == 1)  { ?>
 			<?php if( $redux_builder_amp['enable-single-social-icons'] == true || AMPFORWP_DM_SOCIAL_CHECK === 'true' )  {  if( is_home() ) { ?> 
 <script async custom-element="amp-social-share" src="https://cdn.ampproject.org/v0/amp-social-share-0.1.js"></script>
