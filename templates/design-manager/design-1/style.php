68af165523098330d1763cb92227c404dd678490
Ahmed Kaludi <ahmed@magazine3.com>
2017-03-03 19:27:51 +0530

    Default Table Mark up in AMP #643  Fixed
    Default Table Mark up in AMP #643  Fixed

--- templates/design-manager/design-1/style.php
+++ templates/design-manager/design-1/style.php
@@ -97,8 +97,26 @@ blockquote {color: <?php echo sanitize_hex_color( $text_color ); ?>;background: 
 amp-lightbox{
     background: rgba(0, 0, 0,0.85);
 }
-/* CSS3 icon */
+/* Tables */
+table a:link { color: #666; font-weight: bold; text-decoration: none; }
+table a:visited { color: #999999; font-weight: bold; text-decoration: none; }
+table a:active,
+table a:hover { color: #bd5a35; text-decoration: underline; }
+table { font-family: Arial, Helvetica, sans-serif; color: #666; font-size: 12px; text-shadow: 1px 1px 0px #fff; background: #eee; margin: 20px; width: 95%; }
+table th { padding: 21px 25px 22px 25px; border-top: 1px solid #fafafa; border-bottom: 1px solid #e0e0e0; background: #ededed; background: -webkit-gradient(linear, left top, left bottom, from(#ededed), to(#ebebeb)); background: -moz-linear-gradient(top, #ededed, #ebebeb); }
+table th:first-child { text-align: left; padding-left: 20px; }
+table tr:first-child th:first-child { -moz-border-radius-topleft: 3px; -webkit-border-top-left-radius: 3px; border-top-left-radius: 3px; }
+table tr:first-child th:last-child { -moz-border-radius-topright: 3px; -webkit-border-top-right-radius: 3px; border-top-right-radius: 3px; }
+table tr { text-align: center; padding-left: 20px; }
+table td:first-child { text-align: left; padding-left: 20px; border-left: 0; }
+table td { padding: 18px; border-top: 1px solid #ffffff; border-bottom: 1px solid #e0e0e0; border-left: 1px solid #e0e0e0; background: #fafafa; background: -webkit-gradient(linear, left top, left bottom, from(#fbfbfb), to(#fafafa)); background: -moz-linear-gradient(top, #fbfbfb, #fafafa); }
+table tr.even td { background: #f6f6f6; background: -webkit-gradient(linear, left top, left bottom, from(#f8f8f8), to(#f6f6f6)); background: -moz-linear-gradient(top, #f8f8f8, #f6f6f6); }
+table tr:last-child td {border-bottom: 0;}
+table tr:last-child td:first-child { -moz-border-radius-bottomleft: 3px; -webkit-border-bottom-left-radius: 3px; border-bottom-left-radius: 3px; }
+table tr:last-child td:last-child { -moz-border-radius-bottomright: 3px; -webkit-border-bottom-right-radius: 3px; border-bottom-right-radius: 3px; }
+table tr:hover td { background: #f2f2f2; background: -webkit-gradient(linear, left top, left bottom, from(#f2f2f2), to(#f0f0f0)); background: -moz-linear-gradient(top, #f2f2f2, #f0f0f0); }
 
+/* CSS3 icon */
 [class*=icono-]:after, [class*=icono-]:before {
     content: '';
     pointer-events: none;
@@ -126,13 +144,11 @@ amp-lightbox{
     text-indent: -9999px;
     direction: ltr
 }
-
 .icono-search {
     -webkit-transform: translateX(-50%);
     -ms-transform: translateX(-50%);
     transform: translateX(-50%)
 }
-
 .icono-search {
     border: 1px solid;
     width: 10px;
--- templates/design-manager/design-3/style.php
+++ templates/design-manager/design-3/style.php
@@ -1051,9 +1051,26 @@ amp-lightbox{
     float:right
 }
 
+/* Tables */
+table a:link { color: #666; font-weight: bold; text-decoration: none; }
+table a:visited { color: #999999; font-weight: bold; text-decoration: none; }
+table a:active,
+table a:hover { color: #bd5a35; text-decoration: underline; }
+table { font-family: Arial, Helvetica, sans-serif; color: #666; font-size: 12px; text-shadow: 1px 1px 0px #fff; background: #eee; margin: 20px; width: 95%; }
+table th { padding: 21px 25px 22px 25px; border-top: 1px solid #fafafa; border-bottom: 1px solid #e0e0e0; background: #ededed; background: -webkit-gradient(linear, left top, left bottom, from(#ededed), to(#ebebeb)); background: -moz-linear-gradient(top, #ededed, #ebebeb); }
+table th:first-child { text-align: left; padding-left: 20px; }
+table tr:first-child th:first-child { -moz-border-radius-topleft: 3px; -webkit-border-top-left-radius: 3px; border-top-left-radius: 3px; }
+table tr:first-child th:last-child { -moz-border-radius-topright: 3px; -webkit-border-top-right-radius: 3px; border-top-right-radius: 3px; }
+table tr { text-align: center; padding-left: 20px; }
+table td:first-child { text-align: left; padding-left: 20px; border-left: 0; }
+table td { padding: 18px; border-top: 1px solid #ffffff; border-bottom: 1px solid #e0e0e0; border-left: 1px solid #e0e0e0; background: #fafafa; background: -webkit-gradient(linear, left top, left bottom, from(#fbfbfb), to(#fafafa)); background: -moz-linear-gradient(top, #fbfbfb, #fafafa); }
+table tr.even td { background: #f6f6f6; background: -webkit-gradient(linear, left top, left bottom, from(#f8f8f8), to(#f6f6f6)); background: -moz-linear-gradient(top, #f8f8f8, #f6f6f6); }
+table tr:last-child td {border-bottom: 0;}
+table tr:last-child td:first-child { -moz-border-radius-bottomleft: 3px; -webkit-border-bottom-left-radius: 3px; border-bottom-left-radius: 3px; }
+table tr:last-child td:last-child { -moz-border-radius-bottomright: 3px; -webkit-border-bottom-right-radius: 3px; border-bottom-right-radius: 3px; }
+table tr:hover td { background: #f2f2f2; background: -webkit-gradient(linear, left top, left bottom, from(#f2f2f2), to(#f0f0f0)); background: -moz-linear-gradient(top, #f2f2f2, #f0f0f0); }
 
 /* Responsive */
-
 @media screen and (min-width:1034px){
     .single-post figcaption{
         margin: -17px 17px 0 -17px;
