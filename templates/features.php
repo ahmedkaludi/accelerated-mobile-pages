e120eb153b4684532ad06b65b8812e294f138fc4
Khaled Mohammed <khaled.mak94@gmail.com>
2017-03-15 22:10:34 +0530

    #694 Digg Digg social share compatibility

--- templates/features.php
+++ templates/features.php
@@ -1765,3 +1765,13 @@ function ampforwp_add_notification_scripts( $data ) {
 
 	return $data;
 }
+
+
+//60. Adding Digg Digg compatibility with AMP
+function ampforwp_dd_exclude_from_amp() {
+if(ampforwp_is_amp_endpoint()) {
+      remove_filter('the_excerpt', 'dd_hook_wp_content');
+    	remove_filter('the_content', 'dd_hook_wp_content');
+	}
+}
+add_action('template_redirect', 'ampforwp_dd_exclude_from_amp');
