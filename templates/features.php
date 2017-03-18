a72a4e46a044264a3e5f934af5e826bd26c72830
Ahmed Kaludi <ahmed@magazine3.com>
2017-03-18 19:28:54 +0530

    Add Woo-commerce shortcode #697
    Add Woo-commerce shortcode #697

--- templates/features.php
+++ templates/features.php
@@ -1860,43 +1860,41 @@ Examples:
 			 'posts_per_page' => $number_of_latest_prcts
 			) );
 
-			$content = '';
-
-		  if ( $q->have_posts() ) : while ( $q->have_posts() ) : $q->the_post();
+		  if ( $q->have_posts() ) :  $content .= '<ul class="ampforwp_wc_shortcode">';
+            while ( $q->have_posts() ) : $q->the_post();
 				if( $atts['link'] === 'amp' ) {
 					$ampforwp_post_url = trailingslashit( get_permalink() ) . AMPFORWP_AMP_QUERY_VAR ;
 				} else {
 					$ampforwp_post_url = trailingslashit( get_permalink() ) ;
 				}
-						$content .= '<a href="'.$ampforwp_post_url.'">';
+						$content .= '<li class="ampforwp_wc_shortcode_child"><a href="'.$ampforwp_post_url.'">';
 						global $redux_builder_amp;
-						$content .= '<div class="amp-wp-content ampforwp-wc-parent"><div class="amp-wp-content featured-image-content">';
+//						$content .= '<div class="amp-wp-content ampforwp-wc-parent"><div class="amp-wp-content featured-image-content">';
 						if ( has_post_thumbnail() ) {
 							$thumb_id = get_post_thumbnail_id();
 							$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail', true);
 							$thumb_url = $thumb_url_array[0];
 
-							$content .= '<div class="post-featured-img"><amp-img src='.$thumb_url.' width="150" height="150" ></amp-img></div>' ;
+							$content .= '<amp-img src='.$thumb_url.' width="150" height="150" ></amp-img>' ;
 						}
-						$content .= '</div>';
+//						$content .= '</div>';
 						$content .= '<div class="ampforwp-wc-title">'.get_the_title().'</div>';
-						$content .= '</a>';
 						if (  class_exists( 'WooCommerce' )  ) {
-							$content .= '<div class="ampforwp-wc-price">';
+//							$content .= '<div class="ampforwp-wc-price">';
 							global $woocommerce;
 							$amp_product_price 	=  $woocommerce->product_factory->get_product()->get_price_html();
 							$context = '';
 							$allowed_tags 		= wp_kses_allowed_html( $context );
 
 							if ( $amp_product_price ) {
-								$content .= 'Price: ' .  wp_kses( $amp_product_price,  $allowed_tags  )  ;
+								$content .= '<div class="ampforwp-wc-price">' .  wp_kses( $amp_product_price,  $allowed_tags  ) .'</div>' ;
 							} else {
-								$content .= "Sorry, this item is not for sale at the moment, please check out more products <a href=" . esc_url( home_url('/shop') ) . "> Here </a> " ;
+//								$content .= "Sorry, this item is not for sale at the moment, please check out more products <a href=" . esc_url( home_url('/shop') ) . "> Here </a> " ;
 							}
 						}
-						$content .= '</div></div>'; ?>
-					<?php endwhile;  ?>
-			<?php endif; ?>
+                        $content .= '</a></li>';  ?>
+					<?php endwhile;  $content .= '</ul>'; ?>
+			<?php endif; ?> 
 			<?php wp_reset_postdata();
 
 			 // Add AMP Woocommerce latest Products only on AMP Endpoint
@@ -1913,9 +1911,11 @@ Examples:
 	 add_action( 'amp_init', 'ampforwp_latest_products_register_shortcodes');
 
 	 // Adding the styling for AMP Woocommerce latest Products
-   add_action('amp_post_template_css','amp_latest_products_styling');
-   function amp_latest_products_styling() {
-		 // all the styling goes here between these comments ?>
-
-       <?php // all the styling goes here between these comments
-   }
+   add_action('amp_post_template_css','amp_latest_products_styling',PHP_INT_MAX);
+   function amp_latest_products_styling() { ?>
+.ampforwp_wc_shortcode{padding:0}
+.ampforwp_wc_shortcode li{ font-size:12px; line-height: 1; float: left;max-width: 150px;list-style-type: none;margin: 10px;}
+.single-post .ampforwp_wc_shortcode li amp-img{margin:0}
+.ampforwp-wc-title{ margin: 10px 0px; }
+.ampforwp-wc-price{ color:#444 }
+    <?php }
