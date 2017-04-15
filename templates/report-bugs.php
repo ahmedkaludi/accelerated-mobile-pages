0320a5d13e6839bd6309c6a98c60259142f806fd
Khaled Mohammed <khaled.mak94@gmail.com>
2017-04-16 01:03:07 +0530

    now translatable

--- templates/report-bugs.php
+++ templates/report-bugs.php
@@ -3,12 +3,12 @@ if ( ! function_exists( 'ampforwp_reporting_bugs' ) ) {
 	function ampforwp_reporting_bugs($sections){
 
 $sections[] = array(
-      'title'      => __( 'Report an Issue /<br> Request a Feature', 'redux-framework-demo' ),
+      'title'      => __( 'Report an Issue /<br> Request a Feature', 'accelerated-mobile-pages' ),
       // 'id'         => 'opt-structured-data',
       // 'subsection' => true,
       'icon' => 'el el-warning-sign ',
-      'desc'  => "<p><br /><h3>Tell Us What's Happening</h3><strong>We need your help in improving this plugin!</strong></p>
-<p>We take every issue and bug report very seriously. Me and my team personally goes through your feedback and works hard on solving them.</p>
+      'desc'  => "<p><br />".__("<h3>Tell Us What's Happening</h3><strong>We need your help in improving this plugin!</strong></p>
+<p>We take every issue and bug report very seriously. Me and my team personally goes through your feedback and works hard on solving them.</p>",'accelerated-mobile-pages')."
 <p>
 <a href='https://goo.gl/forms/zIks2sTbhBZK0A3L2' style='background: #E91E63;
     padding: 10px 16px;
