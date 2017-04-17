45322be7bcf16d1da70b5361090e52bdc99b38fc
riyaz95 <sk.95riyaz@gmail.com>
2017-04-17 12:45:26 +0530

    ampforwp_translation Function correction

--- templates/design-manager/design-3/search.php
+++ templates/design-manager/design-3/search.php
@@ -100,9 +100,9 @@
 
 
 		<div id="pagination">
-			<div class="next"><?php next_posts_link( ampforwp_translate($redux_builder_amp['amp-translator-show-more-posts-text'] , 'Show more Posts'), 0 ) ?></div>
+			<div class="next"><?php next_posts_link( ampforwp_translation($redux_builder_amp['amp-translator-show-more-posts-text'] , 'Show more Posts'), 0 ) ?></div>
 				<?php if ( $paged > 1 ) { ?>
-					<div class="prev"><?php previous_posts_link( ampforwp_translate($redux_builder_amp['amp-translator-show-previous-posts-text'], 'Show previous Posts' ) ); ?></div>
+					<div class="prev"><?php previous_posts_link( ampforwp_translation($redux_builder_amp['amp-translator-show-previous-posts-text'], 'Show previous Posts' ) ); ?></div>
 				<?php } ?>
 			<div class="clearfix"></div>
 		</div>
