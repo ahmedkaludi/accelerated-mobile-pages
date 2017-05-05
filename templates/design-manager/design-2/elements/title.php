a27fb6bf812bee40cef383af80afeb380accc199
Mohammed Kaludi <mohammed@magazine3.com>
2017-05-05 16:07:47 +0530

    Filter added to modify title html Fixed #727
    Filter added to modify title html Fixed #727

--- templates/design-manager/design-2/elements/title.php
+++ templates/design-manager/design-2/elements/title.php
@@ -1,4 +1,8 @@
 <header class="amp-wp-article-header ampforwp-title">
-	<h1 class="amp-wp-title"><?php echo wp_kses_data( $this->get( 'post_title' ) ); ?></h1>
+	<h1 class="amp-wp-title"> <?php 
+		$ampforwp_title = $this->get( 'post_title' ) ;
+		$ampforwp_title =  apply_filters('ampforwp_filter_single_title', $ampforwp_title);
+		echo wp_kses_data( $ampforwp_title ); ?>
+	</h1>
 	<?php do_action('ampforwp_below_the_title'); ?>
 </header>
