592932a5743ebdf1323cb6c246809175ba422fb9
Mohammed Kaludi <mohammed@magazine3.com>
2017-05-11 17:38:28 +0530

    extra spacing removed #826
    extra spacing removed #826

--- templates/design-manager/design-1/archive.php
+++ templates/design-manager/design-1/archive.php
@@ -33,9 +33,7 @@
 	  <?php if ( is_archive() ) {
 	    the_archive_title( '<h3 class="page-title">', '</h3>' );
 			$description 	= get_the_archive_description();
-			$sanitizer = new AMPFORWP_Content( $description, array(), apply_filters( 'ampforwp_content_sanitizers',
-			 																														array( 'AMP_Img_Sanitizer' => array(),
-																																         'AMP_Style_Sanitizer' => array() ) ) );
+			$sanitizer = new AMPFORWP_Content( $description, array(), apply_filters( 'ampforwp_content_sanitizers', array( 'AMP_Img_Sanitizer' => array(), 'AMP_Style_Sanitizer' => array() ) ) );
 			$arch_desc 		= $sanitizer->get_amp_content();
 			if( $arch_desc ) {  ?>
 				<div class="amp-wp-content taxonomy-description">
