2ad0251baef7de8422c2f6b50f78cc52902bd8c5
Mohammed Kaludi <mohammed@magazine3.com>
2017-05-18 18:16:06 +0530

    Remove elements of WP Like Button plugin #841
    Remove elements of WP Like Button plugin #841

--- templates/features.php
+++ templates/features.php
@@ -1205,6 +1205,9 @@ function ampforwp_remove_schema_data() {
 	// Removed GTranslate Flags from AMP pages #819
 	remove_filter('wp_nav_menu_items', 'gtranslate_menu_item', 10, 2);
 
+	remove_filter('the_content', 'fb_like_button');
+	remove_filter('the_excerpt', 'fb_like_button');
+
 }
 
 // 22. Removing author links from comments Issue #180
