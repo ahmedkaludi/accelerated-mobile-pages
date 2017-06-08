b526a67afc6718e3da85e8780a788c1d7740cdc8
Ahmed Kaludi <ahmed@magazine3.com>
2017-06-08 16:01:37 +0530

    Styles saved
    Styles saved

--- templates/design-manager/design-2/style.php
+++ templates/design-manager/design-2/style.php
@@ -63,7 +63,6 @@ amp-sidebar{ width: 250px; }
 .amp-category-block-btn{ display: block; text-align: center; font-size: 13px; margin-top: 15px; border-bottom: 1px solid #f1f1f1; text-decoration: none; padding-bottom: 8px;}
 .category-widget-gutter h4{ margin-bottom: 0px;}
 .category-widget-gutter ul{ margin-top: 10px; list-style-type:none; padding:0 }
-.amp-category-block-title{ border-bottom: double #ddd; margin-bottom: 12px; padding-bottom: 1px; text-align: center; font-size: 12px; text-transform: uppercase; color: #666; }
 .amp-category-post{ width: 32%; display:inline-table; }
 .amp-category-post amp-img{ margin-bottom:5px; }
 .amp-category-block li:nth-child(3){ margin: 0 1%;}
