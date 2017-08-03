8a74e0caf17db8ddca5d45a651fc55d4df25a8e5
Mohammed Razzaq <marqas36@gmail.com>
2017-08-03 19:28:05 +0530

    #1013 alt tag on logo added
    In all designs

--- templates/design-manager/design-3/header-bar.php
+++ templates/design-manager/design-3/header-bar.php
@@ -115,11 +115,11 @@
 
             <?php if($redux_builder_amp['ampforwp-custom-logo-dimensions'] == true)  { ?>
 
-                <amp-img src="<?php echo $redux_builder_amp['opt-media']['url']; ?>" width="<?php echo $redux_builder_amp['opt-media-width']; ?>" height="<?php echo $redux_builder_amp['opt-media-height']; ?>" alt="logo" class="amp-logo"></amp-img>
+                <amp-img src="<?php echo $redux_builder_amp['opt-media']['url']; ?>" width="<?php echo $redux_builder_amp['opt-media-width']; ?>" height="<?php echo $redux_builder_amp['opt-media-height']; ?>" alt="<?php bloginfo('name'); ?>" class="amp-logo"></amp-img>
 
             <?php } else { ?>
 
-                <amp-img src="<?php echo $redux_builder_amp['opt-media']['url']; ?>" width="190" height="36" alt="logo" class="amp-logo"></amp-img>
+                <amp-img src="<?php echo $redux_builder_amp['opt-media']['url']; ?>" width="190" height="36" alt="<?php bloginfo('name'); ?>" class="amp-logo"></amp-img>
 
             <?php } ?>
 
