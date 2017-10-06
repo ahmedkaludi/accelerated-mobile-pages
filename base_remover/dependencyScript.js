aecf84d590dc60365b7a780e22e79229d9e17ee5
Ajeet kumar <ajeet.gupta51@gmail.com>
2017-10-06 19:53:50 +0530

    #1051 alert on installation of OLD AMP after search
    #1051 alert on installation of OLD AMP after search

--- base_remover/dependencyScript.js
+++ base_remover/dependencyScript.js
@@ -1,5 +1,21 @@
 jQuery(document).ready(function($){
+	
+	var myElement = document.getElementById('plugin-filter');
+	//if( myElement.innerHtml!=''){
+		if(window.addEventListener) {
+		   // Normal browsers
+		   myElement.addEventListener('DOMSubtreeModified', contentChanged, false);
+		} else
+		   if(window.attachEvent) {
+			  myElement.attachEvent('DOMSubtreeModified', contentChanged,false);
+		   }
+	//}
+	   
+	
+	function contentChanged() {
 		if($('.plugin-card-amp').length>0){
 			$('.plugin-card-amp').find('.plugin-action-buttons').find('li:first a').attr("href","#").attr('onclick','alert(\'This plugin is already added in our core\')');
-		}		
+		}
+	}
+	contentChanged();
 });
