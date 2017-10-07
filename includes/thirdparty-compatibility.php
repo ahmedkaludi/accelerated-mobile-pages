7d0102bf95824d4094a03f0c12a6175529b670a8
Ajeet kumar <ajeet.gupta51@gmail.com>
2017-10-07 12:32:33 +0530

    #1131 change function name

--- includes/thirdparty-compatibility.php
+++ includes/thirdparty-compatibility.php
@@ -1,7 +1,7 @@
 <?php
 //Remove ResponsifyWP #1131
-add_action('plugins_loaded', 'my_functions');
-function my_functions(){
+add_action('plugins_loaded', 'ampforwp_filter_remove_function_responsifywp');
+function ampforwp_filter_remove_function_responsifywp(){
   if(is_plugin_active('responsify-wp/responsify-wp.php')){
 	add_filter('rwp_add_filters','removeFilterOfResponsify');
 	function removeFilterOfResponsify($filter){
