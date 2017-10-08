07cbde052d50898d4f8a816934a85f19e70ab5c6
Ahmed Kaludi <ahmed@magazine3.com>
2017-10-08 16:42:31 +0530

    License Upgraded

--- accelerated-moblie-pages.php
+++ accelerated-moblie-pages.php
@@ -3,11 +3,11 @@
 Plugin Name: Accelerated Mobile Pages
 Plugin URI: https://wordpress.org/plugins/accelerated-mobile-pages/
 Description: AMP for WP - Accelerated Mobile Pages for WordPress
-Version: 0.9.62
+Version: 0.9.63
 Author: Ahmed Kaludi, Mohammed Kaludi
 Author URI: https://ampforwp.com/
 Donate link: https://www.paypal.me/Kaludi/25
-License: GPL2
+License: GPL2+
 */
 
 // Exit if accessed directly.
