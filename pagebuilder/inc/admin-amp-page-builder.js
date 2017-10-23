b8c3832dbf48aa88c315f70ed4fde8410a2f84ca
Ajeet kumar <ajeet.gupta51@gmail.com>
2017-10-23 18:35:27 +0530

    #1162 compatibility issue has fixed
    Removed call of tinymce library call in post/page edit links

--- pagebuilder/inc/admin-amp-page-builder.js
+++ pagebuilder/inc/admin-amp-page-builder.js
@@ -339,29 +339,126 @@ jQuery( document ).ready( function( $ ){
 					console.log(value);
 					tinymce.get(value).destroy();
 				}
+				$("#"+value).before('<button type="button" id="insert-media-button" class="button insert-media add_media" data-editor="'+value+'"><span class="wp-media-buttons-icon"></span> Add Media</button>');
 				tinymce.init( {
 							mode : "exact",
 							elements : value,  //'pre-details',
 							theme: "modern",
 							skin: "lightgray",
+							language: "en",
 							menubar : false,
 							statusbar : false,
-							toolbar: [
-				"bold italic underline strikethrough superscript| alignleft aligncenter alignright | bullist numlist outdent indent | undo redo | headings | link image | removeformat media |wpgallery wpautoresize"
-							],
-							plugins: 'charmap colorpicker compat3x directionality fullscreen hr image lists media paste tabfocus textcolor wordpress wpautoresize wpdialogs wpeditimage wpemoji wpgallery wplink wptextpattern wpview',
-							/* content_css: [
-								'//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
-								'//www.tinymce.com/css/codepen.min.css'
-							], */
-							/* plugins : "paste", */
 							branding: false,
 							paste_auto_cleanup_on_paste : true,
 							paste_postprocess : function( pl, o ) {
 								o.node.innerHTML = o.node.innerHTML.replace( /&nbsp;+/ig, " " );
-							}
+							},
+							formats: {
+								alignleft: [
+								  {
+								    selector: "p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li",
+								    styles: {
+								      textAlign: "left"
+								    }
+								  },
+								  {
+								    selector: "img,table,dl.wp-caption",
+								    classes: "alignleft"
+								  }
+								],
+								aligncenter: [
+								  {
+								    selector: "p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li",
+								    styles: {
+								      textAlign: "center"
+								    }
+								  },
+								  {
+								    selector: "img,table,dl.wp-caption",
+								    classes: "aligncenter"
+								  }
+								],
+								alignright: [
+								  {
+								    selector: "p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li",
+								    styles: {
+								      textAlign: "right"
+								    }
+								  },
+								  {
+								    selector: "img,table,dl.wp-caption",
+								    classes: "alignright"
+								  }
+								],
+								strikethrough: {
+								  inline: "del"
+								}
+							},
+							relative_urls: false,
+							remove_script_host: false,
+							convert_urls: false,
+							browser_spellcheck: true,
+							fix_list_elements: true,
+							entities: "38,amp,60,lt,62,gt",
+							entity_encoding: "raw",
+							keep_styles: false,
+							cache_suffix: "wp-mce-4603-20170530",
+							resize: false,
+							menubar: false,
+							branding: false,
+							preview_styles: "font-family font-size font-weight font-style text-decoration text-transform",
+							end_container_on_empty_block: true,
+							wpeditimage_html5_captions: true,
+							wp_lang_attr: "en-US",
+							wp_shortcut_labels: {
+								"Heading 1": "access1",
+								"Heading 2": "access2",
+								"Heading 3": "access3",
+								"Heading 4": "access4",
+								"Heading 5": "access5",
+								"Heading 6": "access6",
+								"Paragraph": "access7",
+								"Blockquote": "accessQ",
+								"Underline": "metaU",
+								"Strikethrough": "accessD",
+								"Bold": "metaB",
+								"Italic": "metaI",
+								"Code": "accessX",
+								"Align center": "accessC",
+								"Align right": "accessR",
+								"Align left": "accessL",
+								"Justify": "accessJ",
+								"Cut": "metaX",
+								"Copy": "metaC",
+								"Paste": "metaV",
+								"Select all": "metaA",
+								"Undo": "metaZ",
+								"Redo": "metaY",
+								"Bullet list": "accessU",
+								"Numbered list": "accessO",
+								"Insert\/edit image": "accessM",
+								"Remove link": "accessS",
+								"Toolbar Toggle": "accessZ",
+								"Insert Read More tag": "accessT",
+								"Insert Page Break tag": "accessP",
+								"Distraction-free writing mode": "accessW",
+								"Keyboard Shortcuts": "accessH"
+							},
+							content_css: "",
+							plugins: "charmap,colorpicker,hr,lists,media,paste,tabfocus,textcolor,fullscreen,wordpress,wpautoresize,wpeditimage,wpemoji,wpgallery,wplink,wpdialogs,wptextpattern,wpview",
+							selector: "#"+value,
+							wpautop: true,
+							indent: false,
+							toolbar1: "formatselect,bold,italic,bullist,numlist,blockquote,alignleft,aligncenter,alignright,link,unlink,wp_more,spellchecker,dfw,wp_adv,|,ampforwp_tc_button",
+							toolbar2: "strikethrough,hr,forecolor,pastetext,removeformat,charmap,outdent,indent,undo,redo,wp_help",
+							toolbar3: "",
+							toolbar4: "",
+							wp_autoresize_on: true,
+							add_unload_trigger: false
 						} );
-				
+				if ( ! window.wpActiveEditor ) {
+					window.wpActiveEditor = value;
+				}
 			})
 			
 		}
