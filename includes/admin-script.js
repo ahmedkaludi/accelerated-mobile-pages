bf1895ecafe2dae045caa57908a3a0e15063ced1
Mohammed Kaludi <mohammed@magazine3.com>
2017-10-25 19:43:59 +0530

    Updating in 0.9.65.2 quick release #1303  #1299
    Updating in 0.9.65.2 quick release #1303  #1299

--- includes/admin-script.js
+++ includes/admin-script.js
@@ -1,8 +1,79 @@
 jQuery(function($) {
+    var reduxOptionSearch = function(){
+            jQuery('.redux_field_search').typeWatch({
+                callback:function( searchString ){
+                    searchString = searchString.toLowerCase();
+                    var searchArray = searchString.split(' ');
+                    var parent = $(this).parents('.redux-container:first');
+                    var expanded_options = parent.find('.expand_options');
+                    if (searchString != "") {
+                        if (!expanded_options.hasClass('expanded')) {
+                            expanded_options.click();
+                            parent.find('.redux-main').addClass('redux-search');
+                        }
+                    } else {
+                        if (expanded_options.hasClass('expanded')) {
+                            expanded_options.click();
+                            parent.find('.redux-main').removeClass('redux-search');
+                        }
+                        parent.find('.redux-section-field, .redux-info-field, .redux-notice-field, .redux-container-group, .redux-section-desc, .redux-group-tab h3').show();
+                        
+                        if($('.redux-group-tab-link-li.active').length>0){
+                            var rel = $('.redux-group-tab-link-li.active a').attr('data-rel');
+                            var selector = 'div#'+rel+'_section_group';
+                            jQuery(selector).show();
+                            jQuery(selector).css('display','block');
+                           
+                        }else{
+                            $('.redux-group-tab-link-li.activeChild').click();
+                            $('div#'+rel+'_section_group').show();
+                            $('#'+rel+'_section_group').css('display','block');
+                        }
+
+                    }            
+                    parent.find('.redux-field-container').each(function() {
+                        if (searchString != "") {
+                            parent.find('div.redux-group-tab').css('display','none');
+                            $(this).parents('tr:first').hide();
+                        } else {
+                            $(this).parents('tr:first').show();
+                        }
+                    });
+                    parent.find('.form-table tr').filter(function () {
+                        if(searchString==''){
+                            return false;
+                        }
+                        var item = $(this);
+                        var isMatch = true,
+                            text = $(this).find('.redux_field_th').text().toLowerCase();
+                        if ( !text || text == "" ) {
+                            return false;
+                        }
+                        $.each(searchArray, function (i, searchStr) {
+                            if (text.indexOf(searchStr) == -1) {
+                                isMatch = false;
+                            }
+                        });
+                        if (isMatch) {
+                            $(this).show();
+                             $(this).parents('div.redux-group-tab').css('display','block');
+                        }
+                        return isMatch;
+                    }).show( function() {
+
+                           
+                    }); 
+                },
+                wait:400,
+                highlight:false,
+                captureLength:0
+            });
+        }
     $(document).ready(function() {
         $('.redux-container').each(function() {
             if (!$(this).hasClass('redux-no-sections')) {
                 $(this).find('.redux-main').prepend('<input style="float:right" class="redux_field_search" name="" type="text" placeholder="Search the controls"/>');
+                reduxOptionSearch();
             }
         });
 
@@ -13,63 +84,6 @@ jQuery(function($) {
                 return false;
             }
         });
-
-        jQuery('.redux_field_search').typeWatch({
-            callback:function( searchString ){
-                searchString = searchString.toLowerCase();
-                var searchArray = searchString.split(' ');
-                var parent = $(this).parents('.redux-container:first');
-                var expanded_options = parent.find('.expand_options');
-                if (searchString != "") {
-                    if (!expanded_options.hasClass('expanded')) {
-                        expanded_options.click();
-                        parent.find('.redux-main').addClass('redux-search');
-                    }
-                } else {
-                    if (expanded_options.hasClass('expanded')) {
-                        expanded_options.click();
-                        parent.find('.redux-main').removeClass('redux-search');
-                    }
-                    parent.find('.redux-section-field, .redux-info-field, .redux-notice-field, .redux-container-group, .redux-section-desc, .redux-group-tab h3').show();
-
-                }            
-                parent.find('.redux-field-container').each(function() {
-                    if (searchString != "") {
-                        $(this).parents('tr:first').hide();
-                    } else {
-                        $(this).parents('tr:first').show();
-                    }
-                });
-                parent.find('div.redux-group-tab').css('display','none');
-                parent.find('.form-table tr').filter(function () {
-                    var item = $(this);
-                    var isMatch = true,
-                        text = $(this).find('.redux_field_th').text().toLowerCase();
-                    if ( !text || text == "" ) {
-                        return false;
-                    }
-                    $.each(searchArray, function (i, searchStr) {
-                        if (text.indexOf(searchStr) == -1) {
-                            isMatch = false;
-                        }
-                    });
-                    if (isMatch) {
-                    	console.log(this);
-                        $(this).show();
-
-                	 	// console.log($(this).parents('div.redux-group-tab'));
-                         $(this).parents('div.redux-group-tab').css('display','block');
-                    }
-                    return isMatch;
-                }).show( function() {
-
-                       
-                }); 
-            },
-            wait:400,
-            highlight:false,
-            captureLength:0
-        });
-
+        
     });
 });
