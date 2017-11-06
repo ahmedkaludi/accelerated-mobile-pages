364381b376fd2b16e1e1c14b478aa2b60ac78682
Mohammed Razzaq <marqas36@gmail.com>
2017-11-06 16:15:05 +0530

    #868 Fix: PageBuilder+Sidebars issue
    Sidebars and Pagebuilder are in proper synchronous now

--- templates/design-manager/design-1/elements/content.php
+++ templates/design-manager/design-1/elements/content.php
@@ -23,7 +23,8 @@ if( array_key_exists( 'enable-excerpt-single' , $redux_builder_amp ) ) {
 			// Custom/Alternative AMP content added through post meta
 			$ampforwp_the_content = $this->get( 'ampforwp_amp_content' );
 		}
-
+		//Filter to modify the Content
+		$ampforwp_the_content = apply_filters('ampforwp_modify_the_content', $ampforwp_the_content);
 		if($redux_builder_amp['amp-pagination']) {
 			$ampforwp_new_content = explode('<!--nextpage-->', $ampforwp_the_content);
 		      	$queried_var = get_query_var('page');
