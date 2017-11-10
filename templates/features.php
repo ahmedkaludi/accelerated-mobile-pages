5f33ab17ab80fde0bda4ade69788176820505f4c
Mohammed Razzaq <marqas36@gmail.com>
2017-11-10 22:43:56 +0530

    #1285 WPML amphtml code rewrite

--- templates/features.php
+++ templates/features.php
@@ -104,6 +104,8 @@
 	94. OneSignal Push Notifications
 	95. Modify menu link attributes for SiteNavigationElement Schema Markup #1229 #1345
 	96. ampforwp_is_front_page() ampforwp_is_home() and ampforwp_is_blog is created
+	97. Change the format of the post date on Loops #1384
+	98. WPML amphtml modifier
 */
 // Adding AMP-related things to the main theme
 	global $redux_builder_amp;
@@ -239,7 +241,6 @@ define('AMPFORWP_COMMENTS_PER_PAGE',  ampforwp_define_comments_number() );
 	    } else {
 	      $amp_url = amp_get_permalink( get_queried_object_id() );
 	    }
-
         global $post;
         $ampforwp_amp_post_on_off_meta = get_post_meta( get_the_ID(),'ampforwp-amp-on-off',true);
         if(  is_singular() && $ampforwp_amp_post_on_off_meta === 'hide-amp' ) {
@@ -276,20 +277,6 @@ define('AMPFORWP_COMMENTS_PER_PAGE',  ampforwp_define_comments_number() );
 				$amp_url = untrailingslashit($current_search_url);
 			}
 
-			// WPML AMPHTML #1285
-			include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-			if(is_plugin_active( 'sitepress-multilingual-cms/sitepress.php' )){
-			          Global $sitepress_settings;
-			          if($sitepress_settings[ 'language_negotiation_type' ] == 3){
-			              $wpml_url =get_permalink( get_queried_object_id() );
-			              $explode_url = explode('/', $wpml_url);
-			              $append_amp = 'amp';
-			              array_splice( $explode_url, 5, 0, $append_amp );
-			              $impode_url = implode('/', $explode_url);
-			              $amp_url = untrailingslashit($impode_url);
-			          }
-			        }
-			
 			$amp_url = ampforwp_url_purifier($amp_url);
 	        $amp_url = apply_filters('ampforwp_modify_rel_canonical',$amp_url);
 
@@ -4939,7 +4926,7 @@ function ampforwp_is_blog(){
 
   return $get_blog_details ;
 }
-// Change the format of the post date on Loops #1384
+// 97. Change the format of the post date on Loops #1384
 add_filter('ampforwp_modify_post_date', 'ampforwp_full_post_date_loops');
 if( ! function_exists( 'ampforwp_full_post_date_loops' ) ){
 	function ampforwp_full_post_date_loops($date){
@@ -4948,5 +4935,42 @@ if( ! function_exists( 'ampforwp_full_post_date_loops' ) ){
 			$date =  get_the_date();
 		}
 	return $date;
+	}
+}
+
+// 98. WPML amphtml modifier
+add_filter('ampforwp_modify_rel_canonical','ampforwp_wpml_rel_modifier');
+if( ! function_exists('ampforwp_wpml_rel_modifier')){
+	function ampforwp_wpml_rel_modifier($amp_url){
+	include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+	if(is_plugin_active( 'sitepress-multilingual-cms/sitepress.php' )){
+		  global $sitepress_settings, $wp;
+		  if($sitepress_settings[ 'language_negotiation_type' ] == 3){
+		  	if(is_single() ){
+		      $wpml_url =get_permalink( get_queried_object_id() );
+		      $explode_url = explode('/', $wpml_url);
+		      $append_amp = 'amp';
+		      array_splice( $explode_url, 5, 0, $append_amp );
+		      $impode_url = implode('/', $explode_url);
+		      $amp_url = user_trailingslashit($impode_url);
+		    }
+		    if ( is_home()  || is_archive() ){
+		        global $wp;
+		        $current_archive_url = home_url( $wp->request );
+				$explode_path  	= explode("/",$current_archive_url);
+				$inserted 		= array(AMPFORWP_AMP_QUERY_VAR);
+				$query_arg_array = $wp->query_vars;
+				if( array_key_exists( 'paged' , $query_arg_array ) ) {
+					array_splice( $explode_path, -3, 0, $inserted );
+				}
+				else{
+					array_splice( $explode_path, -1, 0, $inserted );
+				}
+				$impode_url = implode('/', $explode_path);
+				$amp_url = $impode_url;
+		    }
+		  }
+		}
+	return $amp_url;
 	}
 }
