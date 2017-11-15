393b90ec39a22a9b4ee421e6ccdc7c3faff20a20
Mohammed Razzaq <marqas36@gmail.com>
2017-11-15 13:39:24 +0530

    #1404 Folder name changed

--- templates/features.php
+++ templates/features.php
@@ -4837,7 +4837,7 @@ if( ! function_exists( ' ampforwp_onesignal_notifications ' ) ){
 		$permission_dialog_url  = '';
 		$service_worker_url		= '';
 		// HTTPS sites
-		$onesignal_domain 		= AMPFORWP_PLUGIN_DIR_URI.'includes/OneSignal/';
+		$onesignal_domain 		= AMPFORWP_PLUGIN_DIR_URI.'includes/onesignal-integration/';
 		$onesignal_app_id		= $redux_builder_amp['ampforwp-one-signal-app-id'];
 		$helper_iframe_url = $onesignal_domain .'amp-helper-frame.html?appId=' . $onesignal_app_id;
 
