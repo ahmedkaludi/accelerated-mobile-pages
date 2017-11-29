14ad5ab8dcdee74947f3c463c4b8eef5842c93fb
Mohammed Razzaq <marqas36@gmail.com>
2017-11-29 14:31:19 +0530

    #1432 Few modifications

--- templates/features.php
+++ templates/features.php
@@ -5248,15 +5248,17 @@ function ampforwp_default_logo($param=""){
 	return $value;
 } 
 // Envira Lazy Load compatibility
-add_action('pre_amp_render_post', 'ampforwp_envira_compatibility');
-function ampforwp_envira_compatibility(){
-	add_filter('envira_gallery_pre_data', 'ampforwp_envira_lazy_load');
-	if( ! function_exists(' ampforwp_envira_lazy_load ') ){
-		function ampforwp_envira_lazy_load($data){
-			// Change the Lazyload value to Zero
-			$data['config']['lazy_loading'] = 0;
-			$data['config']['lazy_loading_delay'] = 0;
-			return $data;
+add_filter('envira_gallery_pre_data', 'ampforwp_envira_lazy_load');
+if( ! function_exists(' ampforwp_envira_lazy_load ') ){
+	function ampforwp_envira_lazy_load($data){
+	if( function_exists('ampforwp_is_amp_endpoint') && ampforwp_is_amp_endpoint() ){
+		if(function_exists('envira_get_config')){
+			$checker = envira_get_config( 'lazy_loading', $data);
+			if( 1 === $checker){
+				$data['config']['lazy_loading'] = 0;
+			}
 		}
 	}	
-}
+	return $data;
+	}
+}	
