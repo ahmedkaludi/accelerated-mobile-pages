273d3994a0e854e3a36217bbf530aff1b679c7e6
Raju Jeelaga <rajujeelaga@gmail.com>
2017-12-15 19:06:46 +0530

    image module is added
    image module is added

---
+++ pagebuilder/modules/image-mod.php
@@ -0,0 +1,81 @@
+<?php 
+$output = '
+<div class="image-blk">
+	<img src="">
+</div>
+
+
+';
+$css = '
+{{module-class}} .image-blk{
+   width:100%;
+   display:inline-block;
+   text-align:{{align_type}};
+}
+
+
+
+
+';
+return array(
+		'label' =>'Image',
+		'name' =>'image-mod',
+		'default_tab'=> 'customizer',
+		'tabs' => array(
+              'customizer'=>'Customizer',
+              'container_css'=>'Container css'
+            ),
+		'fields' => array(
+						array(		
+		 						'type'		=>'upload',		
+		 						'name'		=>"img_upload",		
+		 						'label'		=>'Image Upload',
+		           				 'tab'     =>'customizer',
+		 						'default'	=>'Title',	
+		           				'content_type'=>'html',
+	 						),
+	 					array(		
+	 							'type'	=>'select',		
+	 							'name'  =>'align_type',		
+	 							'label' =>"Alignment",
+								'tab'     =>'customizer',
+	 							'default' =>'center',
+	 							'options_details'=>array(
+	 												'center'    =>'Center',
+	 												'left'  	=>'Left',
+	 												'right'    =>'Right', 													),
+	 							'content_type'=>'css',
+	 						),	
+						array(
+								'type'		=>'spacing',
+								'name'		=>"margin_css",
+								'label'		=>'Margin',
+								'tab'		=>'customizer',
+								'default'	=>array(
+													'left'=>0,
+													'right'=>0,
+													'top'=>0,
+													'bottom'=>0
+													),
+								'content_type'=>'css',
+							),
+							array(
+								'type'		=>'spacing',
+								'name'		=>"padding_css",
+								'label'		=>'Padding',
+								'tab'		=>'customizer',
+								'default'	=>array(
+													'left'=>0,
+													'right'=>0,
+													'top'=>0,
+													'bottom'=>0
+												),
+								'content_type'=>'css',
+							),
+
+			),
+		'front_template'=> $output,
+		'front_css'=> $css,
+	);
+
+?>
