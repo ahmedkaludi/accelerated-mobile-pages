7400992b2f62516cacc51a519138432ee595f7d9
Raju Jeelaga <rajujeelaga@gmail.com>
2017-12-15 19:02:09 +0530

    button module is added and connected to the backend
    button module is added and connected to the backend

---
+++ pagebuilder/modules/button-mod.php
@@ -0,0 +1,122 @@
+<?php 
+$output = '
+<div class="button-blk">
+	<a href="{{btn_link}}" target="_blank" class="btn-txt">{{content_title}}</a>
+</div>
+
+
+';
+$css = '
+{{module-class}} .button-blk{
+   width:100%;
+   display:inline-block;
+   text-align:{{align_type}};
+}
+{{module-class}} .button-blk .btn-txt{
+   font-size:{{text-size}};
+   line-height:1.5;
+   color:{{font_color_picker}};
+   background:{{bg_color_picker}};
+   display: inline-block;
+   font-weight:bold;
+   padding: 10px 60px;
+}
+{{module-class}} .button-blk .btn-txt:hover{
+
+}
+';
+return array(
+		'label' =>'Button',
+		'name' =>'button-mod',
+		'default_tab'=> 'customizer',
+		'tabs' => array(
+              'customizer'=>'Customizer',
+              'container_css'=>'Container css'
+            ),
+		'fields' => array(
+						array(		
+		 						'type'		=>'text',		
+		 						'name'		=>"content_title",		
+		 						'label'		=>'Heading',
+		           				 'tab'     =>'customizer',
+		 						'default'	=>'Title',	
+		           				'content_type'=>'html',
+	 						),
+						array(		
+		 						'type'		=>'text',		
+		 						'name'		=>"btn_link",		
+		 						'label'		=>'Link (Make sure its will link Or #)',
+		           				 'tab'     =>'customizer',
+		 						'default'	=>'#',	
+		           				'content_type'=>'html',
+	 						),
+
+	 					array(		
+		 						'type'		=>'text',		
+		 						'name'		=>"text-size",		
+		 						'label'		=>'Font Size',
+		           				 'tab'     =>'customizer',
+		 						'default'	=>'26px',	
+		           				'content_type'=>'css',
+	 						),
+	 					array(		
+	 							'type'	=>'select',		
+	 							'name'  =>'align_type',		
+	 							'label' =>"Alignment",
+								'tab'     =>'customizer',
+	 							'default' =>'center',
+	 							'options_details'=>array(
+	 												'center'    =>'Center',
+	 												'left'  	=>'Left',
+	 												'right'    =>'Right', 													),
+	 							'content_type'=>'css',
+	 						),	
+						array(
+								'type'		=>'color-picker',
+								'name'		=>"font_color_picker",
+								'label'		=>'Color',
+								'tab'		=>'customizer',
+								'default'	=>'#fff',
+								'content_type'=>'css'
+							),
+						array(
+								'type'		=>'color-picker',
+								'name'		=>"bg_color_picker",
+								'label'		=>'Background color',
+								'tab'		=>'customizer',
+								'default'	=>'#2cbf55',
+								'content_type'=>'css',
+							),
+						array(
+								'type'		=>'spacing',
+								'name'		=>"margin_css",
+								'label'		=>'Margin',
+								'tab'		=>'customizer',
+								'default'	=>array(
+													'left'=>0,
+													'right'=>0,
+													'top'=>0,
+													'bottom'=>0
+													),
+								'content_type'=>'css',
+							),
+							array(
+								'type'		=>'spacing',
+								'name'		=>"padding_css",
+								'label'		=>'Padding',
+								'tab'		=>'customizer',
+								'default'	=>array(
+													'left'=>0,
+													'right'=>0,
+													'top'=>0,
+													'bottom'=>0
+												),
+								'content_type'=>'css',
+							),
+
+			),
+		'front_template'=> $output,
+		'front_css'=> $css,
+	);
+
+?>
