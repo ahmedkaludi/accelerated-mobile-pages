8f32b8b95d17d87f70c1f7a152bde45d61690089
Raju Jeelaga <rajujeelaga@gmail.com>
2017-12-21 19:21:04 +0530

    feature module repeater field is added for testing
    feature module repeater field is added for testing

--- pagebuilder/modules/feature-mod.php
+++ pagebuilder/modules/feature-mod.php
@@ -179,6 +179,127 @@ return array(
 			),
 		'front_template'=> $output,
 		'front_css'=> $css,
+
+		'repeater'=>array(
+          'tab'=>'customizer',
+          'fields' => array(
+						array(		
+		 						'type'		=>'text',		
+		 						'name'		=>"content_title",		
+		 						'label'		=>'Heading',
+		           				'tab'       =>'customizer',
+		 						'default'	=>'Title',	
+		           				'content_type'=>'html',
+	 						),
+						array(
+								'type'		=>'color-picker',
+								'name'		=>"font_color_picker",
+								'label'		=>'Color',
+								'tab'		=>'customizer',
+								'default'	=>'#333',
+								'content_type'=>'css'
+							),
+	 					array(		
+		 						'type'		=>'upload',		
+		 						'name'		=>"img_upload",		
+		 						'label'		=>'Image Upload',
+		           				 'tab'     =>'customizer',
+		 						'default'	=>'',	
+		           				'content_type'=>'html',
+	 					),
+	 					array(
+								'type'		=>'checkbox',
+								'name'		=>"image_layout",
+								'label'		=>'Enable for Responsive Image',
+								'tab'		=>'customizer',
+								'default'	=>array(),
+								'options'	=>array(
+												array(
+													'label'=>'Enable',
+													'value'=>'responsive',
+												),
+											),
+								'content_type'=>'html',
+							),
+	 					array(
+				               'type'  =>'text',
+				              'name'=>"image_height",
+				              'label'=>"Image height",
+				              'tab'  => "customizer",
+				              'default'=>'150',
+				              'content_type'=>'html',
+				            ),
+				        array(
+				               'type'  =>'text',
+				              'name'=>"image_width",
+				              'label'=>"Image width",
+				              'tab'  => "customizer",
+				              'default'=>'150',
+				              'content_type'=>'html',
+				            ),
+	 					array(		
+	 							'type'	=>'select',		
+	 							'name'  =>'align_type',		
+	 							'label' =>"Alignment",
+								'tab'     =>'customizer',
+	 							'default' =>'center',
+	 							'options_details'=>array(
+	 												'center'    =>'Center',
+	 												'left'  	=>'Left',
+	 												'right'    =>'Right', 													),
+	 							'content_type'=>'css',
+	 						),
+	 					array(		
+		 						'type'		=>'textarea',		
+		 						'name'		=>"content",		
+		 						'label'		=>'Content',
+		           				 'tab'     =>'customizer',
+		 						'default'	=>'',	
+		           				'content_type'=>'html',
+	 					),
+	 					array(		
+	 							'type'	=>'select',		
+	 							'name'  =>'align_type',		
+	 							'label' =>"Alignment",
+								'tab'     =>'customizer',
+	 							'default' =>'center',
+	 							'options_details'=>array(
+	 												'center'    =>'Center',
+	 												'left'  	=>'Left',
+	 												'right'    =>'Right', 													),
+	 							'content_type'=>'css',
+	 						),	
+						
+						array(
+								'type'		=>'spacing',
+								'name'		=>"margin_css",
+								'label'		=>'Margin',
+								'tab'		=>'customizer',
+								'default'	=>array(
+													'left'=>0,
+													'right'=>0,
+													'top'=>0,
+													'bottom'=>0
+													),
+								'content_type'=>'css',
+							),
+							array(
+								'type'		=>'spacing',
+								'name'		=>"padding_css",
+								'label'		=>'Padding',
+								'tab'		=>'customizer',
+								'default'	=>array(
+													'left'=>0,
+													'right'=>0,
+													'top'=>0,
+													'bottom'=>0
+												),
+								'content_type'=>'css',
+							),
+
+			),
+          'front_template'=>''
+          ),
 	);
 
 ?>
