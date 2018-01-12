d2bba95428ce3ada9385f2b837845e50f3558f01
Mohammed Kaludi <mohammed@magazine3.com>
2018-01-12 21:28:21 +0530

    #1586 isset added to fix debug warnings
    #1586 isset added to fix debug warnings

--- templates/features.php
+++ templates/features.php
@@ -5028,11 +5028,11 @@ add_action('pre_amp_render_post', 'ampforwp_onesignal_notifications_widget_posit
 if( ! function_exists( 'ampforwp_onesignal_notifications_widget_position' ) ){
 	function ampforwp_onesignal_notifications_widget_position(){
 		global $redux_builder_amp; 
-		if( true == $redux_builder_amp['ampforwp-web-push-onesignal-below-content'] ){
+		if( isset( $redux_builder_amp['ampforwp-web-push-onesignal-below-content'] ) && true == $redux_builder_amp['ampforwp-web-push-onesignal-below-content'] ){
 			add_action('ampforwp_after_post_content', 'ampforwp_onesignal_notifications_widget');
 		}
 
-		if( true == $redux_builder_amp['ampforwp-web-push-onesignal-above-content'] ){
+		if( isset( $redux_builder_amp['ampforwp-web-push-onesignal-above-content'] ) &&  true == $redux_builder_amp['ampforwp-web-push-onesignal-above-content'] ){
 			add_action('ampforwp_inside_post_content_before', 'ampforwp_onesignal_notifications_widget');
 			add_action('ampforwp_before_post_content', 'ampforwp_onesignal_notifications_widget');
 		}
