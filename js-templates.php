c94dabee23f091130492a914db3558caf9d586ee
Raju Jeelaga <rajujeelaga@gmail.com>
2018-01-20 18:19:45 +0530

    proper json file is added in the layouts
    proper json file is added in the layouts

---
+++ js-templates.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ *
+ *
+ * Module Template
+ *
+ *
+ *
+ *
+ */
+global $moduleTemplate;
+include_once AMP_PAGE_BUILDER . '/components/modulesHtmlTemplate.php';
+include_once AMP_PAGE_BUILDER . '/components/fieldsTemplate.php';
+include_once AMP_PAGE_BUILDER . '/components/modulePopupTemplate.php';
+include_once AMP_PAGE_BUILDER . '/components/pbSettingTemplates.php';
+?>
+
+
