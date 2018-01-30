553cba6758dc2f3e242295e0825aa6b2f4d0d034
Ajeet kumar <ajeet.gupta51@gmail.com>
2018-01-30 19:40:06 +0530

    Changes for module

--- pagebuilder/config/moduleTemplate.php
+++ pagebuilder/config/moduleTemplate.php
@@ -4,8 +4,8 @@ $dir = AMP_PAGE_BUILDER.'/modules/';
 if (is_dir($dir)) {
     if ($dh = opendir($dir)) {
         while (($file = readdir($dh)) !== false) {
-        	if(is_file($dir.$file)){
-        		$moduleTemplate[str_replace(".php", "", $file)] = include $dir.$file;
+        	if(is_file($dir.$file) && strpos($file, '-module.php') == true){
+        		$moduleTemplate[str_replace("-module.php", "", $file)] = include $dir.$file;
         	}
         }
         closedir($dh);
