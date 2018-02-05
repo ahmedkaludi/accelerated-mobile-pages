c2ac4b091f7df0910cf7c2c4c268b5dd784dfdb1
Raju Jeelaga <rajujeelaga@gmail.com>
2018-02-05 13:24:58 +0530

    author meta and post date functions are changed
    author meta and post date functions are changed

--- components/components-core.php
+++ components/components-core.php
@@ -120,7 +120,7 @@ function amp_excerpt($no_of_words=15){
 	}
 	else
 		$ID = $post->ID;
-	if( $ID != null ){  ?>
+	if( $ID != null && true == $redux_builder_amp['enable-excerpt-single'] ){  ?>
 			<p><?php 
 				 if( has_excerpt() ){
 					$content = get_the_excerpt();
@@ -377,22 +377,27 @@ $thisTemplate = new AMP_Post_Template($post_id);
     	do_action('ampforwp_after_post_content',$thisTemplate); ?>
 <?php }
 
-function amp_date($args=array()){
-	global $redux_builder_amp;
-	if( 2 == $redux_builder_amp['ampforwp-post-date-format'] ){
-		$args['format'] = 'traditional';
-	}
-	if(isset($args['format']) && $args['format']=='traditional'){
-		$post_date = esc_html( get_the_date() ) . ' '.esc_html( get_the_time());
-    }else{
-    	$post_date =  human_time_diff(
-    						get_the_time('U', get_the_ID() ), 
-    						current_time('timestamp') ) .' '. ampforwp_translation( $redux_builder_amp['amp-translator-ago-date-text'],
-    						'ago');
+function amp_date( $args=array() ) {
+
+    global $redux_builder_amp;
+    if ( 2 == $redux_builder_amp['ampforwp-post-date-format'] ) {
+    	$args['format'] = 'traditional';
     }
-    $post_date = apply_filters('ampforwp_modify_post_date', $post_date);
-    echo '<div class="loop-date">'.$post_date.'</div>';
-}
+    if ( (isset($args['format']) && $args['format'] == 'traditional') || 'time' == $args ) {
+      $post_date = esc_html( get_the_date() ) . ' '.esc_html( get_the_time());
+        } else {
+          $post_date = human_time_diff(
+                    get_the_time('U', get_the_ID() ), 
+                    current_time('timestamp') ) .' '. ampforwp_translation( $redux_builder_amp['amp-translator-ago-date-text'],
+                    'ago');
+        }
+        $post_date = apply_filters('ampforwp_modify_post_date', $post_date);
+        if ( 'date' == $args || 'time' == $args ) {
+          echo $post_date .' ';
+        }
+        else
+          echo '<div class="loop-date">'.$post_date.'</div>';
+  }
 
 //Load font Compoment
 $fontComponent = array();
@@ -412,4 +417,46 @@ add_action( 'amp_meta', 'amp_post_load_custom_fonts');
 function amp_font($fontName){
 	global $fontComponent;
 	$fontComponent[] = $fontName;
+}
+
+
+
+// Author Meta
+function amp_author_meta( $args ) {
+  global $post;
+  $author_name = false;
+  $avatar = false;
+  $avatar_size = 40;
+  if ( isset($args['name']) ) {
+    $author_name = $args['name'];
+  }
+  if ( 'name' === $args ) {
+    $author_name = true;
+  }
+  if ( 'avatar' === $args || 'image' === $args ) {
+    $avatar = true;
+  }
+  if ( isset($args['image']) ) {
+    $avatar = $args['image'];
+  }
+  if ( isset($args['image_size']) ) {
+    $avatar_size = $args['image_size'];
+  }
+  $post_author = get_userdata($post->post_author);
+  $author_link = get_author_posts_url($post_author->ID);
+  if ( $author_name ) {
+    echo ' <a href="'. esc_url(ampforwp_url_controller($author_link)).'"> ' .esc_html( $post_author->display_name ).'</a>';
+   }
+   if ( $avatar && true == ampforwp_gravatar_checker($post_author->user_email) ) {
+    $author_avatar_url = get_avatar_url( $post_author->ID, array( 'size' => $avatar_size ) );
+            ?>
+        <amp-img src="<?php echo esc_url($author_avatar_url); ?>" width="<?php echo $avatar_size; ?>" height="<?php echo $avatar_size; ?>" layout="fixed"></amp-img> 
+    <?php }
+    elseif ( $avatar && false == ampforwp_gravatar_checker($post_author->user_email ) ) {
+      $avatar_img = get_avatar( $post_author->user_email, $avatar_size );
+      $amp_html_sanitizer = new AMPFORWP_Content( $avatar_img, array(), apply_filters( 'ampforwp_content_sanitizers', array( 'AMP_Img_Sanitizer' => array() ) ) );
+      $amp_html =  $amp_html_sanitizer->get_amp_content();
+    echo $amp_html;
+     } 
+   
 }
