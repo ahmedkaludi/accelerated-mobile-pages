983bb32455efb24656f77c9a183c3a4e79bb385a
Raju Jeelaga <rajujeelaga@gmail.com>
2018-02-07 20:27:15 +0530

    recent post desing is added in the design-2 sidebar in single page
    recent post desing is added in the design-2 sidebar in single page

--- templates/design-manager/swift/single.php
+++ templates/design-manager/swift/single.php
@@ -135,7 +135,29 @@
 					</div>
 				</div>
 				<div class="rft">
-
+					<div class="ads"></div>
+					<div class="rc-p">
+						<h3><?php echo ampforwp_translation($redux_builder_amp['amp-translator-recent-text'], 'Recent Posts' ); ?></h3>
+							<?php while( amp_loop('start', array( 'posts_per_page' => 6 ) ) ): ?>
+								<div class="rp">
+									<?php
+									$width 	= 65;
+									$height = 65;
+									if( true == $redux_builder_amp['ampforwp-homepage-posts-image-modify-size'] ){
+										$width 	= $redux_builder_amp['ampforwp-swift-homepage-posts-width'];
+										$height = $redux_builder_amp['ampforwp-swift-homepage-posts-height'];
+									}
+									 $args = array("tag"=>'div',"tag_class"=>'image-container','image_size'=>'full','image_crop'=>'true','image_crop_width'=>$width,'image_crop_height'=>$height, 'responsive'=> true); ?>
+								    <div class="rp-img">
+								    	<?php amp_loop_image($args); ?>
+								    </div>
+								    <div class="rp-cnt">
+									    <?php amp_loop_title(); ?>
+								    </div>
+								</div>
+							<?php endwhile; amp_loop('end'); ?>
+						<?php wp_reset_postdata(); ?>
+					</div>
 				</div>
  			</div>
  		</div>
