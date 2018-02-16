6dbcf03c2c0b58a47e27eefbd7dd2bfc130cfb0f
Marqas <marqas36@gmail.com>
2018-02-16 16:31:22 +0530

    #1701 To avoid errors in ATF

--- templates/features.php
+++ templates/features.php
@@ -5889,15 +5889,17 @@ function get_preview_post_link( $post = null, $query_args = array(), $preview_li
 add_filter('ampforwp_query_args','ampforwp_homepage_loop');
 function ampforwp_homepage_loop( $args ) {
 	global $redux_builder_amp;
-	$post_type = 'post';
-	// Check if Custom Post Type is selected
-	if ( isset($redux_builder_amp['ampforwp-homepage-loop-type']) && '' != $redux_builder_amp['ampforwp-homepage-loop-type'] ) {
-		$post_type = $redux_builder_amp['ampforwp-homepage-loop-type'];
-	}
-	$args['post_type'] = $post_type;
-	// Exclude Categories if any selected
-	if ( isset($redux_builder_amp['ampforwp-homepage-loop-cats']) && !empty($redux_builder_amp['ampforwp-homepage-loop-cats']) ) {
-		$args['category__not_in'] = $redux_builder_amp['ampforwp-homepage-loop-cats'];
+	if ( is_home() ) {
+		$post_type = 'post';
+		// Check if Custom Post Type is selected
+		if ( isset($redux_builder_amp['ampforwp-homepage-loop-type']) && '' != $redux_builder_amp['ampforwp-homepage-loop-type'] ) {
+			$post_type = $redux_builder_amp['ampforwp-homepage-loop-type'];
+		}
+		$args['post_type'] = $post_type;
+		// Exclude Categories if any selected
+		if ( isset($redux_builder_amp['ampforwp-homepage-loop-cats']) && ! empty($redux_builder_amp['ampforwp-homepage-loop-cats']) ) {
+			$args['category__not_in'] = $redux_builder_amp['ampforwp-homepage-loop-cats'];
+		}
 	}
 	return $args; 
 }
