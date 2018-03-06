11bc889fb99b876b82b38915f42967e94328a778
Ajeet kumar <ajeet.gupta51@gmail.com>
2018-03-06 20:54:27 +0530

    [Travis] version check

--- templates/features.php
+++ templates/features.php
@@ -4744,7 +4744,7 @@ function ampforwp_generate_inline_related_posts($content){
 	$content_parts = explode($break_point, $content);
 	array_walk($content_parts, function(&$value, $key) {
 		 	$value = trim($value);
-			if(!empty($value)){
+			if(!empty(trim($value))){
 			         $value .= '</p>';
 			}
 		}
