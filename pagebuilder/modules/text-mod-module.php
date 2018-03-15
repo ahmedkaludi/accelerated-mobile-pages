96a88e782399c136de1cecbd3ade47dd65ac0493
Raju Jeelaga <rajujeelaga@gmail.com>
2018-03-15 14:21:40 +0530

    Text mod max-width option is added
    Text mod max-width option is added

--- pagebuilder/modules/text-mod-module.php
+++ pagebuilder/modules/text-mod-module.php
@@ -3,7 +3,7 @@ $output = '
 	{{content_title}}
 ';
 $css = '
-{{module-class}}.text-mod{width:100%;text-align:{{align_type}};margin:{{margin_css}};padding:{{padding_css}};font-size:{{font-size}};color:{{text_color_picker}};}
+{{module-class}}.text-mod{width:100%;max-width:{{max-width}};text-align:{{align_type}};margin:{{margin_css}};padding:{{padding_css}};font-size:{{font-size}};color:{{text_color_picker}};}
 ';
 return array(
 		'label' =>'Text',
@@ -51,7 +51,15 @@ return array(
 	 												'left'  	=>'Left',
 	 												'right'    =>'Right', 													),
 	 							'content_type'=>'css',
-	 						),	
+	 						),
+	 					array(
+								'type'		=>'text',
+								'name'		=>"max-width",
+								'label'		=>'Max Width',
+								'tab'		=>'design',
+								'default'	=>'100%',
+								'content_type'=>'css'
+							),	
 						array(
 								'type'		=>'spacing',
 								'name'		=>"margin_css",
