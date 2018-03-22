24ca482344c7e68e11d7afc5640d054044f090cb
Raju Jeelaga <rajujeelaga@gmail.com>
2018-03-22 19:50:40 +0530

    Carousel Module is added and option are Connected
    Carousel Module is added and option are Connected

---
+++ pagebuilder/modules/carousel-mod-module.php
@@ -0,0 +1,118 @@
+<?php 
+$output = '
+<amp-carousel width="400" height="300" layout="responsive" type="slides" autoplay delay="2000">
+	{{repeater}}
+</amp-carousel>
+';
+$css = '
+.amp-img{
+	width:100%;
+	height:auto;
+}
+{{module-class}}{text-align:{{align_type}};margin:{{margin_css}};padding:{{padding_css}};width:{{width}}}
+';
+
+return array(
+		'label' =>'Carousel',
+		'name' =>'carousel-mod',
+		'default_tab'=> 'customizer',
+		'tabs' => array(
+              'customizer'=>'Content',
+              'design'=>'Design',
+              'advanced' => 'Advanced'
+            ),
+		'fields' => array(
+                        array(		
+	 						'type'		=>'text',		
+	 						'name'		=>"width",		
+	 						'label'		=>'Image Size',
+	           				 'tab'      =>'customizer',
+	 						'default'	=>'90%',	
+	           				'content_type'=>'css',
+ 						),
+				        array(
+								'type'		=>'checkbox',
+								'name'		=>"image_layout",
+								'tab'		=>'customizer',
+								'default'	=>array('responsive'),
+								'options'	=>array(
+												array(
+													'label'=>'Responsive',
+													'value'=>'responsive',
+												),
+											),
+								'content_type'=>'html',
+							),
+
+	 					array(		
+	 							'type'	=>'select',		
+	 							'name'  =>'align_type',		
+	 							'label' =>"Align",
+								'tab'     =>'design',
+	 							'default' =>'center',
+	 							'options_details'=>array(
+	 												'center'    =>'Center',
+	 												'left'  	=>'Left',
+	 												'right'    =>'Right', 													),
+	 							'content_type'=>'css',
+	 						),	
+						array(
+								'type'		=>'spacing',
+								'name'		=>"margin_css",
+								'label'		=>'Margin',
+								'tab'		=>'advanced',
+								'default'	=>
+                            array(
+                                'top'=>'20px',
+                                'right'=>'auto',
+                                'bottom'=>'20px',
+                                'left'=>'auto',
+                            ),
+								'content_type'=>'css',
+							),
+							array(
+								'type'		=>'spacing',
+								'name'		=>"padding_css",
+								'label'		=>'Padding',
+								'tab'		=>'advanced',
+								'default'	=>array(
+													'left'=>'0px',
+													'right'=>'0px',
+													'top'=>'0px',
+													'bottom'=>'0px'
+												),
+								'content_type'=>'css',
+							),
+							array(		
+	 						'type'		=>'require_script',		
+	 						'name'		=>"carousel_script",		
+	 						'label'		=>'amp-carousel',
+	 						'default'	=>'https://cdn.ampproject.org/v0/amp-carousel-0.1.js',	
+	           				'content_type'=>'js',
+ 						),
+
+			),
+		'front_template'=> $output,
+		'front_css'=> $css,
+		'front_common_css'=>'',
+		'repeater'=>array(
+	          'tab'=>'customizer',
+	          'fields'=>array(
+			                array(		
+		 						'type'		=>'upload',		
+		 						'name'		=>"img_upload",		
+		 						'label'		=>'Upload',
+		           				'tab'     =>'customizer',
+		 						'default'	=>'',	
+		           				'content_type'=>'html',
+	 						),
+							
+	                
+	              ),
+	          'front_template'=>
+	        	'
+					<amp-img src="{{img_upload}}" width="{{image_width}}" height="{{image_height}}" {{if_image_layout}}layout="{{image_layout}}"{{ifend_image_layout}}></amp-img>
+				'
+	          ),
+	);
+?>
