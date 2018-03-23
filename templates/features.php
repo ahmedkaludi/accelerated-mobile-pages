1b46e4ab7311b7d6c9b58af58fa10fb840836770
Marqas <marqas36@gmail.com>
2018-03-23 18:43:23 +0530

    #1916 No View Non-AMP for AMP Takeover

--- templates/features.php
+++ templates/features.php
@@ -3881,6 +3881,9 @@ function ampforwp_view_nonamp(){
   }
    if( true == $redux_builder_amp['ampforwp-nofollow-view-nonamp'] ){
    		$nofollow = 'rel="nofollow"';
+   }
+   if ( isset($redux_builder_amp['ampforwp-amp-takeover']) && $redux_builder_amp['ampforwp-amp-takeover'] ) {
+   	$ampforwp_backto_nonamp = '';
    } 
    if ( $ampforwp_backto_nonamp ) { ?> <a class="view-non-amp" href="<?php echo $ampforwp_backto_nonamp; ?>" <?php echo $nofollow; ?>><?php echo esc_html( $redux_builder_amp['amp-translator-non-amp-page-text'] ) ;?> </a> <?php  }
  }
