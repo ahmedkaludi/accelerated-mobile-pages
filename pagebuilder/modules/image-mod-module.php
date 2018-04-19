5ad09aa0add9481f884ca891c5220126582da14c
Marqas <marqas36@gmail.com>
2018-04-19 16:11:02 +0530

    #1989 Condition witth default value for link
    Added proper condition for hyperlink checkbox and also a default value for the link

--- pagebuilder/modules/image-mod-module.php
+++ pagebuilder/modules/image-mod-module.php
@@ -1,11 +1,10 @@
 <?php 
-$output = '{{if_img_upload}}<amp-img src="{{img_upload}}" width="{{image_width}}" height="{{image_height}}" {{if_image_layout}}layout="{{image_layout}}"{{ifend_image_layout}}  alt="{{image_alt}}"></amp-img>{{ifend_img_upload}}';
-
 $output = '
-	{{if_hyperlink_link_img}}<a href="{{hyperlink_link_img}}" {{if_condition_img_link_open==new_page}}target="_blank"{{ifend_condition_img_link_open_new_page}}>{{ifend_hyperlink_link_img}}'.$output;
+	{{if_condition_img_hyperlink==1}}<a href="{{hyperlink_link_img}}" {{if_condition_img_link_open==new_page}}target="_blank"{{ifend_condition_img_link_open_new_page}}>{{ifend_condition_img_hyperlink_1}}
+		{{if_img_upload}}<amp-img src="{{img_upload}}" width="{{image_width}}" height="{{image_height}}" {{if_image_layout}}layout="{{image_layout}}"{{ifend_image_layout}}  alt="{{image_alt}}"></amp-img>{{ifend_img_upload}}
+	{{if_condition_img_hyperlink==1}}</a>{{ifend_condition_img_hyperlink_1}}';
 
-// $output .= '{{if_hyperlink_link_img}}</a>{{ifend_hyperlink_link_img}}';
-
+// $output .= '{{if_img_hyperlink}}<a href="{{hyperlink_link_img}}">"'.$output.'</a>{{ifend_img_hyperlink}}';
 $css = '
 {{module-class}}{text-align:{{align_type}};margin:{{margin_css}};padding:{{padding_css}};width:{{width}}}
 {{if_condition_check_for_fullwidth==1}}
@@ -45,12 +44,28 @@ return array(
 	 						),
 
 	 					array(		
+		 						'type'		=>'checkbox_bool',		
+		 						'name'		=>"img_hyperlink",		
+		 						'label'		=>'Hyperlink',
+		           				'tab'     	=>'customizer',
+		 						'default'	=>0,
+		 						'content_type'=>'html',	
+		           				'options'	=>array(
+												array(
+													'label'=>'Make an Hyperlink Image',
+													'value'=>1,
+												),
+											),
+	 						),
+
+	 					array(		
 		 						'type'		=>'text',		
 		 						'name'		=>"hyperlink_link_img",		
-		 						'label'		=>'Hyperlink URL',
+		 						'label'		=>'URL',
 		           				 'tab'     =>'customizer',
-		 						'default'	=>'',	
+		 						'default'	=>'#',	
 		           				'content_type'=>'html',
+		           				'required'  => array('img_hyperlink'=>'1'),
 	 						),
 	 					array(		
 	 							'type'	=>'select',		
@@ -63,6 +78,7 @@ return array(
 	 												'same_page'    =>'Same page'
 	 											),
 	 							'content_type'=>'html',
+	 							'required'  => array('img_hyperlink'=>'1'),
 	 						),
 
 				        array(
