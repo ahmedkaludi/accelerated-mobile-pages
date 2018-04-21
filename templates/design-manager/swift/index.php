cf69359a53f7f3fb5a68f7a18045cbd97c3dfb23
Sejiro-Hiko <mohdasif7026@gmail.com>
2018-04-21 12:16:19 +0530

    #2033 [Fixed]

--- templates/design-manager/swift/index.php
+++ templates/design-manager/swift/index.php
@@ -19,10 +19,10 @@
 		if($i==1 && $paged==1){ 
 			?>
 			<div class="fbp">
-				<?php $argsbig = array("tag"=>'div',"tag_class"=>'image-container','image_size'=>'full','image_crop'=>'true','image_crop_width'=>723,'image_crop_height'=>394, 'responsive'=> true); ?>
+				<?php $argsbig = array("tag"=>'div',"tag_class"=>'image-container','image_size'=>'full','image_crop'=>'true','image_crop_width'=>723,'image_crop_height'=>394, 'responsive'=> true);if (ampforwp_has_post_thumbnail()  ) { ?>
 			    <div class="fbp-img">
 			    	<?php amp_loop_image($argsbig); ?>
-			    </div>
+			    </div> <?php } ?>
 			    <div class="fbp-cnt">
 			    	<?php amp_loop_category(); ?>
 				    <?php amp_loop_title(); ?>
