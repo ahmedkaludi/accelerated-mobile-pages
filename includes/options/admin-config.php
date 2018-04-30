a7c9fd6d548d7b803658a240de21235d05af0942
Marqas <marqas36@gmail.com>
2018-04-30 15:38:49 +0530

    #1941 More strings translatable

--- includes/options/admin-config.php
+++ includes/options/admin-config.php
@@ -2769,6 +2769,30 @@ Redux::setSection( $opt_name, array(
                        'required' => array( 'amp-use-pot', '=' , 0 )
                    ),
                    array(
+                       'id'       => 'amp-translator-archives-text',
+                       'type'     => 'text',
+                       'title'    => __('Page', 'accelerated-mobile-pages'),
+                       'default'  => __('Page','accelerated-mobile-pages'),
+                       'placeholder'=>__('write here','accelerated-mobile-pages'),
+                       'required' => array( 'amp-use-pot', '=' , 0 )
+                   ),
+                   array(
+                       'id'       => 'amp-translator-breadcrumbs-search-text',
+                       'type'     => 'text',
+                       'title'    => __('Page', 'accelerated-mobile-pages'),
+                       'default'  => __('Page','accelerated-mobile-pages'),
+                       'placeholder'=>__('write here','accelerated-mobile-pages'),
+                       'required' => array( 'amp-use-pot', '=' , 0 )
+                   ),
+                   array(
+                       'id'       => 'amp-translator-error-404-text',
+                       'type'     => 'text',
+                       'title'    => __('Page', 'accelerated-mobile-pages'),
+                       'default'  => __('Page','accelerated-mobile-pages'),
+                       'placeholder'=>__('write here','accelerated-mobile-pages'),
+                       'required' => array( 'amp-use-pot', '=' , 0 )
+                   ),
+                   array(
                        'id'       => 'amp-translator-footer-text',
                        'type'     => 'textarea',
                        'title'    => __('Footer', 'accelerated-mobile-pages'),
