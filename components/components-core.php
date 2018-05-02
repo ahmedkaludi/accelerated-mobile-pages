d1b21bde140f8fdd1fbd41b89004d62287aa75f6
Marqas <marqas36@gmail.com>
2018-05-02 17:07:58 +0530

    Footer text inside span

--- components/components-core.php
+++ components/components-core.php
@@ -381,7 +381,7 @@ function amp_footer_core(){
 function amp_non_amp_link(){
 	global $allowed_html;
     global $redux_builder_amp;
-    echo wp_kses($redux_builder_amp['amp-translator-footer-text'],$allowed_html) ;
+    echo '<span>' . wp_kses($redux_builder_amp['amp-translator-footer-text'],$allowed_html) . '</span>' ;
     if($redux_builder_amp['amp-footer-link-non-amp-page']=='1') { ampforwp_view_nonamp(); }
 }
 
