b9b35eab4440d71344b5becb4b33ef3a26ab2fc6
Raju <rajujeelaga@gmail.com>
2018-05-18 11:01:25 +0530

    #2098 Row Setting background image not appearing is Resolved
    #2098 Row Setting background image not appearing is Resolved

--- pagebuilder/inc/admin-amp-page-builder.js
+++ pagebuilder/inc/admin-amp-page-builder.js
@@ -522,6 +522,7 @@ Vue.component('fields-data',{
 							 	Vue.set(field,'default',imageList);
 							}else{*/
 								if(type=='tag'){
+									field[field['name']+'_image_data'] = response.data.front_image;
 									jQuery(currentSelectfield.$el).find('img').attr('src',response.data.detail[0]);
 								}else{
 									//console.log(jQuery(currentSelectfield).parents('p'))
