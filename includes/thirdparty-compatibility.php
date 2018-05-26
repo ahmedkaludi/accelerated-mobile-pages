4bd585d7e524a3e2f469f0c60d5da7e017bed8db
Sejiro-Hiko <mohdasif7026@gmail.com>
2018-05-26 12:51:09 +0530

    #1503  removed wrong conditions.
    TODO: make sure it fires only on AMP Environment

--- includes/thirdparty-compatibility.php
+++ includes/thirdparty-compatibility.php
@@ -326,8 +326,8 @@ function ampforwp_add_upcomminglayouts($layoutTemplate){
 
 add_action('amp_init', 'ampforwp_remove_relevanssi_search_takeover');
 function ampforwp_remove_relevanssi_search_takeover(){
-	global $redux_builder_amp;
-	if( function_exists(start_non_amp_to_amp_conversion()) && start_non_amp_to_amp_conversion() || amp_get_slug()){
+	
 	remove_filter( 'the_posts', 'relevanssi_query', 99, 2 );
-	remove_filter( 'posts_request', 'relevanssi_prevent_default_request', 10, 2 );}
+	remove_filter( 'posts_request', 'relevanssi_prevent_default_request', 10, 2 );
+
 }
