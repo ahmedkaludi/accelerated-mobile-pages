23d534eaa8390ba1c42ca715017d4488a51dfdba
Ajeet kumar <ajeet.gupta51@gmail.com>
2018-06-04 12:51:58 +0530

    #2145 0.9.96 Bulk hide by default option is requires Permalinks to be saved.

--- includes/thirdparty-compatibility.php
+++ includes/thirdparty-compatibility.php
@@ -362,3 +362,13 @@ function ampforwp_is_amp_inURL($url){
   }
   return true;
 }
+
+if(is_admin()){
+	add_action( 'redux/options/redux_builder_amp/saved', 'ampForWP_extension_individual_amp_page');
+	function ampForWP_extension_individual_amp_page(){
+		$selectedOption = get_option('redux_builder_amp',true);
+		if($selectedOption['amp-pages-meta-default']=='hide'){
+			delete_transient('ampforwp_current_version_check');
+		}
+	}
+}
