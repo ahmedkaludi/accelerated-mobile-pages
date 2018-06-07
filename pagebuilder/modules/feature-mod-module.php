34004a27183a3e9fe3745589f8ea2c29df98bf37
Raju <rajujeelaga@gmail.com>
2018-06-07 12:26:14 +0530

    #2068 Button field is created in the Feature Mod and connected to options
    #2068 Button field is created in the Feature Mod and connected to
    options

--- pagebuilder/modules/feature-mod-module.php
+++ pagebuilder/modules/feature-mod-module.php
@@ -10,6 +10,18 @@ $css = '
 .feat-blk h3{font-size:28px;color:{{font_color_picker}};font-weight:400;padding-bottom:15px;}
 .feat-blk amp-img{margin:0 auto;width:100%}
 .feat-blk amp-img{width:{{img_width}};}
+{{if_condition_check_for_btn==1}}
+.feat-blk .fe_btn{
+	font-size:{{btn_size}};
+	font-weight:{{btn_weight}};
+	color:{{btn_color}};
+	background:{{btn_bg_color}};
+	border-radius:{{bdr_rds}};
+	padding: 10px 20px;
+    margin-top: 15px;
+    display: inline-block;
+}
+{{ifend_condition_check_for_btn_1}}
 @media(max-width:768px){
     .feat-blk{width: 100%;margin-right: 0;}
 }
@@ -68,7 +80,53 @@ return array(
 		           				'content_type'=>'css',
 		           				'required'  => array('feature_layout_type'=> 1)
 	 						),
-
+				         array(
+				                'type'    =>'checkbox_bool',
+				                'name'    =>"check_for_btn",
+				                'label'   => 'Enable Button',
+				                'tab'   =>'customizer',
+				                'default' =>0,
+				                'options' =>array(
+						                        array(
+						                          'label'=>'Yes',
+						                          'value'=>1,
+						                        )
+				                      		),
+				                'content_type'=>'html',
+				                'required'  => array('feature_layout_type'=> 1)
+				            ),
+	 					array(		
+		 						'type'		=>'text',		
+		 						'name'		=>"btn_txt",		
+		 						'label'		=>'Button Text',
+		           				 'tab'     =>'customizer',
+		 						'default'	=>'Learn More',	
+		           				'content_type'=>'html',
+		           				'required'  =>  array('feature_layout_type'=>'1', 'check_for_btn'=>'1'),
+		           								
+	 						),
+	 					array(		
+		 						'type'		=>'text',		
+		 						'name'		=>"btn_link",		
+		 						'label'		=>'URL',
+		           				'tab'     =>'customizer',
+		 						'default'	=>'#',	
+		           				'content_type'=>'html',
+		           				'required'  =>  array('feature_layout_type'=>'1', 'check_for_btn'=>'1'),
+	 						),
+	 					array(		
+	 							'type'	=>'select',		
+	 							'name'  =>'page_link_open',		
+	 							'label' =>"Open link in",
+								'tab'     =>'customizer',
+	 							'default' =>'new_page',
+	 							'options_details'=>array(
+	 												'new_page'  	=>'New tab',
+	 												'same_page'    =>'Same page'
+	 											),
+	 							'content_type'=>'html',
+	 							'required'  =>  array('feature_layout_type'=>'1', 'check_for_btn'=>'1'),
+	 						),
 						array(
 								'type'		=>'color-picker',
 								'name'		=>"background_color_picker",
@@ -100,7 +158,58 @@ return array(
 	 							'content_type'=>'css',
 	 							'required'  => array('feature_layout_type'=> 1)
 	 						),
-					
+	 					array(		
+		 						'type'		=>'text',		
+		 						'name'		=>"btn_size",		
+		 						'label'		=>'Button Font Size',
+		           				 'tab'     =>'design',
+		 						'default'	=>'16px',	
+		           				'content_type'=>'css',
+		           				'required'  => array('feature_layout_type'=> 1)
+	 						),
+	 					array(		
+	 							'type'	=>'select',		
+	 							'name'  =>'btn_weight',		
+	 							'label' =>"Button Font Weight",
+								'tab'     =>'design',
+	 							'default' =>'400',
+	 							'options_details'=>array(
+                                    '300'   =>'Light',
+                                    '400'  	=>'Regular',
+                                    '500'  	=>'Medium',
+                                    '600'  	=>'Semi Bold',
+                                    '700'  	=>'Bold',
+                                ),
+	 							'content_type'=>'css',
+	 							'required'  => array('feature_layout_type'=> 1)
+	 						),
+						array(
+								'type'		=>'color-picker',
+								'name'		=>"btn_color",
+								'label'		=>'Button Text Color',
+								'tab'		=>'design',
+								'default'	=>'#fff',
+								'content_type'=>'css',
+								'required'  => array('feature_layout_type'=> 1)
+							),
+						array(
+								'type'		=>'color-picker',
+								'name'		=>"btn_bg_color",
+								'label'		=>'Background Color',
+								'tab'		=>'design',
+								'default'	=>'#555',
+								'content_type'=>'css',
+								'required'  => array('feature_layout_type'=> 1)
+							),
+						array(		
+		 						'type'		=>'text',		
+		 						'name'		=>'bdr_rds',		
+		 						'label'		=>'Border Radius',
+		           				 'tab'     =>'design',
+		 						'default'	=>'0px',	
+		           				'content_type'=>'css',
+		           				'required'  => array('feature_layout_type'=> 1)
+	 						),
 						array(
 								'type'		=>'spacing',
 								'name'		=>"margin_css",
@@ -165,13 +274,16 @@ return array(
 		           				'content_type'=>'html',
 		           				'required'  => array('feature_layout_type'=> 1)
 	 					),
-                
+	 					
               ),
           'front_template'=>
         '{{if_condition_feature_layout_type==1}}<div class="feat-blk">
       		<h3 class="t-txt">{{content_title}}</h3>
 			{{if_img_upload}}<amp-img src="{{img_upload}}" width="{{image_width}}" height="{{image_height}}" {{image_layout}} alt="{{image_alt}}"></amp-img>{{ifend_img_upload}}
 			<p>{{content}}</p>
+			{{if_condition_check_for_btn==1}}
+				<a href="{{btn_link}}" {{if_condition_page_link_open==new_page}}target="_blank"{{ifend_condition_page_link_open_new_page}} class="fe_btn">{{btn_txt}}</a>
+			{{ifend_condition_check_for_btn_1}}
       	</div> {{ifend_condition_feature_layout_type_1}}'
           ),
 
