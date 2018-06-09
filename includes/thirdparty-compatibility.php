c973dee37644f6deb84ac7d377895c048fd0a2b1
Ajeet kumar <ajeet.gupta51@gmail.com>
2018-06-09 17:14:28 +0530

    #2204 If link contain only # than return it
    In current situation its gives blank,
    
    Because of that a tag removed by sanitizer

--- includes/thirdparty-compatibility.php
+++ includes/thirdparty-compatibility.php
@@ -334,6 +334,7 @@ if(!function_exists('ampforwp_findInternalUrl')){
   function ampforwp_findInternalUrl($url){
     global $redux_builder_amp;
    
+    if($url=='#'){ return $url; }
     if(isset($redux_builder_amp['convert-internal-nonamplinks-to-amp']) && ! $redux_builder_amp['convert-internal-nonamplinks-to-amp']){
         return $url;
     }
