4af986515c5776572c377b629bbe4dd61ca8b216
Marqas <marqas36@gmail.com>
2018-06-20 16:23:56 +0530

    #1875 wistia support improvement

--- templates/features.php
+++ templates/features.php
@@ -1360,7 +1360,7 @@ function ampforwp_new_dir( $dir ) {
 				 $content = preg_replace('#<style scoped.*?>(.*?)</style>#i', '', $content); */
 				 $content = preg_replace('/href="javascript:void*/', ' ', $content);
 				 // Convert the Wistia embed into URL to build amp-wistia-player and remove unnecesarry wistia code
-				 $content = preg_replace('/<script src="(https?).*(\/\/fast|support)(\.wistia\.com\/)(embed\/medias\/)([0-9|\w]+)(.*?)<\/script>/', "$1:$2$3$4$5", $content);
+				 $content = preg_replace('/<script src="(https?).*(\/\/fast|support)(\.wistia\.com\/)(embed\/medias\/)([0-9|\w]+)(.*?)<\/script>/', "$1:$2$3$4$5\n", $content);
 				 $content = preg_replace('/<div class="wistia_responsive_padding" (.*?)>/', "", $content);
 				 $content = preg_replace('/<div class="wistia_responsive_wrapper" (.*?)>/', "", $content);
 				 $content = preg_replace('/<div class="wistia_embed (.*?)>/', "", $content);
