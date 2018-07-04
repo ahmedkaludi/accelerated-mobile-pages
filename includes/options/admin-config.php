b7682c91af64f38c151bd1374d4480c4bf399a1e
Marqas <marqas36@gmail.com>
2018-07-04 13:58:20 +0530

    #1486 Page ID meta tag
    and some code improvement for #1474

--- includes/options/admin-config.php
+++ includes/options/admin-config.php
@@ -3258,6 +3258,13 @@ Redux::setSection( $opt_name, array(
                         'title'    => __('Facebook Instant Articles Feed URL', 'accelerated-mobile-pages'),
                         'required'  => array('fb-instant-article-switch', '=', 1)
                     ),
+                    array(
+                        'id'    => 'fb-instant-page-id',
+                        'type'  => 'text',
+                        'title' => __('Facebook Page ID', 'accelerated-mobile-pages'),
+                        'desc' => __('Follow <a href="https://www.facebook.com/instant_articles/signup" target="_blank">these instructions</a> to sign up to Instant Articles and get your Facebook Page ID.', 'accelerated-mobile-pages'),
+                        'required'  => array('fb-instant-article-switch', '=', 1)
+                    ),
 
            array(
                         'required'  => array('fb-instant-article-switch', '=', 1),
@@ -3362,13 +3369,6 @@ Redux::setSection( $opt_name, array(
                         'tooltip-subtitle' => __('Add ia:markup meta tag. Find out more about<a href="https://developers.facebook.com/docs/instant-articles/crawler-ingestion" target="_blank">here</a> ', 'accelerated-mobile-pages'),
                         'required'  => array('fb-instant-article-switch', '=', 1)
                     ),
-                    array(
-                    	'id'	=> 'fb-instant-page-id',
-                    	'type'	=> 'text',
-                    	'title'	=> __('Facebook Page Id', 'accelerated-mobile-pages'),
-                    	'tooltip-subtitle' => __('Follow <a href="https://www.facebook.com/instant_articles/signup?__mref=facebook-instant-articles-wp" target="_blank">these instructions</a> to sign up to Instant Articles and get your Facebook Page ID.', 'accelerated-mobile-pages'),
-                    	'required'  => array('fb-instant-crawler-ingestion', '=', 1)
-                    )
     ),
    )
 );
