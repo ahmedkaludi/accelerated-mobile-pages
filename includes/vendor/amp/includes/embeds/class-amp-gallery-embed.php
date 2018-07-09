c7741db2c24b80f9891779395f5126644f9cddb1
Raju <rajujeelaga@gmail.com>
2018-07-09 16:19:32 +0530

    #1968 Unwanted css are Removed
    #1968 Unwanted css are Removed

--- includes/vendor/amp/includes/embeds/class-amp-gallery-embed.php
+++ includes/vendor/amp/includes/embeds/class-amp-gallery-embed.php
@@ -119,80 +119,18 @@ class AMP_Gallery_Embed_Handler extends AMP_Base_Embed_Handler {
 			'1'=>array(
 						'image-with-caption-html'=>'<figure><div class="ampforwp-gallery-item amp-carousel-container">{{main_images}} </div><figcaption :openbrack:class:closebrack:="expanded? \'expanded\' : \'\'" on="tap:AMP.setState({expanded: !expanded})" tabindex="0" role="button" >{{main_images_caption}}<span :openbrack:text:closebrack:="expanded ? \'less\' : \'more\'">more</span> </figcaption></figure>',
 						'image-without-caption-html' =>'<div class="ampforwp-gallery-item amp-carousel-container">{{main_images}} </div>',
-						'gallery_css' => '.collapsible-captions {--caption-height: 32px; --image-height: 100%; --caption-padding:1rem; --button-size: 28px; --caption-color: #f5f5f5;; --caption-bg-color: #111;}
-								    .collapsible-captions * {
-								      -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
-								      box-sizing: border-box;
-								    }
-								    .collapsible-captions .amp-carousel-container  {position: relative; width: 100%;}
-								    .collapsible-captions amp-img img {object-fit: contain; }
-								    .collapsible-captions figure { margin: 0; padding: 0; }
-								    .collapsible-captions figcaption { position: absolute; bottom: 0;width: 100%;
-								      max-height: var(--caption-height);margin-bottom:0;
-								      line-height: var(--caption-height);
-								      padding: 0 var(--button-size) 0 5px;
-								      white-space: nowrap;
-								      overflow: hidden;
-								      text-overflow: ellipsis;
-								      transition: max-height 200ms cubic-bezier(0.4, 0, 0.2, 1);
-								      z-index: 1000;
-								      color: var(--caption-color);
-								      background: rgba(0, 0, 0, 0.6);   
-								    }
-								    .collapsible-captions figcaption.expanded {
-								      line-height: inherit;
-								      white-space: normal;
-								      text-overflow: auto;
-								      max-height: 100px;
-								      overflow: auto;
-								    }
-								    .collapsible-captions figcaption:focus { outline: none; border: none; }
-								    .collapsible-captions figcaption span { display: block; position: absolute;
-								      top: calc((var(--caption-height) - var(--button-size)) / 2);
-								      right: 2px; width: var(--button-size); height: var(--button-size);
-								      line-height: var(--button-size); text-align: center; font-size: 12px; color: inherit;
-								      cursor: pointer; }
-									figcaption{ margin-bottom: 20px; }'
+						'gallery_css' => '
+								     
+								    '
 									),
 			'2' => array(
 						'image-with-caption-html'=>'<figure><div class="ampforwp-gallery-item amp-carousel-container">{{main_images}} </div><figcaption :openbrack:class:closebrack:="expanded? \'expanded\' : \'\'" on="tap:AMP.setState({expanded: !expanded})" tabindex="0" role="button" >{{main_images_caption}}<span :openbrack:text:closebrack:="expanded ? \'less\' : \'more\'">more</span> </figcaption></figure>',
 						'image-without-caption-html' =>'<div class="ampforwp-gallery-item amp-carousel-container">{{main_images}} </div>',
 						'carousel_with_thumbnail_html'=>'<button on="tap:carousel-with-carousel-preview-{{unique_id}}.goToSlide(index={{unique_index}})" class="amp-carousel-slide amp-scrollable-carousel-slide">{{thumbnail}}</button>',
-						'gallery_css' => '.collapsible-captions {--caption-height: 32px; --image-height: 100%; --caption-padding:1rem; --button-size: 28px; --caption-color: #f5f5f5;; --caption-bg-color: #111;}
-					    .collapsible-captions * {
-					      -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
-					      box-sizing: border-box;
-					    }
-						    .collapsible-captions .amp-carousel-container  {position: relative; width: 100%;}
-						    .collapsible-captions amp-img img {object-fit: contain; }
-						    .collapsible-captions figure { margin: 0; padding: 0; }
-						    .collapsible-captions figcaption { position: absolute; bottom: 0;width: 100%;
-						      max-height: var(--caption-height);margin-bottom:0;
-						      line-height: var(--caption-height);
-						      padding: 0 var(--button-size) 0 5px;
-						      white-space: nowrap;
-						      overflow: hidden;
-						      text-overflow: ellipsis;
-						      transition: max-height 200ms cubic-bezier(0.4, 0, 0.2, 1);
-						      z-index: 1000;
-						      color: var(--caption-color);
-						      background: rgba(0, 0, 0, 0.6);   
-						    }
-						    .collapsible-captions figcaption.expanded {
-						      line-height: inherit;
-						      white-space: normal;
-						      text-overflow: auto;
-						      max-height: 100px;
-						      overflow: auto;
-						    }
-						    .collapsible-captions figcaption:focus { outline: none; border: none; }
-						    .collapsible-captions figcaption span { display: block; position: absolute;
-						      top: calc((var(--caption-height) - var(--button-size)) / 2);
-						      right: 2px; width: var(--button-size); height: var(--button-size);
-						      line-height: var(--button-size); text-align: center; font-size: 12px; color: inherit;
-						      cursor: pointer; }
-							figcaption{ margin-bottom: 20px; }
-							.carousel-preview amp-img{height:40px;width:60px;position:relative;}'
+						'gallery_css' => '
+							.carousel-preview amp-img{height:40px;width:60px;position:relative;}
+
+								'
 									),
 
 		);
