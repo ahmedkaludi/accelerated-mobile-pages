02f37fce29ae055db03063a5a00877ed6a9d73ca
pegazee <abdulazeem.8179@gmail.com>
2018-08-14 19:51:35 +0530

    #2459 proper code added

--- components/logo/logo.php
+++ components/logo/logo.php
@@ -28,10 +28,11 @@ function ampforwp_framework_get_logo(){
           }?>
         <?php $ampforwp_home_url = apply_filters('ampforwp_header_url', $ampforwp_home_url); ?>
         <?php if ( true == ($redux_builder_amp['opt-media']['url']) ) {
-          $alt = ampforwp_default_logo('alt')  ?>
+          $alt = ampforwp_default_logo('alt'); 
+          $logo_url = __( $redux_builder_amp['opt-media']['url'], 'accelerated-mobile-pages' ); ?>
           <a href="<?php echo esc_url( $ampforwp_home_url ); ?>"  <?php if($set_rel_to_noamp){ echo ' rel="nofollow"'; } ?>  >
 
-                <amp-img src="<?php echo __( $redux_builder_amp['opt-media']['url'], 'accelerated-mobile-pages' ); ?>" width="<?php echo ampforwp_default_logo('width'); ?>" height="<?php echo ampforwp_default_logo('height'); ?>" alt="<?php echo $alt; ?>" class="amp-logo" layout="responsive"></amp-img>
+                <amp-img src="<?php echo esc_url( $logo_url );  ?>" width="<?php echo ampforwp_default_logo('width'); ?>" height="<?php echo ampforwp_default_logo('height'); ?>" alt="<?php echo $alt; ?>" class="amp-logo" layout="responsive"></amp-img>
 
           </a>
         <?php } else { if(is_single()){ ?>
