2dcaebd44840fa66580f143ab22665b5e8d299e8
Mohammed Kaludi <mohammed@magazine3.com>
2018-08-17 18:40:08 +0530

    #2459 added the code in master and pushed with 0.9.97.10
    #2459 added the code in master and pushed with 0.9.97.10
    
    Also Removed the spaces in logo.php for #2445 reported by Frenchomatic

--- components/logo/logo.php
+++ components/logo/logo.php
@@ -1,11 +1,9 @@
 <?php
 function ampforwp_framework_get_logo(){
   global $redux_builder_amp; ?>
-<div class="amp-logo">
-  <?php
+<div class="amp-logo"><?php
         do_action('amp_header_top');
         $set_rel_to_noamp=false;
-
         if( $redux_builder_amp['amp-on-off-support-for-non-amp-home-page'] ) {
                 if( $redux_builder_amp['amp-mobile-redirection'] ) {
                   $ampforwp_home_url = trailingslashit( get_bloginfo('url') ).'?nonamp=1';
@@ -25,19 +23,16 @@ function ampforwp_framework_get_logo(){
                           $ampforwp_home_url = trailingslashit( get_bloginfo('url') );
                          }
                 }
-          }?>
-        <?php $ampforwp_home_url = apply_filters('ampforwp_header_url', $ampforwp_home_url); ?>
-        <?php if ( true == ($redux_builder_amp['opt-media']['url']) ) {
-          $alt = ampforwp_default_logo('alt')  ?>
-          <a href="<?php echo esc_url( $ampforwp_home_url ); ?>"  <?php if($set_rel_to_noamp){ echo ' rel="nofollow"'; } ?>  >
-
-                <amp-img src="<?php echo $redux_builder_amp['opt-media']['url']; ?>" width="<?php echo ampforwp_default_logo('width'); ?>" height="<?php echo ampforwp_default_logo('height'); ?>" alt="<?php echo $alt; ?>" class="amp-logo" layout="responsive"></amp-img>
-
-          </a>
-        <?php } else { ?>
+          }
+          $ampforwp_home_url = apply_filters('ampforwp_header_url', $ampforwp_home_url);
+          if ( true == ($redux_builder_amp['opt-media']['url']) ) {
+          $alt = $logo_url = "";
+          $alt      = ampforwp_default_logo('alt'); 
+          $logo_url = __( $redux_builder_amp['opt-media']['url'], 'accelerated-mobile-pages' ); 
+          ?><a href="<?php echo esc_url( $ampforwp_home_url ); ?>" <?php if($set_rel_to_noamp){ echo ' rel="nofollow"'; } ?>><amp-img src="<?php echo esc_url( $logo_url );  ?>" width="<?php echo ampforwp_default_logo('width'); ?>" height="<?php echo ampforwp_default_logo('height'); ?>" alt="<?php echo esc_attr($alt); ?>" class="amp-logo" layout="responsive"></amp-img></a><?php } else { ?>
           <h1><a href="<?php echo esc_url( $ampforwp_home_url ); ?>"  <?php if($set_rel_to_noamp){ echo ' rel="nofollow"'; } ?>  ><?php bloginfo('name'); ?></a></h1>
-        <?php } ?>
- </div>
+        <?php } 
+?></div>
  <?php }
 
 add_action('amp_post_template_css','amp_framework_logo_styles',11); 
