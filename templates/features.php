e82b1bf8777231c8b8566c71d6f1b4b19cf2caf7
Syed Zabiuddin <syedzabi332@gmail.com>
2018-08-31 15:07:39 +0530

    #2395 warnings fixed and Added in master and will be released in 0.9.97.14

--- templates/features.php
+++ templates/features.php
@@ -6960,6 +6960,7 @@ function ampforwp_remove_instant_articles_amp_markup(){
 }
 // #2042 
 function ampforwp_404_canonical(){
+	global $wp;
 	return home_url( $wp->request );
 }
 // #2001 removing unused JS from the Paginated Posts
