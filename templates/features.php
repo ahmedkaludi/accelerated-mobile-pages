090abf241f88dd95a807f9d380fe23d554d9ab46
Syed Zabiuddin <syedzabi332@gmail.com>
2018-09-06 14:06:36 +0530

    #1491 ago translation added in master

--- templates/features.php
+++ templates/features.php
@@ -6271,7 +6271,7 @@ if( ! function_exists( 'ampforwp_full_post_date_loops' ) ){
 		if( $redux_builder_amp['ampforwp-post-date-format-text'] ){
 			$full_date = $redux_builder_amp['ampforwp-post-date-format-text'];
 			// Change the % days into the actual number of days
-			$full_date = str_replace('% days', $date, $full_date);
+			$full_date = str_replace('ago', ampforwp_translation( $redux_builder_amp['amp-translator-ago-date-text'],'ago'), $full_date);
 		}
 	}
 	return $full_date;
