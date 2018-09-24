007c9a3399aa78871a647e727e0d290ca8101cbd
Syed Zabiuddin <syedzabi332@gmail.com>
2018-09-24 15:37:41 +0530

    #2434 css for floating issue for iphone and added in master

--- templates/design-manager/swift/style.php
+++ templates/design-manager/swift/style.php
@@ -439,6 +439,15 @@ if($redux_builder_amp['swift-social-position'] == 'below-content'){?>
  // swift Content Heading Sizes Ends?>
 .cntn-wrp p, .cntn-wrp ul, .cntn-wrp ol{margin:0px 0px 30px 0px;word-break: break-word;}
 .sp-rt p strong, .pg p strong{font-weight: 700;}
+.m-ctr {
+position: fixed;
+overflow: scroll;
+}
+.m-scrl {
+display: inline-block;
+width: 100%;
+max-height: 94vh;
+}
 <?php if($redux_builder_amp['rp_design_type'] == '1'){?>
 	.srp{margin-top:20px;}
 	.srp .amp-related-posts amp-img{float: left;width: 100%;margin: 0px;height:100%;}
