b652b9b9c603934cabe8d08d281cf340ecee998d
Syed Zabiuddin <syedzabi332@gmail.com>
2018-10-11 12:56:13 +0530

    #2531 condition properly added for styling

--- templates/design-manager/design-3/style.php
+++ templates/design-manager/design-3/style.php
@@ -611,7 +611,37 @@ if(is_single()){?>
 <?php } ?>
 <?php 
 if (ampforwp_get_setting('enable-amp-ads-resp-1')){?>
-.amp-ad-1,.amp-ad-2,.amp-ad-3,.amp-ad-4,.amp-ad-5,.amp-ad-6 {
+.amp-ad-1 {
+    max-width: 1000px;
+}
+<?php } ?>
+<?php 
+if (ampforwp_get_setting('enable-amp-ads-resp-2')){?>
+.amp-ad-2 {
+    max-width: 1000px;
+}
+<?php } ?>
+<?php 
+if (ampforwp_get_setting('enable-amp-ads-resp-3')){?>
+.amp-ad-3 {
+    max-width: 1000px;
+}
+<?php } ?>
+<?php 
+if (ampforwp_get_setting('enable-amp-ads-resp-4')){?>
+.amp-ad-4 {
+    max-width: 1000px;
+}
+<?php } ?>
+<?php 
+if (ampforwp_get_setting('enable-amp-ads-resp-5')){?>
+.amp-ad-5 {
+    max-width: 1000px;
+}
+<?php } ?>
+<?php 
+if (ampforwp_get_setting('enable-amp-ads-resp-6')){?>
+.amp-ad-6 {
     max-width: 1000px;
 }
 <?php } ?>
