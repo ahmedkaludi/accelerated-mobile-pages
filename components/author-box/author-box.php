2ff89acb15d3bf54ec60179cd325b3b7f37b8e7a
Marqas <marqas36@gmail.com>
2018-10-24 14:36:31 +0530

    Code improvement to resolve XSS issues

--- components/author-box/author-box.php
+++ components/author-box/author-box.php
@@ -62,27 +62,27 @@ if ( isset($args['show_time']) ) {
 }
 
  ?>
-    <div class="amp-author <?php echo $class; ?>">
+    <div class="amp-author <?php echo esc_attr($class); ?>">
         <?php if ( $avatar ) {
     $author_avatar_url = ampforwp_get_wp_user_avatar();
     if( null == $author_avatar_url ){
        $author_avatar_url = get_avatar_url( $post_author->ID, array( 'size' => $avatar_size ) );
     } ?>
-        <div class="amp-author-image <?php echo $author_image_wrapper; ?>">
+        <div class="amp-author-image <?php echo esc_html($author_image_wrapper); ?>">
             <amp-img <?php if(ampforwp_get_data_consent()){?>data-block-on-consent <?php } ?>src="<?php echo esc_url($author_avatar_url); ?>" width="<?php echo $avatar_size; ?>" height="<?php echo $avatar_size; ?>" layout="fixed"></amp-img> 
         </div>
         <?php } ?>
-        <?php echo '<div class="author-details '. $author_wrapper_class .'">';
+        <?php echo '<div class="author-details '. esc_attr($author_wrapper_class) .'">';
         if ( true == $redux_builder_amp['ampforwp-author-page-url'] ){
             if ( function_exists('coauthors_posts_links') ) {
-                echo '<span class="author-name">' .$author_prefix . $author_link . ' </span>';
+                echo '<span class="author-name">' . esc_html($author_prefix) . esc_url($author_link) . ' </span>';
             }
             else {
-                echo '<span class="author-name">' .$author_prefix . ' <a href="'. esc_url(ampforwp_url_controller($author_link)).'"> ' .esc_html( $author_name ).'</a></span>';
+                echo '<span class="author-name">' . esc_html($author_prefix) . ' <a href="'. esc_url(ampforwp_url_controller($author_link)).'"> ' .esc_html( $author_name ).'</a></span>';
             }
         }
         else
-            echo '<span class="author-name">' . $author_prefix . esc_html( $author_name ) . '</span>';
+            echo '<span class="author-name">' . esc_html($author_prefix) . esc_html( $author_name ) . '</span>';
 
         //to show date and time
         if ( $show_date || $show_time ) {
@@ -96,7 +96,7 @@ if ( isset($args['show_time']) ) {
          echo '</span>';
         }
         if ( $author_description ) {
-        	echo "<p>".$post_author->description."</p>";
+        	echo "<p>". esc_html($post_author->description)."</p>";
         } ?>
         </div>
     </div>
--- components/call-now/call-now.php
+++ components/call-now/call-now.php
@@ -4,7 +4,7 @@ function amp_call_button_html_output(){
 	global $redux_builder_amp;
 	if ( $redux_builder_amp['ampforwp-callnow-button'] ) { ?>
 		<div class="amp-phone">
-			<a href="tel:<?php echo $redux_builder_amp['enable-amp-call-numberfield']; ?>"></a>
+			<a href="tel:<?php echo esc_url($redux_builder_amp['enable-amp-call-numberfield']); ?>"></a>
 		</div> <?php
   }
 }
@@ -12,5 +12,5 @@ add_action('amp_post_template_css','amp_callnow_styles',11);
 function amp_callnow_styles(){
     global $redux_builder_amp; if ($redux_builder_amp['ampforwp-callnow-button']) { ?>
 .amp-phone{ position:relative }
-.amp-phone a:before { content: ""; display:inline-block; width: 4px; height: 8px; border-width: 6px 0 6px 3px; border-style: solid; border-color:<?php echo $redux_builder_amp['amp-opt-color-rgba-colorscheme-call']['color']; ?>; background: transparent; transform: rotate(-30deg); box-sizing: initial; border-top-left-radius: 3px 5px; border-bottom-left-radius: 3px 5px; }
+.amp-phone a:before { content: ""; display:inline-block; width: 4px; height: 8px; border-width: 6px 0 6px 3px; border-style: solid; border-color:<?php echo esc_attr($redux_builder_amp['amp-opt-color-rgba-colorscheme-call']['color']); ?>; background: transparent; transform: rotate(-30deg); box-sizing: initial; border-top-left-radius: 3px 5px; border-bottom-left-radius: 3px 5px; }
 <?php }} ?>
