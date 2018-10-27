4fc242d469ad9df640645327e4be67f59189f36f
Mohammed Kaludi <mohammed@magazine3.com>
2018-10-27 18:04:41 +0530

    categories-tags.php cleaned #2650
    categories-tags.php cleaned #2650

--- components/categories-tags/categories-tags.php
+++ components/categories-tags/categories-tags.php
@@ -9,12 +9,12 @@ function ampforwp_framework_get_categories_list(){
 	 $ampforwp_categories = get_the_terms( $post->ID, 'category' );
 		if ( $ampforwp_categories ) : ?>
 		<div class="amp-category">
-				<span><?php echo ampforwp_translation($redux_builder_amp['amp-translator-categories-text'], 'Categories' ); ?></span>
+				<span><?php echo esc_html(ampforwp_translation($redux_builder_amp['amp-translator-categories-text'], 'Categories' )); ?></span>
 				<?php foreach ($ampforwp_categories as $cat ) {
 						if( true == $redux_builder_amp['ampforwp-archive-support'] && true == $redux_builder_amp['ampforwp-cats-tags-links-single']) {
-								echo ('<span class="amp-cat amp-cat-'.$cat->term_id.'"><a href="'. ampforwp_url_controller( get_category_link( $cat->term_id ) )   .'" > '. $cat->name .'</a></span>');//#934
+								echo ('<span class="amp-cat amp-cat-'.esc_attr($cat->term_id).'"><a href="'. ampforwp_url_controller( get_category_link( $cat->term_id ) )   .'" > '. esc_html($cat->name) .'</a></span>');//#934
 						} else {
-							 echo '<span class="amp-cat">'. $cat->name .'</span>';
+							 echo '<span class="amp-cat">'. esc_html($cat->name) .'</span>';
 						}
 			} ?>
 		</div>
@@ -25,21 +25,14 @@ function ampforwp_framework_get_tags_list(){
 	 	$ampforwp_tags = get_the_terms( $post->ID, 'post_tag' );
 			if ( $ampforwp_tags && ! is_wp_error( $ampforwp_tags )  ) :?>
 				<div class="amp-tags">
-					<?php /* if($redux_builder_amp['amp-rtl-select-option']==0) {
-					  		 global $redux_builder_amp; printf( ampforwp_translation($redux_builder_amp['amp-translator-tags-text'] .' ', 'accelerated-mobile-pages' ));
-							 		}*/
-							 		?>
-					<span><?php echo ampforwp_translation($redux_builder_amp['amp-translator-tags-text'], 'Tags' ); ?></span>
+					<span><?php echo esc_html(ampforwp_translation($redux_builder_amp['amp-translator-tags-text'], 'Tags' )); ?></span>
 					<?php foreach ( $ampforwp_tags as $tag ) {
 						if( true == $redux_builder_amp['ampforwp-archive-support'] && true == $redux_builder_amp['ampforwp-cats-tags-links-single'] ) {
-                			echo ('<span class="amp-tag amp-tag-'.$tag->term_id.'"><a href="'. ampforwp_url_controller( get_tag_link( $tag->term_id ) ).'" >'.$tag->name .'</a></span>');//#934
+                			echo ('<span class="amp-tag amp-tag-'.esc_attr($tag->term_id).'"><a href="'. ampforwp_url_controller( get_tag_link( $tag->term_id ) ).'" >'.esc_html($tag->name) .'</a></span>');//#934
 						} else {
-							 	echo ('<span class="amp-tag">'.$tag->name.'</span>');
+							 	echo ('<span class="amp-tag">'.esc_html($tag->name).'</span>');
 						}
-					}
-					/*	if($redux_builder_amp['amp-rtl-select-option']) {
-						  		 global $redux_builder_amp; printf( ampforwp_translation($redux_builder_amp['amp-translator-tags-text'] .' ', 'accelerated-mobile-pages' ));
-						}*/ ?>
+					}?>
 				</div>
 	<?php endif; 
 }
