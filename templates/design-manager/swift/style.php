017e87d3fc0dbef8e894bd742d48641a58b28750
Syed Zabiuddin <syedzabi332@gmail.com>
2018-11-01 15:51:44 +0530

    #2641 line social icon added in swift

--- templates/design-manager/swift/style.php
+++ templates/design-manager/swift/style.php
@@ -1308,6 +1308,9 @@ if($redux_builder_amp['enable-single-vk-share']){?>
 .s_vk{background:#45668e;}
 .s_vk:after{content: "\e944";}
 <?php } 
+if($redux_builder_amp['enable-single-line-share'] == true)  { ?>
+.s_li{background:#00cc00;}
+<?php } 	
 if($redux_builder_amp['enable-single-reddit-share']){?>
 .s_rd{background:#ff4500;}
 .s_rd:after{content: "\e938";}
