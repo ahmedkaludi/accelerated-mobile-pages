d489661b68b246e9065b30d830fb66f30f84ea37
Syed Zabiuddin <syedzabi332@gmail.com>
2018-11-08 17:02:58 +0530

    #2665 justify added in text module

--- pagebuilder/modules/text-mod-module.php
+++ pagebuilder/modules/text-mod-module.php
@@ -93,7 +93,9 @@ return array(
 	 							'options_details'=>array(
 	 												'center'    =>'Center',
 	 												'left'  	=>'Left',
-	 												'right'    =>'Right', 													),
+	 												'right'    =>'Right', 
+	 												'justify'	=>'Justify'	
+	 											),
 	 							'content_type'=>'css',
 	 						),
 	 					array(
