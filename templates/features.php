95c2b9b43ae10dc7077e6bf28ec1e4f4e2744eb5
Marqas <marqas36@gmail.com>
2018-12-06 17:14:59 +0530

    #2723 Proper escaping

--- templates/features.php
+++ templates/features.php
@@ -4007,11 +4007,11 @@ function ampforwp_dev_mode_notice(){
 	global $redux_builder_amp;
 	$message = '';
 	if(isset($redux_builder_amp['ampforwp-development-mode']) && $redux_builder_amp['ampforwp-development-mode']) {
-			$message =  '<strong>AMP Dev mode is Enabled! </strong> Please turn off Development mode, when you are done.';?>		
+			$message =  ' Please turn off Development mode, when you are done.';?>		
 			<div class="notice notice-success is-dismissible amp-dev-notice" style="position:relative;
 		    height: 40px; overflow: hidden; ">
 				<div class="ampforwp-dev-mode-message" style="margin-top: 10px;">
-					<?php echo $message; ?>					
+					<?php echo '<strong>'.esc_html__('AMP Dev mode is Enabled!').'</strong>'. esc_html__($message); ?>					
 				</div>	
 			</div>
 <?php }
