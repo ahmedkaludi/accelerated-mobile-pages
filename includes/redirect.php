aced0f6c91f8e61016a12ad8a5e052bb72d42c9b
Syed Zabiuddin <syedzabi332@gmail.com>
2018-12-06 15:40:34 +0530

    #2476 updated the code for getting the current url and added in master

--- includes/redirect.php
+++ includes/redirect.php
@@ -100,9 +100,7 @@ function ampforwp_check_amp_page_status() {
     // Removing the AMP on login register etc of Theme My Login plugin
     if (function_exists('tml_register_default_actions')){
         $tml_pages = theme_my_login()->get_actions();
-    if ( isset( $_SERVER['REQUEST_URI']) && $_SERVER['REQUEST_URI']  ) {
-        $current_page = $_SERVER['REQUEST_URI'];
-    }
+        $current_page = home_url( $wp->request);
         $current_page = explode('/', $current_page);
        if ( isset($tml_pages) && $tml_pages ) {
         foreach ($tml_pages as $page) {
