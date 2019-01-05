b3dd2ec54da572f00d3930fd9a0d432c2b21bc8a
AJEET KUMAR <ajeet.gupta51@gmail.com>
2019-01-05 18:57:41 +0530

    #2718 Added condition for only frontpage and levelup not enabled

--- templates/design-manager/swift/style.php
+++ templates/design-manager/swift/style.php
@@ -43,7 +43,7 @@ amp-carousel > amp-img > img {object-fit: contain;}
 .amp-carousel-img img {object-fit: contain;}
 amp-instagram{box-sizing: initial;}
 figure.aligncenter amp-img {margin: 0 auto;}
-<?php if(!is_page()){ ?>
+<?php if(!ampforwp_is_front_page() && !is_plugin_active('levelup/levelup.php')){ ?>
 .cntr {max-width: 1100px;margin: 0 auto;width:100%;padding:0px 20px}
 <?php } ?>
 <?php if(!function_exists('if_levelup_has_builder') || (function_exists('if_levelup_has_builder') && !if_levelup_has_builder()) ){  // Level up Condition starts?>
