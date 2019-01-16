1b659e86757eee71d9cc4d045045362e4f3e7bd9
Marqas <marqas36@gmail.com>
2019-01-16 17:01:10 +0530

    #2808 No need for this as we have already escaped it in the class

--- templates/design-manager/design-3/404.php
+++ templates/design-manager/design-3/404.php
@@ -1,6 +1,6 @@
 <?php global $redux_builder_amp;  ?>
 <!doctype html>
-<html amp <?php echo esc_attr(AMP_HTML_Utils::build_attributes_string( $this->get( 'html_tag_attributes' ) )); ?>>
+<html amp <?php echo AMP_HTML_Utils::build_attributes_string( $this->get( 'html_tag_attributes' ) ); ?>>
 <head>
 	<meta charset="utf-8">
     <link rel="dns-prefetch" href="https://cdn.ampproject.org">
