5f8861d73bb11b02510dcc7ec656affc24c252ec
AJEET KUMAR <ajeet.gupta51@gmail.com>
2019-01-21 16:12:39 +0530

    #2824 Added Gutenberg get Content

--- templates/custom-amp-content-button.js
+++ templates/custom-amp-content-button.js
@@ -6,10 +6,15 @@
             classes: 'ampforwp-copy-content-button ', 
             tooltip: 'Visual Editor to AMP Editor', 
             onclick: function() {
-                if(typeof tinymce.editors.content.getContent()!= undefined){
-                    editor.insertContent(tinymce.editors.content.getContent());
-                }else{
-                    editor.insertContent(document.getElementById('content').value());
+                if(typeof tinymce.editors.content!= undefined){
+                    if(typeof tinymce.editors.content.getContent()!= undefined){
+                        editor.insertContent(tinymce.editors.content.getContent());
+                    }else{
+                        editor.insertContent(document.getElementById('content').value());
+                    }
+                }else{//Gutenberg
+                 var editedContent = wp.data.select( "core/editor" ).getEditedPostContent();
+                    editor.insertContent(editedContent);   
                 }
             }
         });
