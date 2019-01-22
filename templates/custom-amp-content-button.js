ecbace8693b8649058c8e328e4ba52deb440a1b3
AJEET KUMAR <ajeet.gupta51@gmail.com>
2019-01-22 19:45:58 +0530

    #2824 Fix with classic editor

--- templates/custom-amp-content-button.js
+++ templates/custom-amp-content-button.js
@@ -6,15 +6,17 @@
             classes: 'ampforwp-copy-content-button ', 
             tooltip: 'Visual Editor to AMP Editor', 
             onclick: function() {
-                if(typeof tinymce.editors.content!= undefined){
+                if(wp.data === undefined){
                     if(typeof tinymce.editors.content.getContent()!= undefined){
                         editor.insertContent(tinymce.editors.content.getContent());
                     }else{
                         editor.insertContent(document.getElementById('content').value());
                     }
                 }else{//Gutenberg
-                 var editedContent = wp.data.select( "core/editor" ).getEditedPostContent();
-                    editor.insertContent(editedContent);   
+                    if(wp.data){
+                        var editedContent = wp.data.select( "core/editor" ).getEditedPostContent();
+                        editor.insertContent(editedContent);   
+                    }
                 }
             }
         });
