daebf7fa51b7938e7cf906c7c5207bb348a38982
Syed Zabiuddin <syedzabi332@gmail.com>
2019-02-06 11:47:31 +0530

    #2832 removed amphtml on pages which don't have amp

--- templates/features.php
+++ templates/features.php
@@ -7142,6 +7142,7 @@ if (! function_exists('ampforwp_bulktool_takeover') ) {
 function ampforwp_bulktool_takeover($data){
 	$bulk_option = ampforwp_get_setting('amp-pages-meta-default');
 	if(is_page() && $bulk_option == "hide" && (true == ampforwp_get_setting('ampforwp-amp-takeover') || true == ampforwp_get_setting('ampforwp-amp-convert-to-wp'))){
+		remove_action( 'wp_head', 'ampforwp_home_archive_rel_canonical', 1 );
 		return false; 
 	}
 	return $data;
