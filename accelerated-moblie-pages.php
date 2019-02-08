24a322a91a9fbb11e0d1dee656ed390e25bd89b3
Marqas <marqas36@gmail.com>
2019-02-08 15:18:15 +0530

    Color sanitizer added in Beta

--- accelerated-moblie-pages.php
+++ accelerated-moblie-pages.php
@@ -873,3 +873,18 @@ function ampforwp_add_plugin_meta_links($meta_fields, $file) {
 }
 // AMPforWP Global Data variable
 $ampforwp_data = array();
+
+// color sanitizer
+function ampforwp_sanitize_color( $color ) {
+    if ( empty( $color ) || is_array( $color ) )
+        return 'rgba(0,0,0,0)';
+    // If string does not start with 'rgba', then treat as hex
+    // sanitize the hex color and finally convert hex to rgba
+    if ( false === strpos( $color, 'rgba' ) ) {
+        return sanitize_hex_color( $color );
+    }
+    // By now we know the string is formatted as an rgba color so we need to further sanitize it.
+    $color = str_replace( ' ', '', $color );
+    sscanf( $color, 'rgba(%d,%d,%d,%f)', $red, $green, $blue, $alpha );
+    return 'rgba('.$red.','.$green.','.$blue.','.$alpha.')';
+}
