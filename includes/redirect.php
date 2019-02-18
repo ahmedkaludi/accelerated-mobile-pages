ba5c1b102e2e0b2ae7bc1437572e3f8c42c59982
AJEET KUMAR <ajeet.gupta51@gmail.com>
2019-02-18 12:59:46 +0530

    #2436 Added proper condition For mobile and tablet

--- includes/redirect.php
+++ includes/redirect.php
@@ -151,10 +151,17 @@ function ampforwp_page_template_redirect() {
     require_once AMPFORWP_PLUGIN_DIR.'/includes/vendor/Mobile_Detect.php';
     // instantiate the Mobile detect class
     $mobile_detect = new AMPforWP_Mobile_Detect;
+    $isMobile = $mobile_detect->isMobile();
     $isTablet = $mobile_detect->isTablet();
-    if ( false == ampforwp_get_setting('amp-mobile-redirection-tabs') ) {
-      $isTablet = ! $mobile_detect->isTablet();
+    $isTabletUserAction = ampforwp_get_setting('amp-mobile-redirection-tabs');
+    
+    $redirectToAMP = false;
+    if( $isMobile && $isTabletUserAction && $isTablet ){ //Only For tablet
+      $redirectToAMP = true;
+    }else if($isMobile && !$isTablet){ // Only for mobile
+      $redirectToAMP = true;
     }
+
     // No mobile redirection on oembeds #2003
     if ( function_exists('is_embed') && is_embed() ){
       return;
@@ -224,7 +231,7 @@ function ampforwp_page_template_redirect() {
       $url_to_redirect = ampforwp_url_controller($url_to_redirect); 
     }
     // Check if we are on Mobile phones then start redirection process
-    if ( $mobile_detect->isMobile() && $isTablet ) {
+    if ( $redirectToAMP ) {
 
         if ( ! isset($_SESSION['ampforwp_amp_mode']) || ! isset($_GET['nonamp']) ) {
 
