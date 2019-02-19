155db0b19705df482a43441ad6a3f647c64b0d3c
Marqas <marqas36@gmail.com>
2019-02-19 19:05:09 +0530

    #2904 Code improvement

--- components/logo/logo.php
+++ components/logo/logo.php
@@ -2,43 +2,32 @@
 function ampforwp_framework_get_logo(){
   global $redux_builder_amp; ?>
 <div class="amp-logo">
-  <?php
-        do_action('amp_header_top');
-        $set_rel_to_noamp=false;
+    <?php
+    do_action('amp_header_top');
+    $set_rel_to_noamp=false;
+    $ampforwp_home_url = user_trailingslashit( get_bloginfo('url') );
+    if ( true == ampforwp_get_setting('ampforwp-homepage-on-off-support') ) {
+      if ( ampforwp_get_setting('amp-on-off-support-for-non-amp-home-page') && ampforwp_get_setting('amp-mobile-redirection') ) {
+          $ampforwp_home_url = trailingslashit( get_bloginfo('url') ).'?nonamp=1';
+          $set_rel_to_noamp = true;
+      }
+      else{
+        $ampforwp_home_url = ampforwp_url_controller( get_bloginfo('url') );
+      }
+    }?>
+    <?php $ampforwp_home_url = apply_filters('ampforwp_header_url', $ampforwp_home_url); ?>
+    <?php if ( true == ($redux_builder_amp['opt-media']['url']) ) {
+      $alt = ampforwp_default_logo('alt'); 
+      $logo_url = __( $redux_builder_amp['opt-media']['url'], 'accelerated-mobile-pages' ); ?>
+      <a href="<?php echo esc_url( $ampforwp_home_url ); ?>"  <?php if($set_rel_to_noamp){ echo ' rel="nofollow"'; } ?>  >
 
-        if( $redux_builder_amp['amp-on-off-support-for-non-amp-home-page'] ) {
-                if( $redux_builder_amp['amp-mobile-redirection'] ) {
-                  $ampforwp_home_url = trailingslashit( get_bloginfo('url') ).'?nonamp=1';
-                  $set_rel_to_noamp = true;
-                  } else {
-                    $ampforwp_home_url = user_trailingslashit( get_bloginfo('url') );
-                 }
-        } else {
-                 if($redux_builder_amp['ampforwp-homepage-on-off-support']) {
-                    $ampforwp_home_url = ampforwp_url_controller( get_bloginfo('url') );
-                    //$ampforwp_home_url = user_trailingslashit( trailingslashit( get_bloginfo('url') ) . AMPFORWP_AMP_QUERY_VAR );
-                 } else {
-                        if( $redux_builder_amp['amp-mobile-redirection'] ) {
-                          $ampforwp_home_url = trailingslashit( get_bloginfo('url') ).'?nonamp=1';
-                          $set_rel_to_noamp = true;
-                         } else {
-                          $ampforwp_home_url = trailingslashit( get_bloginfo('url') );
-                         }
-                }
-          }?>
-        <?php $ampforwp_home_url = apply_filters('ampforwp_header_url', $ampforwp_home_url); ?>
-        <?php if ( true == ($redux_builder_amp['opt-media']['url']) ) {
-          $alt = ampforwp_default_logo('alt'); 
-          $logo_url = __( $redux_builder_amp['opt-media']['url'], 'accelerated-mobile-pages' ); ?>
-          <a href="<?php echo esc_url( $ampforwp_home_url ); ?>"  <?php if($set_rel_to_noamp){ echo ' rel="nofollow"'; } ?>  >
+            <amp-img src="<?php echo esc_url( $logo_url );  ?>" width="<?php echo ampforwp_default_logo('width'); ?>" height="<?php echo ampforwp_default_logo('height'); ?>" alt="<?php echo $alt; ?>" class="amp-logo" layout="responsive"></amp-img>
 
-                <amp-img src="<?php echo esc_url( $logo_url );  ?>" width="<?php echo ampforwp_default_logo('width'); ?>" height="<?php echo ampforwp_default_logo('height'); ?>" alt="<?php echo $alt; ?>" class="amp-logo" layout="responsive"></amp-img>
-
-          </a>
-        <?php } else { if(is_single()){ ?>
-          <h2> <?php } else{?><h1><?php }?>
-          <a href="<?php echo esc_url( $ampforwp_home_url ); ?>"  <?php if($set_rel_to_noamp){ echo ' rel="nofollow"'; } ?>  ><?php bloginfo('name'); ?></a><?php if(is_single()){?></h2><?php } else{?></h1><?php }
-         } ?>
+      </a>
+    <?php } else { if(is_single()){ ?>
+      <h2> <?php } else{?><h1><?php }?>
+      <a href="<?php echo esc_url( $ampforwp_home_url ); ?>"  <?php if($set_rel_to_noamp){ echo ' rel="nofollow"'; } ?>  ><?php bloginfo('name'); ?></a><?php if(is_single()){?></h2><?php } else{?></h1><?php }
+     } ?>
  </div>
  <?php }
 
