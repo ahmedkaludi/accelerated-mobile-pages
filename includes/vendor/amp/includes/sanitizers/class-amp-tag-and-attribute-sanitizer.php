b5e23135cf9ae4d91e4305bcea52ee23e9330900
Marqas <marqas36@gmail.com>
2019-02-20 18:30:59 +0530

    #2868 Lets add this for now

--- includes/vendor/amp/includes/sanitizers/class-amp-tag-and-attribute-sanitizer.php
+++ includes/vendor/amp/includes/sanitizers/class-amp-tag-and-attribute-sanitizer.php
@@ -207,7 +207,7 @@ class AMP_Tag_And_Attribute_Sanitizer extends AMP_Base_Sanitizer {
 			$scriptArray[$ext_key] = sprintf(
 				'https://cdn.ampproject.org/v0/%s-%s.js',
 				$ext_key,
-				'latest'
+				'0.1'
 			);
 		}
 		if(count($scriptArray)>0){
