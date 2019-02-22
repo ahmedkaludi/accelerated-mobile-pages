821baf494a6ebd96fb92ed15ed72f6c9d338b437
Raju Jeelaga <rajujeelaga@gmail.com>
2019-02-22 16:13:32 +0530

    #2618 Condition is added for Default AMP Theme Frame CSS
    #2618 Condition is added for Default AMP CSS, It will load only when Theme frame is active

--- templates/design-manager/design-2/index.php
+++ templates/design-manager/design-2/index.php
@@ -93,9 +93,9 @@
 					?>
 			        <p class="<?php echo $class; ?>">
 					<?php 
-						$excerpt_length		='';
-						$excerpt_length 	= $redux_builder_amp['amp-design-2-excerpt'];
-						$excerpt_length = (int) $excerpt_length
+						$excerpt_length	= $final_content  ='';
+						$excerpt_length = $redux_builder_amp['amp-design-2-excerpt'];
+						$excerpt_length = (int) $excerpt_length ;
 						$final_content  = apply_filters('ampforwp_modify_index_content', $content,  $excerpt_length );
 
 						if ( false === has_filter('ampforwp_modify_index_content' ) ) {
