954ad3c8bc83c48da475d20329aa4dcd92869be5
Syed Zabiuddin <syedzabi332@gmail.com>
2019-03-01 17:55:14 +0530

    #2917 remove mobile redirection when api in url

--- includes/redirect.php
+++ includes/redirect.php
@@ -192,6 +192,9 @@ function ampforwp_redirection() {
     if ( is_feed() ) { 
       return; 
     }
+    if(get_query_var( 'json' )){
+      return; 
+    }
     // #1192 Password Protected posts exclusion
     if ( post_password_required( $post ) ) { 
       return; 
