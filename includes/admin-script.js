f6956ad3824dc22fc12d1a27e9d9a7044274f636
Marqas <marqas36@gmail.com>
2019-03-05 16:44:17 +0530

    #2933 Added in master

--- includes/admin-script.js
+++ includes/admin-script.js
@@ -597,7 +597,7 @@ AmpForWpYoastAnalysis = function() {
      */
     AmpForWpYoastAnalysis.prototype.myContentModification = function(data) {
         if($('#amp-page-builder-ready').length){
-            var pbdata  = $('#amp-page-builder-ready').val();
+            var pbdata  = $('#amp-page-builder-ready').html();
             var takeover = redux_data['ampforwp-amp-takeover'];
             var pb2 = $('input[name="ampforwp_page_builder_enable"]').val();
             if ( takeover == 1 && 'yes' == pb2 ) {
