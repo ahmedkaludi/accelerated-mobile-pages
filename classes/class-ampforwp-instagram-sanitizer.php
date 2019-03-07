fa5633b6f410155d75390bd3c4cad7ac35718759
Marqas <marqas36@gmail.com>
2019-03-07 18:48:33 +0530

    #2629 Added in master

--- classes/class-ampforwp-instagram-sanitizer.php
+++ classes/class-ampforwp-instagram-sanitizer.php
@@ -1,16 +1,13 @@
 <?php
-// Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) {
-    exit;
-}
 
 /**
  * Converts Instagram embeds to <amp-instagram>
  */
 class AMPFORWP_Instagram_Embed_Sanitizer extends AMP_Base_Sanitizer {
 private $instagram_medias = array();
+const URL_PATTERN = '#http(s?)://(www\.)?instagr(\.am|am\.com)/p/([^/?]+)#i';
 private static $script_slug = 'amp-instagram';
- private static $script_src = 'https://cdn.ampproject.org/v0/amp-instagram-0.1.js';
+private static $script_src = 'https://cdn.ampproject.org/v0/amp-instagram-0.1.js';
 public function sanitize() {
   $body = $this->get_body_node();
     $xpath = new \DOMXPath($this->dom);
@@ -27,9 +24,11 @@ function replace_with_amp_instagram ($instagram_media){
   $medias = $instagram_media->getElementsByTagName('a');
   if($medias->length > 0){
    $media = $medias->item(0);
+   // Insta link
    $href = $media->getAttribute('href');
-   $explode = explode('/', rtrim($href, '/'));
-   $sourcecode = end($explode);
+   // Get the ID from the link
+   $sourcecode = $this->get_instagram_id_from_url($href);
+   // Create Instagram tag from the link
    $tag = $this->create_instagram_tag($sourcecode);
    $this->instagram_medias[] = $tag; // add it to array
    $instagram_media->parentNode->replaceChild( $tag, $instagram_media);
@@ -52,4 +51,14 @@ public function get_scripts() {
   }
 return array( self::$script_slug => self::$script_src );
  }
+
+private function get_instagram_id_from_url( $url ) {
+    $found = preg_match( self::URL_PATTERN, $url, $matches );
+
+    if ( ! $found ) {
+      return false;
+    }
+
+    return end( $matches );
+  }
 }
