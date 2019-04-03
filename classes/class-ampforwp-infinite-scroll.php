653724e9f000073390d3fafcc1feca211a48f94e
RajuJeelaga <rajujeelaga@gmail.com>
2019-04-03 13:43:25 +0530

    #2682 Repeating widgets are removed, when infinate scroll is eanbled
    #2682 Repeating widgets are removed, when infinate scroll is eanbled

--- classes/class-ampforwp-infinite-scroll.php
+++ classes/class-ampforwp-infinite-scroll.php
@@ -146,7 +146,7 @@ if( ! class_exists('AMPforWP_Infinite_Scroll') ) {
 				$classes = array("#headerwrap",".relatedpost",".footer_wrapper",".pagination-holder");
 			}
 			if ( 4 == $design ) {
-				$classes = array(".p-m-fl",".loop-pagination",".footer",".r-pf",".srp ul",".srp h3","#pagination",".h_m_w");
+				$classes = array(".p-m-fl",".loop-pagination",".footer",".r-pf",".srp ul",".srp h3","#pagination",".h_m_w", ".f-w");
 			}
 
 			return json_encode($classes);
