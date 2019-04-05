99e90fb8ee6f7dc8f08b545b52caedb2b36eeead
Syed Zabiuddin <syedzabi332@gmail.com>
2019-04-05 13:10:18 +0530

    #2990 Smush Pro lazyload compatibility

--- templates/features.php
+++ templates/features.php
@@ -1145,7 +1145,8 @@ function ampforwp_remove_schema_data() {
 		ampforwp_remove_filters_for_class( 'wp_nav_menu_args', 'BF_Menus', 'walker_front', 10 );
 		// Removing A3 Lazy Load plugin #2872
 		ampforwp_remove_filters_for_class( 'the_content', 'A3_Lazy_Load', 'filter_content_images', 100 );
-
+		// Removing Smush Pro Lazy Load plugin #2990
+		ampforwp_remove_filters_for_class( 'the_content', 'WP_Smush_Lazy_Load', 'set_lazy_load_attributes', 100 );
 		//SiteOrigin Page builder compatibilty with AMP Frontpage
 		if ( ampforwp_is_front_page() ) {
 				ampforwp_remove_filters_for_class( 'the_content', 'SiteOrigin_Panels', 'generate_post_content', 10 );
