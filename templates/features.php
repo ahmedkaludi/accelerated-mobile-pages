832bc02df98dc75e251a1a53390dc2911caebcb4
Marqas <marqas36@gmail.com>
2019-04-17 19:53:33 +0530

    #2287 Sanitizer (xyz) class does not exist resolved

--- templates/features.php
+++ templates/features.php
@@ -6623,6 +6623,17 @@ function ampforwp_modify_plugin_manager_options($sections){
 
 
 // Fallbacks for Vendor AMP #2287
+// Class AMP_Blacklist_Sanitizer
+add_action('plugins_loaded', 'ampforwp_sanitizers_loader');
+function ampforwp_sanitizers_loader(){
+	if ( ! class_exists('AMP_Blacklist_Sanitizer') ) {
+		if(defined('AMP__VENDOR__DIR__')){
+			$amp_blacklist_sanitizer =  realpath( AMP__VENDOR__DIR__ . 'includes/sanitizers/class-amp-blacklist-sanitizer.php') ;
+			require_once $amp_blacklist_sanitizer;
+			class AMP_Blacklist_Sanitizer extends AMPforWP\AMPVendor\AMP_Blacklist_Sanitizer{}
+		} 
+	}
+}
 // Class AMP_Base_Sanitizer
 if ( ! class_exists('AMP_Base_Sanitizer') && class_exists('AMPforWP\\AMPVendor\\AMP_Base_Sanitizer') ) {
 	abstract class AMP_Base_Sanitizer extends AMPforWP\AMPVendor\AMP_Base_Sanitizer
@@ -6638,14 +6649,7 @@ if ( ! class_exists('AMP_HTML_Utils') && class_exists('AMPforWP\\AMPVendor\\AMP_
 if ( ! class_exists('AMP_DOM_Utils') && class_exists('AMPforWP\\AMPVendor\\AMP_DOM_Utils') ) {
 	class AMP_DOM_Utils extends AMPforWP\AMPVendor\AMP_DOM_Utils{}
 }
-// Class AMP_Blacklist_Sanitizer
-if ( ! class_exists('AMP_Blacklist_Sanitizer') ) {
-	if(defined('AMP__VENDOR__DIR__')){
-		$amp_blacklist_sanitizer =  realpath( AMP__VENDOR__DIR__ . 'includes/sanitizers/class-amp-blacklist-sanitizer.php') ;
-		include_once $amp_blacklist_sanitizer;
-		class AMP_Blacklist_Sanitizer extends AMPforWP\AMPVendor\AMP_Blacklist_Sanitizer{}
-	} 
-}
+
 // Function is_amp_endpoint
 add_action('pre_amp_render_post', 'ampforwp_is_amp_endpoint_old');
 if ( !function_exists('ampforwp_is_amp_endpoint_old') ) {
