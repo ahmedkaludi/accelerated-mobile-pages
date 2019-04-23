1f748a153984bdbcc67f36313aa249b03776979e
Syed Zabiuddin <syedzabi332@gmail.com>
2019-04-23 13:28:44 +0530

    #2538 Added in Master

--- includes/options/admin-config.php
+++ includes/options/admin-config.php
@@ -1515,6 +1515,7 @@ Redux::setArgs( "redux_builder_amp", $args );
                     'rank_math' => 'Rank Math SEO',
                     'genesis'    => 'Genesis',
                     'seopress'    => 'SEOPress',
+                    'bridge'    => 'Bridge Qode SEO'
                 ),
                 'default'  => ampforwp_seo_default(),
             ),
