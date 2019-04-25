4a288dcdd62cc655c9f36e1951aacf70520e488d
Syed Zabiuddin <syedzabi332@gmail.com>
2019-04-25 13:54:20 +0530

    #2608 Added in Master

--- components/author-box/author-box.php
+++ components/author-box/author-box.php
@@ -105,7 +105,7 @@ if ( isset($args['show_time']) ) {
         </div>
     </div>
 <?php 
-    if(is_singular() && ( isset($args['ads_below_the_author']) && true == $args['ads_below_the_author'] ) && 1 == ampforwp_get_setting('ampforwp-standard-ads-7')){
+    if(is_singular() && ( isset($args['ads_below_the_author']) && true == $args['ads_below_the_author'] )){
             do_action('ampforwp_below_author_box');
         }
     }
