0935f8861888461654772973b3e2e1c457485721
Mohammed Kaludi <mohammed@magazine3.com>
2019-04-26 20:52:48 +0530

    updated the version number
    updated the version number

--- accelerated-moblie-pages.php
+++ accelerated-moblie-pages.php
@@ -3,7 +3,7 @@
 Plugin Name: Accelerated Mobile Pages
 Plugin URI: https://wordpress.org/plugins/accelerated-mobile-pages/
 Description: AMP for WP - Accelerated Mobile Pages for WordPress
-Version: 0.9.97.49-performance-improvements
+Version: 0.9.97.49.1
 Author: Ahmed Kaludi, Mohammed Kaludi
 Author URI: https://ampforwp.com/
 Donate link: https://www.paypal.me/Kaludi/25
