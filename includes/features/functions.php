3119a59c2d639ecaec48242e6e6258d6a6b90ab0
pegazee <abdulazeem.8179@gmail.com>
2019-04-27 19:23:58 +0530

    #2628 Added in master

--- includes/features/functions.php
+++ includes/features/functions.php
@@ -722,6 +722,11 @@ if(!function_exists('ampforwp_amp_nonamp_convert')){
                 $returnData = preg_replace(
                 '/<amp-youtube\sdata-videoid="(.*?)"(.*?)><\/amp-youtube>/',
                  '<iframe src="'. esc_url("https://www.youtube.com/embed/$1").'" style="width:100%;height:360px;" ></iframe>', $returnData);
+                $returnData = preg_replace_callback(
+                '/<amp-iframe(.*?)src="(.*?)"(.*?)><\/amp-iframe>/', 
+                function($matches){
+                    return '<iframe src="'.esc_url($matches[2]).'" style="width:100%;height:400px;" ></iframe>';
+                }, $returnData);
             break;
         }
         return $returnData;
