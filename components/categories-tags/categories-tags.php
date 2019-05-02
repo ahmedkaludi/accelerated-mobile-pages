976511510e049c8a13a32277fc11a735c013537e
Marqas <marqas36@gmail.com>
2019-05-02 16:15:26 +0530

    #2580 Added in master

--- components/categories-tags/categories-tags.php
+++ components/categories-tags/categories-tags.php
@@ -4,35 +4,41 @@ TODO: 1: Connect with options panel(archive support and translational panel)
 2: Do we have to change the classes names as well?
 */
 global $post;
-function ampforwp_framework_get_categories_list(){
+function ampforwp_framework_get_categories_list( $separator = '' ){
 	global $post, $redux_builder_amp;
 	 $ampforwp_categories = get_the_terms( $post->ID, 'category' );
 	 if(ampforwp_get_setting('ampforwp-cats-single') == '1'){
 		if ( $ampforwp_categories ) : ?>
 		<div class="amp-category">
 				<span><?php echo esc_html(ampforwp_translation($redux_builder_amp['amp-translator-categories-text'], 'Categories' )); ?></span>
-				<?php foreach ($ampforwp_categories as $cat ) {
+				<?php foreach ($ampforwp_categories as $key=>$cat ) {
 						if( true == $redux_builder_amp['ampforwp-archive-support'] && true == $redux_builder_amp['ampforwp-cats-tags-links-single']) {
 								echo ('<span class="amp-cat amp-cat-'.esc_attr($cat->term_id).'"><a href="'. ampforwp_url_controller( get_category_link( $cat->term_id ) )   .'" > '. esc_html($cat->name) .'</a></span>');//#934
 						} else {
-							 echo '<span class="amp-cat">'. esc_html($cat->name) .'</span>';
+							 echo '<span class="amp-cat"> '. esc_html($cat->name) .'</span>';
+						}
+						if(!empty($separator) && count($ampforwp_categories)-1 > $key){
+							echo $separator;
 						}
 			} ?>
 		</div>
 	<?php endif; 
 }
 }
-function ampforwp_framework_get_tags_list(){
+function ampforwp_framework_get_tags_list($separator=''){
 	global $post, $redux_builder_amp;
 	 	$ampforwp_tags = get_the_terms( $post->ID, 'post_tag' );
 			if ( $ampforwp_tags && ! is_wp_error( $ampforwp_tags )  ) :?>
 				<div class="amp-tags">
 					<span><?php echo esc_html(ampforwp_translation($redux_builder_amp['amp-translator-tags-text'], 'Tags' )); ?></span>
-					<?php foreach ( $ampforwp_tags as $tag ) {
+					<?php foreach ( $ampforwp_tags as $key=>$tag ) {
 						if( true == $redux_builder_amp['ampforwp-archive-support'] && true == $redux_builder_amp['ampforwp-cats-tags-links-single'] ) {
-                			echo ('<span class="amp-tag amp-tag-'.esc_attr($tag->term_id).'"><a href="'. ampforwp_url_controller( get_tag_link( $tag->term_id ) ).'" >'.esc_html($tag->name) .'</a></span>');//#934
+                			echo ('<span class="amp-tag amp-tag-'.esc_attr($tag->term_id).'"><a href="'. ampforwp_url_controller( get_tag_link( $tag->term_id ) ).'" > '.esc_html($tag->name) .'</a></span>');//#934
 						} else {
-							 	echo ('<span class="amp-tag">'.esc_html($tag->name).'</span>');
+							 	echo ('<span class="amp-tag"> '.esc_html($tag->name).'</span>');
+						}
+						if(!empty($separator) && count($ampforwp_tags)-1 > $key){
+							echo $separator;
 						}
 					}?>
 				</div>
