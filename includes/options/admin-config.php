a8eb26e4453ecf10934727eb464781e995a7a674
Ahmed Kaludi <ahmed@magazine3.com>
2019-05-02 18:49:28 +0530

    Updated again

--- includes/options/admin-config.php
+++ includes/options/admin-config.php
@@ -437,7 +437,7 @@ $extension_listing_array = array(
                             'price'=>'$19',
                             'url_link'=>'https://ampforwp.com/table-of-contents-plus/#utm_source=options-panel&utm_medium=extension-tab_tableofcontent&utm_campaign=AMP%20Plugin',
                             'plugin_active_path'=> 'table-of-content-plus-for-amp/table-of-content-plus-for-amp.php',
-                            'item_name'=>'Table Of Content For AMP',
+                            'item_name'=>'Table Of Content Plus For AMP',
                             'store_url'=>'https://accounts.ampforwp.com',
                             'is_activated'=>(is_plugin_active('table-of-content-plus-for-amp/table-of-content-plus-for-amp.php')? 1 : 2),
                         ),
