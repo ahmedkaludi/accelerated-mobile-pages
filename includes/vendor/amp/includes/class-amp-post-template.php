18a4c8c27e59d4744a3bff278edc17eb26961c6f
Syed Zabiuddin <syedzabi332@gmail.com>
2019-05-08 07:51:02 +0530

    #2959 Added in master

--- includes/vendor/amp/includes/class-amp-post-template.php
+++ includes/vendor/amp/includes/class-amp-post-template.php
@@ -198,8 +198,8 @@ class AMP_Post_Template {
 				'name' => $this->get( 'blog_name' ),
 			),
 			'headline' => $post_title,
-			'datePublished' => date( 'c', $post_publish_timestamp ),
-			'dateModified' => date( 'c', $post_modified_timestamp ),
+			'datePublished' => mysql2date( 'c', $post->post_date_gmt, false ),
+			'dateModified' => mysql2date( 'c', $post->post_modified_gmt, false ),
 			'author' => array(
 				'@type' => 'Person',
 				'name' => $post_author_name,
