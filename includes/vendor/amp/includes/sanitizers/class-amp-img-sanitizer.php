d495aa51fc1efd1c3ba47420501ea3450c8a01bd
Marqas <marqas36@gmail.com>
2019-05-16 16:31:21 +0530

    #3112 Image sanitizer improved

--- includes/vendor/amp/includes/sanitizers/class-amp-img-sanitizer.php
+++ includes/vendor/amp/includes/sanitizers/class-amp-img-sanitizer.php
@@ -94,10 +94,10 @@ class AMP_Img_Sanitizer extends AMP_Base_Sanitizer {
 
 				$width  = isset( $this->args['content_max_width'] ) ? $this->args['content_max_width'] : self::FALLBACK_WIDTH;
 				$height = self::FALLBACK_HEIGHT;
-				if ( isset( $dimensions['width'] ) ) {
+				if ( isset( $dimensions['width'] ) && 0 != $dimensions['width'] ) {
 					$width = $dimensions['width'];
 				}
-				if ( isset( $dimensions['height'] ) ) {
+				if ( isset( $dimensions['height'] ) && 0 != $dimensions['height'] ) {
 					$height = $dimensions['height'];
 				}
 
