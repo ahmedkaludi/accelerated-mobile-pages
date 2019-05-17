af14150b50bb7360394ed98eddde387079c7235a
pegazee <abdulazeem.8179@gmail.com>
2019-05-17 16:26:04 +0530

    #3170 proper code for AMP HTML generate

--- includes/features/functions.php
+++ includes/features/functions.php
@@ -621,8 +621,15 @@ function ampforwp_url_purifier($url){
                 if ( true == $checker && false == strpos($url, $endpointq) )
                     $url =  trailingslashit($url) . $endpointq;
                 else {
-                    if ( false == strpos($url, '/'.$endpoint) )
-                        $url = user_trailingslashit( trailingslashit($url) . $endpoint );
+                    $checker =  explode('/', $url);
+                    $amp_check = in_array($endpoint, $checker); 
+                    if ( false == $amp_check ) {
+                        $url = user_trailingslashit( trailingslashit($url) . $endpoint ); 
+                    }
+                    if ( true == $amp_check ) {
+                        $url =  trailingslashit($url) . $endpointq;
+                        // to debug this code goto https://ampforwp.com/tag/amp
+                    }  
                 }   
             }
         }
