20f46691d25b486fbcee52e1be6cef7ef286b05b
Marqas <marqas36@gmail.com>
2019-05-25 15:47:18 +0530

    #2744 To avoid errors with apostrophe in Swift

--- templates/design-manager/swift/single.php
+++ templates/design-manager/swift/single.php
@@ -285,7 +285,7 @@ do_action("ampforwp_single_design_type_handle");
 										<?php } ?>
 										<?php if($redux_builder_amp['enable-single-twitter-share']){?>
 										<li>
-											<a title="twitter share" class="s_tw" target="_blank" href="https://twitter.com/intent/tweet?url=<?php the_permalink(); ?>&text=<?php echo esc_attr(rawurlencode(get_the_title())); ?>">
+											<a title="twitter share" class="s_tw" target="_blank" href="https://twitter.com/intent/tweet?url=<?php the_permalink(); ?>&text=<?php echo esc_attr(htmlentities(rawurlencode(get_the_title()))); ?>">
 											</a>
 										</li>
 										<?php } ?>
