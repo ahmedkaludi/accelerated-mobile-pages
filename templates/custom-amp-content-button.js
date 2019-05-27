d490ad249bfba7bb8e6e3b9a13b3ac5b29d30722
Marqas <marqas36@gmail.com>
2019-05-27 16:38:18 +0530

    #3193 Code improvement

--- templates/custom-amp-content-button.js
+++ templates/custom-amp-content-button.js
@@ -6,18 +6,23 @@
             classes: 'ampforwp-copy-content-button ', 
             tooltip: 'Visual Editor to AMP Editor', 
             onclick: function() {
-                if(wp.data === undefined){
-                    if(typeof tinymce.editors.content.getContent()!= undefined){
-                        editor.insertContent(tinymce.editors.content.getContent());
-                    }else{
-                        editor.insertContent(document.getElementById('content').value());
-                    }
-                }else{//Gutenberg
-                    if(wp.data){
-                        var editedContent = wp.data.select( "core/editor" ).getEditedPostContent();
-                        editor.insertContent(editedContent);   
-                    }
+              if(wp.data === undefined){
+                if(typeof tinymce.editors.content.getContent()!= undefined){
+                    editor.insertContent(tinymce.editors.content.getContent());
+                }else{
+                    editor.insertContent(document.getElementById('content').value());
                 }
+              }else if(wp.data){
+                if ( wp.data.select( "core/editor" ) != undefined && typeof wp.data.select( "core/editor" ).getEditedPostContent() != undefined ) {
+                    var editedContent = wp.data.select( "core/editor" ).getEditedPostContent();
+                    editor.insertContent(editedContent);
+                }
+                else if( tinymce.editors.content && typeof tinymce.editors.content.getContent()!= undefined){
+                    editor.insertContent(tinymce.editors.content.getContent());
+                }else{
+                    editor.insertContent(document.getElementById('content').value());
+                } 
+              }
             }
         });
     });
