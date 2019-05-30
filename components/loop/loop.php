b848e0470b3e5048b7f995b12f6c81dc17194451
pegazee <abdulazeem.8179@gmail.com>
2019-05-30 17:17:11 +0530

    #3084 layout="fill" added in loop array

--- components/loop/loop.php
+++ components/loop/loop.php
@@ -422,9 +422,11 @@ function amp_loop_image( $data=array() ) {
 			$tag = $data['tag'];
 		}
 
-		if ( isset($data['responsive']) && $data['responsive'] != "" ) {
+		if ( isset($data['responsive']) && $data['responsive'] == "layout=responsive" ) {
 			$layout_responsive = 'layout=responsive';
-			}
+		}elseif (isset($data['responsive']) && $data['responsive'] == "layout=fill" ) {
+			$layout_responsive = 'layout=fill';
+		}
 
 		if ( isset($data['tag_class']) && $data['tag_class'] != "" ) {
 			$tag_class = $data['tag_class'];
