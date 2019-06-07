0c26bcfd56ba1276613596e37b34eacdb5b7eb25
santosh8793 <santosh.veggalam@gmail.com>
2019-06-07 15:07:55 +0530

    #3281 Added code to support more variables in advance google analytics

--- includes/features/analytics/analytics-functions.php
+++ includes/features/analytics/analytics-functions.php
@@ -362,19 +362,46 @@ function ampforwp_add_advance_ga_fields($ga_fields){
 	global $redux_builder_amp, $post;
 	$url = $title = $id = $author_id = $author_name = '';
 	$url = get_the_permalink();
+	$tag_names = array();
 	if(!is_object($post)){ return $ga_fields; }
 	$title = $post->post_title;
 	$id = $post->ID;
+	$category_detail = get_the_category($id);//$post->ID
+	foreach($category_detail as $cd){
+		$category_name = $cd->cat_name;
+	}
+	$tags = get_the_tags( $id );
+	$focusKeyword = '';
+	$seoScore = '';
+	if ( is_plugin_active('wordpress-seo/wp-seo.php') ) {
+		$focusKeyword = get_post_meta($id, '_yoast_wpseo_focuskw', true); 
+		$seoScore = get_post_meta($id, '_yoast_wpseo_content_score', true); 
+	}
+	$tagNames = '';
+	if( !empty($tags) ){
+	    foreach( $tags as $tag ) {
+	    	$tag_names[] = $tag->name;
+	    }
+	    $tagNames = implode( ', ', $tag_names );
+	}
 	$author_id = $post->post_author;
 	$author_name = get_the_author_meta( 'display_name' , $author_id );
+	$published_at = get_the_date( 'l F j, Y' , $id );
 	$ampforwp_adv_ga_fields = array();
 	$ampforwp_adv_ga_fields = ampforwp_get_setting('ampforwp-ga-field-advance');
 	if($ampforwp_adv_ga_fields)	{
 		$ampforwp_adv_ga_fields = str_replace('{url}', $url, $ampforwp_adv_ga_fields);
 		$ampforwp_adv_ga_fields = str_replace('{id}', $id, $ampforwp_adv_ga_fields);
 		$ampforwp_adv_ga_fields = str_replace('{title}', $title, $ampforwp_adv_ga_fields);
-		$ampforwp_adv_ga_fields = str_replace('{author_name}', $author_name, $ampforwp_adv_ga_fields);
 		$ampforwp_adv_ga_fields = str_replace('{author_id}', $author_id, $ampforwp_adv_ga_fields);
+		$ampforwp_adv_ga_fields = str_replace('{author_name}', $author_name, $ampforwp_adv_ga_fields);
+		$ampforwp_adv_ga_fields = str_replace('{category}', $category_name, $ampforwp_adv_ga_fields);
+		$ampforwp_adv_ga_fields = str_replace('{published_at}', $published_at, $ampforwp_adv_ga_fields);
+		$ampforwp_adv_ga_fields = str_replace('{tags}', $tagNames, $ampforwp_adv_ga_fields);
+		if ( is_plugin_active('wordpress-seo/wp-seo.php') ) {
+			$ampforwp_adv_ga_fields = str_replace('{seo_score}', $seoScore, $ampforwp_adv_ga_fields);
+			$ampforwp_adv_ga_fields = str_replace('{focus_keyword}', $focusKeyword, $ampforwp_adv_ga_fields);
+		}
 		return $ampforwp_adv_ga_fields;
 	}	
 	return $ga_fields;	
