176e03f9b6c64a627ce1c15c5977335f80ddae89
Syed Zabiuddin <32957211+Zabi09@users.noreply.github.com>
2019-06-11 13:00:33 +0530

    #2996 properly added for other designs

--- templates/design-manager/design-2/elements/featured-image.php
+++ templates/design-manager/design-2/elements/featured-image.php
@@ -6,6 +6,7 @@ $caption = "";
 $featured_image = "";
 $featured_image = $this->get( 'featured_image' );
 if( $featured_image || ( ampforwp_is_custom_field_featured_image() && ampforwp_cf_featured_image_src() ) || true == $redux_builder_amp['ampforwp-featured-image-from-content'] || (function_exists('has_post_video') && has_post_video($post->ID)) ) {
+	ampforwp_webp_featured_image();
 	// Featured Video SmartMag theme Compatibility #2559
 	if(class_exists('Bunyad') && Bunyad::posts()->meta('featured_video') ){
 			global $wp_embed;
@@ -44,7 +45,5 @@ if( $featured_image || ( ampforwp_is_custom_field_featured_image() && ampforwp_c
 				</figure>
 			</div> <?php
 		}
-}else{
-		ampforwp_webp_featured_image();
-	}
+}
 do_action('ampforwp_after_featured_image_hook',$this); ?>
