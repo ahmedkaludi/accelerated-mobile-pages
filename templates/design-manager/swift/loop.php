38f5d16b43db2838c2295a4fe751c3a13bbf2895
Marqas <marqas36@gmail.com>
2019-06-14 16:30:46 +0530

    #3309 Some improvement in code as it was giving some errors with single result

--- templates/design-manager/swift/loop.php
+++ templates/design-manager/swift/loop.php
@@ -1,11 +1,12 @@
-<?php global $redux_builder_amp; 
+<?php global $redux_builder_amp, $search_found; 
 $width  = 346;
 $height = 188;
 if( true == $redux_builder_amp['ampforwp-homepage-posts-image-modify-size'] ){
     $width  = $redux_builder_amp['ampforwp-swift-homepage-posts-width'];
     $height = $redux_builder_amp['ampforwp-swift-homepage-posts-height'];
 } ?>
-<?php while(amp_loop('start')): ?>
+<?php while(amp_loop('start')): 
+$search_found = true;   ?>
 <div class="fsp">
 	<?php if(ampforwp_has_post_thumbnail()){ $args = array("tag"=>'div',"tag_class"=>'image-container','image_size'=>'full','image_crop'=>'true','image_crop_width'=>$width,'image_crop_height'=>$height, 'responsive'=> true); ?>
     <div class="fsp-img">
