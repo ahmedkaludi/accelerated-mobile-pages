668d97043117994c4850862f6b8ee7e3f0a5f918
Mohammed Kaludi <mohammed@magazine3.com>
2019-06-14 19:48:07 +0530

    #2996 Empty varailbe created
    #2996 Empty varailbe created

--- templates/design-manager/design-1/elements/featured-image.php
+++ templates/design-manager/design-1/elements/featured-image.php
@@ -5,7 +5,7 @@ $amp_html 		= "";
 $caption 		= "";
 $featured_image = "";
 $featured_image = $this->get( 'featured_image' );
-$get_webp = "";
+$get_webp = $get_webp_type =  "";
 $get_webp = get_post_thumbnail_id($post->ID);
 $get_webp_type =  get_post_mime_type( $get_webp );
 if($featured_image || ( ampforwp_is_custom_field_featured_image() && ampforwp_cf_featured_image_src() ) || true == $redux_builder_amp['ampforwp-featured-image-from-content'] || (function_exists('has_post_video') && has_post_video($post->ID)) ){
