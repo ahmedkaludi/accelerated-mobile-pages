b6ebabff9640e86b037c9162a586f7aa7581e248
Pegazee <abdulazeem.8179@gmail.com>
2019-06-27 15:49:37 +0530

    #3341 ampforwp_amphtml_generator() code improved

--- templates/features.php
+++ templates/features.php
@@ -198,18 +198,17 @@ define('AMPFORWP_COMMENTS_PER_PAGE',  ampforwp_define_comments_number() );
 	}
 
 	function ampforwp_amphtml_generator(){
-		global $redux_builder_amp;
 		global $wp, $post;
 		$post_id = '';
 		$endpoint_check = false;
-		$endpoint_check = $redux_builder_amp['amp-core-end-point'];
+		$endpoint_check = ampforwp_get_setting('amp-core-end-point');
 	    if( is_attachment() ) {
         return;
 	    }
-	    if( is_home() && is_front_page() && !$redux_builder_amp['ampforwp-homepage-on-off-support'] ) {
+	    if( is_home() && is_front_page() && !ampforwp_get_setting('ampforwp-homepage-on-off-support') ) {
         return;
 	    }
-	    if( is_front_page() && ! $redux_builder_amp['ampforwp-homepage-on-off-support'] ) {
+	    if( is_front_page() && ! ampforwp_get_setting('ampforwp-homepage-on-off-support') ) {
         return;
 	    }
 	    if ( is_archive() && !is_tax() ) {
@@ -261,14 +260,14 @@ define('AMPFORWP_COMMENTS_PER_PAGE',  ampforwp_define_comments_number() );
 		if ( is_category_amp_disabled() ) {
 			return;
 		}
-      	if ( is_page() && ! $redux_builder_amp['amp-on-off-for-all-pages'] && ! is_home() && ! is_front_page() ) {
+      	if ( is_page() && ! ampforwp_get_setting('amp-on-off-for-all-pages') && ! is_home() && ! is_front_page() ) {
 			return;
 		}
-		if ( is_home() && ! ampforwp_is_blog() && !$redux_builder_amp['ampforwp-homepage-on-off-support'] ) {
+		if ( is_home() && ! ampforwp_is_blog() && !ampforwp_get_setting('ampforwp-homepage-on-off-support') ) {
 			return;
 		}
 
-		if ( ampforwp_is_blog() && ! $redux_builder_amp['amp-on-off-for-all-pages'] ) {
+		if ( ampforwp_is_blog() && ! ampforwp_get_setting('amp-on-off-for-all-pages') ) {
 			return;
 		}
 			$query_arg_array = $wp->query_vars;
@@ -313,12 +312,9 @@ define('AMPFORWP_COMMENTS_PER_PAGE',  ampforwp_define_comments_number() );
 
 			$type = get_post_type();
 			if(is_home() || is_front_page()){
-		      if(isset($redux_builder_amp['ampforwp-homepage-on-off-support']) 
-		          && $redux_builder_amp['ampforwp-homepage-on-off-support'] == 1 
-		          && isset($redux_builder_amp['amp-on-off-for-all-posts']) 
-		          && $redux_builder_amp['amp-on-off-for-all-posts'] == 0 
-		          && isset($redux_builder_amp['amp-on-off-for-all-pages']) 
-		          && $redux_builder_amp['amp-on-off-for-all-pages'] == 0 ){
+		      if( ampforwp_get_setting('ampforwp-homepage-on-off-support') == 1 
+		          && ampforwp_get_setting('amp-on-off-for-all-posts') == 0 
+		          && ampforwp_get_setting('amp-on-off-for-all-pages') == 0 ){
 
                   $supported_types['post'] = 'post';
       			}
@@ -350,46 +346,9 @@ define('AMPFORWP_COMMENTS_PER_PAGE',  ampforwp_define_comments_number() );
 			if( is_search() ) {
 				$current_search_url =trailingslashit(get_home_url())."?amp=1&s=".get_search_query();
 				$amp_url = untrailingslashit($current_search_url);
-			}
-			// WPML compatibility
-			if( class_exists('SitePress') ){
-				if( get_option('permalink_structure') ){
-					global $sitepress_settings, $wp;
-					if($sitepress_settings[ 'language_negotiation_type' ] == 3){
-					  	if( is_singular() ){
-							$wpml_url =get_permalink( get_queried_object_id() );
-							$wpml_url = untrailingslashit($wpml_url);
-							$explode_url = explode('/', $wpml_url);
-							$append_amp = AMPFORWP_AMP_QUERY_VAR;
-							array_splice( $explode_url, count($explode_url), 0, $append_amp );
-							$impode_url = implode('/', $explode_url);
-							$amp_url = untrailingslashit($impode_url);
-					    }
-					    if ( is_home()  || is_archive() ){
-					        global $wp;
-					        $current_archive_url = home_url( $wp->request );
-							$explode_path  	= explode("/",$current_archive_url);
-							$inserted 		= array(AMPFORWP_AMP_QUERY_VAR);
-							$query_arg_array = $wp->query_vars;
-							if( array_key_exists( 'paged' , $query_arg_array ) ) {
-								array_splice( $explode_path, count($explode_path), 0, $inserted );
-							}
-							else{
-								array_splice( $explode_path, count($explode_path), 0, $inserted );
-							}
-							$impode_url = implode('/', $explode_path);
-							$amp_url = $impode_url;
-					    }
-					}else{
-						$amp_url = ampforwp_url_purifier($amp_url);
-					}
-				}
-			}
-			if( !class_exists('SitePress') ){
-				// URL Purifier
-				$amp_url = ampforwp_url_purifier($amp_url);
-			}
-			
+			} 
+			// URL Purifier
+			$amp_url = ampforwp_url_purifier($amp_url);
 	        $amp_url = apply_filters('ampforwp_modify_rel_canonical',$amp_url);
 
 	        if( $supported_amp_post_types) {					
