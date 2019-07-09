f828ac6094a2cd8fd24baafc3b9600752826eea0
Marqas <marqas36@gmail.com>
2019-07-09 15:23:05 +0530

    #3000 Remove unused variable

--- templates/features.php
+++ templates/features.php
@@ -6743,7 +6743,7 @@ add_filter('get_search_form', 'ampforwp_search_form');
 if ( ! function_exists('ampforwp_search_form') ) {
 	function ampforwp_search_form($form){
 		if ( ampforwp_is_amp_endpoint() ) {	
-			$form = '<form role="search" ' . $aria_label . 'method="get" class="search-form" action="' . esc_url( home_url( '/' ) ) . '">
+			$form = '<form role="search" method="get" class="search-form" action="' . esc_url( home_url( '/' ) ) . '">
 						<label>
 							<span class="screen-reader-text">' . _x( 'Search for:', 'label' ) . '</span>
 							<input type="search" class="search-field" placeholder="' . esc_attr_x( 'Search &hellip;', 'placeholder' ) . '" value="' . get_search_query() . '" name="s" />
