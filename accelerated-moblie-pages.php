ecf5a7fd2da2a44c8952aef04bbaa80d86970a9f
Marqas <marqas36@gmail.com>
2019-07-15 19:12:19 +0530

    #3415 To avoid is_amp_endpoint error with Ads for WP plugin

--- accelerated-moblie-pages.php
+++ accelerated-moblie-pages.php
@@ -1183,7 +1183,7 @@ function ampforwp_automattic_notice_delete(){
 // is_amp_endpoint Fallback #2287 #3055
 add_action('parse_query','ampforwp_vendor_is_amp_endpoint'); 
 function ampforwp_vendor_is_amp_endpoint(){
-	if ( ! function_exists('amp_activate') && ! function_exists('is_amp_endpoint' ) ) {
+	if ( ! function_exists('amp_activate') && ! function_exists('is_amp_endpoint' ) && ! defined('ADSFORWP_PLUGIN_DIR') ) {
 		function is_amp_endpoint(){
 			return false !== get_query_var( AMP_QUERY_VAR, false );
 		}
