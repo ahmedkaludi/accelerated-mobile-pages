016a32deef57ee13ec359639b0391eb8bce2085b
santosh8793 <santosh.veggalam@gmail.com>
2019-07-16 17:53:05 +0530

    #3449 Added code to load message in AMP option panel

--- includes/features/performance/performance-functions.php
+++ includes/features/performance/performance-functions.php
@@ -82,6 +82,7 @@ if( true == ampforwp_get_setting('ampforwp_leverage_browser_caching_mode')){
 }
 
 function ampforwp_leverage_browser_caching_remove(){
+
     $htaccess_file = wp_normalize_path( ABSPATH . '.htaccess' );
     if ( file_exists( $htaccess_file ) ) {
 
@@ -107,6 +108,7 @@ function ampforwp_leverage_browser_caching_remove(){
     }
 }
 function ampforwp_leverage_browser_caching(){
+    global $pagenow;
     $htaccess_file = wp_normalize_path( ABSPATH . '.htaccess' );
     if ( file_exists( $htaccess_file ) ) {
         if ( is_readable( $htaccess_file ) && is_writable( $htaccess_file ) ) {
@@ -121,31 +123,28 @@ function ampforwp_leverage_browser_caching(){
                 file_put_contents( $htaccess_file, $htaccess_cntn );
             }
         } else {
-            add_action( 'admin_notices', 'ampforwp_no_htaccess_access_notice' );
+            if( $pagenow == 'admin.php' && isset($_GET['page']) && esc_attr($_GET['page']) == 'amp_options'){
+                add_action( 'admin_notices', 'ampforwp_no_htaccess_access_notice' );
+            }
         }
     }else {
-        add_action( 'admin_notices', 'ampforwp_no_htaccess_notice');
+        if( $pagenow == 'admin.php' && isset($_GET['page']) && esc_attr($_GET['page']) == 'amp_options'){
+            add_action( 'admin_notices', 'ampforwp_no_htaccess_notice');
+        }
     }
 }
 
 function ampforwp_no_htaccess_access_notice(){
-    global $pagenow;
-    if( $pagenow == 'admin.php' && isset($_GET['page']) && $_GET['page'] == 'amp_options'){
-        $message = '<div class="error"><p>';
-        $message .= esc_html__( 'Accelerated Mobile Pages: htaccess file is not readable or writable for Leverage Browser Caching. Please change permission of htaccess file.', 'accelerated-mobile-pages' );
-        $message .= '</p></div>';
-        echo wp_kses_post( $message );
-    }
-    
+    $message = '<div class="error"><p>';
+    $message .= esc_html__( 'Accelerated Mobile Pages: htaccess file is not readable or writable for Leverage Browser Caching. Please change permission of htaccess file.', 'accelerated-mobile-pages' );
+    $message .= '</p></div>';
+    echo wp_kses_post( $message );
 }
 function ampforwp_no_htaccess_notice(){
-    global $pagenow;
-    if( $pagenow == 'admin.php' && isset($_GET['page']) && $_GET['page'] == 'amp_options'){
-        $message = '<div class="error"><p>';
-        $message .= esc_html__( 'Accelerated Mobile Pages: htaccess file not found. Please create htaccess in your root folder. Leverage Browser Caching works only for Apache server at this moment. If you are using NginX then please disable Leverage Browser Caching from AMP options panel -> Performance -> Leverage Browser Caching' );
-        $message .= '</p></div>';
-        echo wp_kses_post( $message );
-    }
+    $message = '<div class="error"><p>';
+    $message .= esc_html__( 'Accelerated Mobile Pages: htaccess file not found. Please create htaccess in your root folder. Leverage Browser Caching works only for Apache server at this moment. If you are using NginX then please disable Leverage Browser Caching from AMP options panel -> Performance -> Leverage Browser Caching' );
+    $message .= '</p></div>';
+    echo wp_kses_post( $message );
 }
 function ampforwp_code_to_add_in_htaccess(){
     $htaccess_cntn  = "\n";
