df6b21ded6f1c8a1f4ba1e71dc6af666e454f4f7
Syed Zabiuddin <syedzabi332@gmail.com>
2019-07-20 15:50:54 +0530

    #3455 Validation errors in amp category page fixed

--- includes/thirdparty-compatibility.php
+++ includes/thirdparty-compatibility.php
@@ -29,6 +29,12 @@ function ampforwp_thirdparty_compatibility(){
 	remove_action( 'amp_post_template_css', 'thb_amp_additional_css_styles' );
 	//Menu css is not loading when directory plus theme is active. #2963
 	remove_filter('wp_nav_menu_args',array('AitMenu','modify_arguments'),100);
+	//Validation errors in amp category page due to HotWP PRO #3455
+	if(function_exists('hotwp_get_option')){
+		add_filter('single_cat_title' , 'ampforwp_hotwp_remove_cat_title' );
+		add_action( 'ampforwp_after_header' , 'ampforwp_hotwp_add_cat_title');
+		add_action( 'below_the_header_design_1' , 'ampforwp_hotwp_add_cat_title');
+	}
 	/**
 	* toc 
 	**/
@@ -1367,4 +1373,8 @@ function ampforwp_safia_theme($content){
 		ob_get_clean();
 		$content = $videoContent.$content;
 	return $content;
+}
+function ampforwp_hotwp_add_cat_title(){
+	$category = get_the_category();
+	echo '<div class="cntr amp-wp-content amp-wp-article"><h2 class="amp-archive-title"><br><br>' . $category[0]->cat_name . '</h2></div>';
 }
