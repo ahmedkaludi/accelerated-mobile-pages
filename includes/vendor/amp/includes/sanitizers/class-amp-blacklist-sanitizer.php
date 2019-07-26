8133241f6ca20b9f567b5b0156b919cce8889b03
santosh8793 <santosh.veggalam@gmail.com>
2019-07-26 17:44:18 +0530

    #3483 Added conditional code before getting rel attribute

--- includes/vendor/amp/includes/sanitizers/class-amp-blacklist-sanitizer.php
+++ includes/vendor/amp/includes/sanitizers/class-amp-blacklist-sanitizer.php
@@ -40,14 +40,16 @@ class AMP_Blacklist_Sanitizer extends AMP_Base_Sanitizer {
 
 		if($node->nodeName=='a' && $node->hasAttribute('href')){
 			$href = $node->getAttribute('href');
-			$rel = $node->getAttribute('rel');
 			$node->setAttribute('href', \ampforwp_findInternalUrl($href));
 			// Adding rel="noreferrer" to external links to prevent security vulnerabilities #3276
 			if ( \ampforwp_isexternal($href) ) {
-				if(strpos($rel, 'nofollow') !== false){
-					$node->setAttribute('rel', 'nofollow noreferrer');
-				}else{
-					$node->setAttribute('rel', 'noreferrer');
+				if ($node->hasAttribute('rel') ){
+					$rel = $node->getAttribute('rel');
+					if(strpos($rel, 'nofollow') !== false){
+						$node->setAttribute('rel', 'nofollow noreferrer');
+					}else{
+						$node->setAttribute('rel', 'noreferrer');
+					}
 				}
 				
 			}
