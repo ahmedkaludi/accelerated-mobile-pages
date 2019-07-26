be37177b25d3553a766742a5659a33e1498fb8f3
santosh8793 <santosh.veggalam@gmail.com>
2019-07-26 16:49:23 +0530

    #3495 Added code to show title only when it is not empty

--- components/components-core.php
+++ components/components-core.php
@@ -115,12 +115,14 @@ function amp_title(){
 	else
 		$ID = $post->ID;
 	if( $ID!=null ){
-		do_action('ampforwp_above_the_title'); 
-		?><h1 class="amp-post-title"><?php 
-			$ampforwp_title = get_the_title($ID);
-			$ampforwp_title =  apply_filters('ampforwp_filter_single_title', $ampforwp_title);
-			echo wp_kses_data( $ampforwp_title ); 
-		?></h1><?php 
+		do_action('ampforwp_above_the_title');
+		$ampforwp_title = get_the_title($ID);
+		$ampforwp_title =  apply_filters('ampforwp_filter_single_title', $ampforwp_title);
+		if(!empty($ampforwp_title)){
+		?>
+		<h1 class="amp-post-title"><?php echo wp_kses_data( $ampforwp_title ); ?></h1>
+		<?php
+		}
 		do_action('ampforwp_below_the_title');
     }
 }
--- templates/design-manager/design-3/elements/title.php
+++ templates/design-manager/design-3/elements/title.php
@@ -1,10 +1,15 @@
 <?php global $redux_builder_amp;
 do_action('ampforwp_above_the_title',$this); ?>
 <header class="amp-wp-content amp-wp-article-header ampforwp-title">
-	<h1 class="amp-wp-title"><?php 
-		$ampforwp_title = $this->get( 'post_title' ) ;
-		$ampforwp_title =  apply_filters('ampforwp_filter_single_title', $ampforwp_title);
-		echo wp_kses_data( $ampforwp_title );?></h1>
+	<?php
+	$ampforwp_title = $this->get( 'post_title' ) ;
+	$ampforwp_title =  apply_filters('ampforwp_filter_single_title', $ampforwp_title);
+	if(!empty($ampforwp_title)){
+	?>
+	<h1 class="amp-wp-title"><?php echo wp_kses_data( $ampforwp_title );?></h1>
+	<?php
+	}
+	?>
 	<?php if( array_key_exists( 'enable-excerpt-single' , $redux_builder_amp ) ) {
 				if($redux_builder_amp['enable-excerpt-single']) {
 					if( has_excerpt() ){ ?>
