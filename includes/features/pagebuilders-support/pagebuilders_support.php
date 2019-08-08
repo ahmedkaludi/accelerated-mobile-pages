f822283dbd13c9774a5f21fdf0b4e185883dd767
Ajeet <ajeet.gupta51@gmail.com>
2019-08-08 15:48:37 +0530

    #3550 added Avada compatibility parent in pagebuilder section

--- includes/features/pagebuilders-support/pagebuilders_support.php
+++ includes/features/pagebuilders-support/pagebuilders_support.php
@@ -26,6 +26,14 @@ $elemntr_pb_for_ampchecker = array(
                     ),
                  'desc' => sprintf('<div style="background: #FFF9C4;padding: 12px;line-height: 1.6;margin: -45px -14px -18px -17px;"><b>%s</b> %s <a href="https://ampforwp.com/page-builder-compatibility-for-amp/" target="_blank">%s</a> extension.<br /> <div style="margin-top:4px;">(<a href="https://ampforwp.com/page-builder-compatibility-for-amp/" target="_blank">%s</a>)</div></div>',esc_html__( 'ONE LAST STEP REQUIRED:','accelerated-mobile-pages'),esc_html__( 'This feature requires', 'accelerated-mobile-pages' ),esc_html__( 'Page Builder For AMP', 'accelerated-mobile-pages'),esc_html__( 'Click here for more info', 'accelerated-mobile-pages' )),               
            );
+$avada_pb_for_ampchecker = array( 
+                'id'   => 'avada_pb_for_amp_info_normal',
+                'type' => 'info',
+                'required' => array(
+                    array('ampforwp-avada-pb-for-amp', '=' , true),  
+                    ),
+                 'desc' => sprintf('<div style="background: #FFF9C4;padding: 12px;line-height: 1.6;margin: -45px -14px -18px -17px;"><b>%s</b> %s <a href="https://ampforwp.com/page-builder-compatibility-for-amp/" target="_blank">%s</a> extension.<br /> <div style="margin-top:4px;">(<a href="https://ampforwp.com/page-builder-compatibility-for-amp/" target="_blank">%s</a>)</div></div>',esc_html__( 'ONE LAST STEP REQUIRED:','accelerated-mobile-pages'),esc_html__( 'This feature requires', 'accelerated-mobile-pages' ),esc_html__( 'Page Builder compatibility For AMP', 'accelerated-mobile-pages'),esc_html__( 'Click here for more info', 'accelerated-mobile-pages' )),               
+           );
   if( !function_exists('amp_activate') ){
      $pb_for_amp[] =  array(
                 'id' => 'ampforwp-pagebuilder-accor',
@@ -92,10 +100,26 @@ $elemntr_pb_for_ampchecker = array(
                'default'  => false
             ); 
     $pb_for_amp[] = $elemntr_pb_for_ampchecker;
+    $pb_for_amp[] =  array(
+                'id' => 'ampforwp-avada-pb-for-amp-accor',
+                'type' => 'section',
+                'title' => esc_html__('AMP Avada (Fusion builder) Compatibility', 'accelerated-mobile-pages'),
+                'indent' => true,
+                'layout_type' => 'accordion',
+                'accordion-open'=> 1, 
+            );
+    $pb_for_amp[] = array(
+               'id'       => 'ampforwp-avada-pb-for-amp',
+               'type'     => 'switch',
+               'title'    => esc_html__('AMP Avada (Fusion builder) (BETA)','accelerated-mobile-pages'),
+               'tooltip-subtitle' => esc_html__('Enable or Disable the Elementor support for AMP', 'accelerated-mobile-pages'),
+               'default'  => false
+            ); 
+    $pb_for_amp[] = $avada_pb_for_ampchecker;
 
   $pb_title = 'Page Builder';
   $theme = wp_get_theme(); // gets the current theme
-  if( class_exists('Vc_Manager') || ( class_exists('ET_Builder_Plugin') || 'Divi' == $theme->name || 'Divi' == $theme->parent_theme ) || did_action( 'elementor/loaded' ) ){
+  if( class_exists('Vc_Manager') || ( class_exists('ET_Builder_Plugin') || 'Divi' == $theme->name || 'Divi' == $theme->parent_theme ) || did_action( 'elementor/loaded' ) || class_exists( 'FusionBuilder' )  ){
      if(class_exists('Vc_Manager') ){
          $pb_title =  'WPBakery Page Builder Support';
       }
@@ -104,6 +128,9 @@ $elemntr_pb_for_ampchecker = array(
       }
       if(did_action( 'elementor/loaded' ) ){
           $pb_title =  'Elementor Support';
+      }
+      if ( class_exists( 'FusionBuilder' ) ) {
+        $pb_title =  'Avada Fusion Builder Support';
       }
   }
 Redux::setSection( $opt_name, array(
