9b5b2bdc0783f4089485a8370e8fdb148fbd51a4
Syed Zabiuddin <syedzabi332@gmail.com>
2019-08-20 13:33:25 +0530

    #3253 Code improvements

--- components/post-navigation/post-navigation.php
+++ components/post-navigation/post-navigation.php
@@ -6,23 +6,25 @@ function ampforwp_framework_get_post_navigation(){
 					<div class="next">
 					<?php $next_post = get_next_post();
 						if (!empty( $next_post )) {
-						$next_text = $next_post->post_title;	
-						if(false == ampforwp_get_setting('single-next-prev-to-nonamp')){ ?>
-						<a href="<?php echo ampforwp_url_controller( get_permalink( $next_post->ID ) ); ?>"><span><?php echo ampforwp_translation($redux_builder_amp['amp-translator-next-text'], 'Next' ); ?></span><?php echo apply_filters('ampforwp_next_link',$next_text ); ?> &raquo;</a> <?php
-						} else { ?>
-						<a href="<?php echo get_permalink( $next_post->ID ); ?>"><span><?php echo ampforwp_translation(ampforwp_get_setting('amp-translator-next-text'), 'Next' ); ?></span><?php echo apply_filters('ampforwp_next_link',$next_text ); ?> &raquo;</a>	
-						<?php } } ?>
+						$next_text = $next_post->post_title;
+						$next_link = ampforwp_url_controller( get_permalink( $next_post->ID ));
+						if(true == ampforwp_get_setting('single-next-prev-to-nonamp')){
+						$next_link = get_permalink( $next_post->ID );
+						} ?>
+						<a href="<?php echo esc_url($next_link); ?>"><span><?php echo ampforwp_translation(ampforwp_get_setting('amp-translator-next-text'), 'Next' ); ?></span><?php echo apply_filters('ampforwp_next_link',$next_text ); ?> &raquo;</a> <?php
+						} ?>
 					</div>
 
 					<div class="prev">
 					<?php $prev_post = get_previous_post();
 					 if (!empty( $prev_post )) {
 						 $prev_text = $prev_post->post_title;
-						 if(false == ampforwp_get_setting('single-next-prev-to-nonamp')){ ?>
-					    <a href="<?php echo ampforwp_url_controller( get_permalink( $prev_post->ID ) ); ?>"><span><?php echo ampforwp_translation(ampforwp_get_setting('amp-translator-previous-text'), 'Previous' ); ?></span> &laquo; <?php echo apply_filters('ampforwp_prev_link',$prev_text ); ?></a> <?php
-					 } else { ?>
-					 	<a href="<?php echo get_permalink( $prev_post->ID ); ?>"><span><?php echo ampforwp_translation(ampforwp_get_setting('amp-translator-previous-text'), 'Previous' ); ?></span> &laquo; <?php echo apply_filters('ampforwp_prev_link',$prev_text ); ?></a>
-					 <?php } } ?>	
+						 $prev_link = ampforwp_url_controller( get_permalink( $prev_post->ID ));
+						 if(true == ampforwp_get_setting('single-next-prev-to-nonamp')){
+						    $prev_link = get_permalink( $prev_post->ID );
+						 } ?>	 
+					    <a href="<?php echo esc_url($prev_link); ?>"><span><?php echo ampforwp_translation(ampforwp_get_setting('amp-translator-previous-text'), 'Previous' ); ?></span> &laquo; <?php echo apply_filters('ampforwp_prev_link',$prev_text ); ?></a> <?php
+					  } ?>	
 					</div>
 
 					<div class="clearfix"></div>
--- templates/design-manager/design-2/elements/content.php
+++ templates/design-manager/design-2/elements/content.php
@@ -17,11 +17,11 @@
 						<?php $next_post = get_next_post();
 							if (!empty( $next_post )) {
 								$next_text = $next_post->post_title;
-								if(false == ampforwp_get_setting('single-next-prev-to-nonamp')){ ?>
-									<a href="<?php echo ampforwp_url_controller( get_permalink( $next_post->ID ) ); ?>"><?php echo apply_filters('ampforwp_next_link',$next_text ); ?> &raquo;</a> <?php
-								} else{ ?>
-					 				<a href="<?php echo get_permalink( $next_post->ID ); ?>"><?php echo apply_filters('ampforwp_next_link',$next_text ); ?> &raquo;</a>
-                 				 <?php } } ?>
+								$next_link = ampforwp_url_controller( get_permalink( $next_post->ID ));
+								if(true == ampforwp_get_setting('single-next-prev-to-nonamp')){
+								$next_link = get_permalink( $next_post->ID );
+								} ?>
+									<a href="<?php echo esc_url($next_link); ?>"><?php echo apply_filters('ampforwp_next_link',$next_text ); ?> &raquo;</a> <?php } ?>
 					</div>
 					<!--Next Link code-->
 
@@ -30,11 +30,11 @@
 							<?php $prev_post = get_previous_post();
 								 if (!empty( $prev_post )) {
 									 $prev_text = $prev_post->post_title;
-									if(false == ampforwp_get_setting('single-next-prev-to-nonamp')){ ?>
-								   <a href="<?php echo ampforwp_url_controller( get_permalink( $prev_post->ID ) ); ?>"> &laquo; <?php echo apply_filters('ampforwp_prev_link',$prev_text ); ?></a> <?php
-								} else{ ?>   
-									<a href="<?php echo get_permalink( $prev_post->ID ); ?>"> &laquo; <?php echo apply_filters('ampforwp_prev_link',$prev_text ); ?></a>
-								<?php } } ?>
+									$prev_link = ampforwp_url_controller( get_permalink( $prev_post->ID ));
+									 if(true == ampforwp_get_setting('single-next-prev-to-nonamp')){
+									    $prev_link = get_permalink( $prev_post->ID );
+									 } ?>
+								   <a href="<?php echo esc_url($prev_link); ?>"> &laquo; <?php echo apply_filters('ampforwp_prev_link',$prev_text ); ?></a> <?php } ?>
 					</div>
 					<!--Prev Link code-->
 
