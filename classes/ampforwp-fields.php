b287be05afea9bf9c6b2517267de1d6802aa05f9
Marqas <marqas36@gmail.com>
2019-08-23 12:36:00 +0530

    #3520 New method of creating the options

---
+++ classes/ampforwp-fields.php
@@ -0,0 +1,9 @@
+<?php
+require_once AMPFORWP_PLUGIN_DIR."classes/class-ampforwp-fields.php";
+$ampforwp_fields = new AMPforWP_Fields();
+$ampforwp_fields->section_start(array('id'=>'ampforwp-ux-website-type-section','class'=>'section-1'));
+$ampforwp_fields->setField('select',array('title'=>'Select','class'=>'select_class','id'=>'select_id','options'=>array('Blog'=>'Blog','News'=>'News'),'default'=>'Blog'));
+$ampforwp_fields->setField('checkbox',array('title'=>'Checkbox','class'=>'checkbox_class','id'=>'checkbox_id'));
+$ampforwp_fields->setField('switch',array('title'=>'switch','class'=>'switch_class','id'=>'switch_id'));
+$ampforwp_fields->section_end();
+ ?>
