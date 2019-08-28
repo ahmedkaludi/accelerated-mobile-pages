12cc55aa34b313f5a51a304a52e44aa567b20eae
Pegazee <abdulazeem.8179@gmail.com>
2019-08-28 15:05:40 +0530

    #3537 to work with homepage loop

--- templates/features.php
+++ templates/features.php
@@ -308,7 +308,7 @@ define('AMPFORWP_COMMENTS_PER_PAGE',  ampforwp_define_comments_number() );
 				if ( (is_home() || is_archive()) && $wp->query_vars['paged'] >= '2' ) {
 					$new_url 		=  home_url('/');
 					// If its custom permalink with /index.php/ #3537
-			        if ( is_archive() && false !== strpos($wp->matched_rule, 'index.php') && false === strpos( home_url( $wp->request ), 'index.php') ) {
+			        if ( (is_home() || is_archive()) && false !== strpos($wp->matched_rule, 'index.php') && false === strpos( home_url( $wp->request ), 'index.php') ) {
 						$new_url = home_url( 'index.php' );
 						$new_url = trailingslashit($new_url);
 					}
