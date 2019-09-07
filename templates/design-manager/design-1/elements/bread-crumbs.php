4b31832f11bbf65afd83689be19efa965aeb2425
Syed Zabiuddin <syedzabi332@gmail.com>
2019-09-07 17:39:54 +0530

    #3598 Added in 98.5

--- templates/design-manager/design-1/elements/bread-crumbs.php
+++ templates/design-manager/design-1/elements/bread-crumbs.php
@@ -111,16 +111,16 @@ if ( ( (is_single() && 1 == ampforwp_get_setting('ampforwp-bread-crumb')) || (is
                     $last_category = end($last_category);
                       $category_name = get_category($last_category);
                     // Get parent any categories and create array
-                    $get_cat_parents = rtrim(get_category_parents($last_category->term_id, false, ','),',');
+                    $get_cat_parents = rtrim(get_category_parents($last_category->term_id, false, '/'),'/');
                     if(class_exists( 'WPSEO_Options' )){
                         $primary_cateogory = get_post_meta(ampforwp_get_the_ID(), '_yoast_wpseo_primary_category', true);
                     if(isset($primary_cateogory) && $primary_cateogory!=""){
                         $pcname = get_the_category_by_ID($primary_cateogory);
                         $category_name = $pcname;
-                        $get_cat_parents = rtrim(get_category_parents($primary_cateogory, false, ','),',');
+                        $get_cat_parents = rtrim(get_category_parents($primary_cateogory, false, '/'),'/');
                        }
                    }
-                    $cat_parents = explode(',',$get_cat_parents);
+                    $cat_parents = explode('/',$get_cat_parents);
                       
                     // Loop through parent categories and store in variable $cat_display
                     $cat_display = '';
