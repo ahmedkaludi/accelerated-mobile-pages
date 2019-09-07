bf6f64af2882aa7179a428f568edbf4341b59d05
Syed Zabiuddin <syedzabi332@gmail.com>
2019-09-07 18:18:04 +0530

    #3589 added in 98.5

--- templates/design-manager/swift/footer.php
+++ templates/design-manager/swift/footer.php
@@ -75,7 +75,7 @@ if( (is_single() && $redux_builder_amp['enable-single-social-icons']) || (is_pag
   					</div>
 			<?php }
 					else { ?>
-						<amp-facebook-like width=75 height=35 style="margin: 0 auto;margin-top: 5px;"
+						<amp-facebook-like width=90 height=35 style="margin: 0 auto;margin-top: 5px;"
 				 			layout="fixed"
 				 			data-size="large"
 				    		data-layout="button_count"
