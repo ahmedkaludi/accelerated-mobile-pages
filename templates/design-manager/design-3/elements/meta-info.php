c2b163d24b4c279fed882a5c7e68b95f3f6a567d
Syed Zabiuddin <syedzabi332@gmail.com>
2019-09-11 13:56:13 +0530

    #3453 code added for design 3

--- templates/design-manager/design-3/elements/meta-info.php
+++ templates/design-manager/design-3/elements/meta-info.php
@@ -41,8 +41,17 @@ if( isset($redux_builder_amp['ampforwp-cats-single']) && $redux_builder_amp['amp
 <?php if ( $redux_builder_amp['amp-design-3-date-feature'] ) : ?>
 	<span class="ampforwp-design3-single-date"><?php global $redux_builder_amp;
   $date = get_the_date( get_option( 'date_format' ));
-  if( 2 == $redux_builder_amp['ampforwp-post-date-global'] ){
+  if(1 == ampforwp_get_setting('ampforwp-post-date-global') && true == ampforwp_get_setting('ampforwp-post-time')){
+    $date = get_the_date( get_option( 'date_format' )) . ', ' . get_the_time();
+  }
+  elseif( 2 == ampforwp_get_setting('ampforwp-post-date-global') && true == ampforwp_get_setting('ampforwp-post-time')){
     $date = get_the_modified_date( get_option( 'date_format' ) ) . ', ' . get_the_modified_time() ;
+  }
+  elseif(1 == ampforwp_get_setting('ampforwp-post-date-global')){
+    $date = get_the_date( get_option( 'date_format' ));
+  }
+  elseif(2 == ampforwp_get_setting('ampforwp-post-date-global')){
+    $date = get_the_modified_date( get_option( 'date_format' ) );
   }
 
   echo apply_filters('ampforwp_modify_post_date', ampforwp_translation($redux_builder_amp['amp-translator-on-text'], 'On') . ' ' . $date ) ?></span>
