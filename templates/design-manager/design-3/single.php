f75ad9626babf1cb20716c05d6e7e9b6616f6c71
Wasim Akhtar <md.w.akhtar@gmail.com>
2019-09-12 13:42:05 +0530

    #3531 - Merged in 98.5

--- templates/design-manager/design-3/single.php
+++ templates/design-manager/design-3/single.php
@@ -23,6 +23,44 @@
 		<?php do_action('ampforwp_post_before_design_elements') ?>
 		<?php $this->load_parts( apply_filters( 'ampforwp_design_elements', array( 'empty-filter' ) ) ); ?>
 		<?php do_action('ampforwp_post_after_design_elements') ?>
+		<?php if(true==ampforwp_get_setting('ampforwp-design3-recent-posts') && !checkAMPforPageBuilderStatus(get_the_ID()) ) {?>
+					<div class="amp-wp-content relatedpost recentpost">
+						 <div class="related_posts">
+						<span class="related-title"><?php echo esc_attr(ampforwp_translation(ampforwp_get_setting('amp-translator-recent-text'), 'Recent Posts' )); ?></span>
+						<ol class="clearfix">
+						<?php 
+						$number_of_posts = 6;
+						if( !empty(ampforwp_get_setting('ampforwp-design3-number-of-recent-posts')) ){
+							$number_of_posts = (int) ampforwp_get_setting('ampforwp-design3-number-of-recent-posts');
+						}
+						while( amp_loop('start', array( 'posts_per_page' => $number_of_posts ) ) ): ?>
+							<li class="<?php if ( ampforwp_has_post_thumbnail() ) { echo'has_related_thumbnail'; } else { echo 'no_related_thumbnail'; } ?>">
+								<div class="related-post_image">
+	                            <a href="<?php echo esc_url( amp_loop_permalink() ); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
+							<?php if ( ampforwp_has_post_thumbnail() ) { 
+							$thumb_url 		= ampforwp_get_post_thumbnail();
+							$thumb_width  	= ampforwp_get_post_thumbnail('width');
+							$thumb_height 	= ampforwp_get_post_thumbnail('height');
+							if( $thumb_url && true == ampforwp_get_setting('amforwp-design3-recentpost-image-switch') ) { ?>
+				            	<amp-img src="<?php echo esc_url( $thumb_url ); ?>" width=<?php echo esc_attr($thumb_width); ?> height=<?php echo esc_attr($thumb_height); ?> layout="responsive"></amp-img>
+							<?php } 
+							}?>
+	                  		</a>
+	                  	</div>
+			                <div class="related_link">
+			                    <?php $title = get_the_title(); ?>
+			                    <a href="<?php echo esc_url( amp_loop_permalink() ); ?>" title="<?php echo esc_html( $title ); ?>" ><?php the_title(); ?></a>
+			                    <?php 
+			                    if (true == ampforwp_get_setting('amforwp-design3-recentpost-date-switch') ) {
+			                    		amp_loop_date();
+			                   		 }
+			                     ?>
+			                </div>
+		            		</li>
+						<?php endwhile; amp_loop('end');  ?>
+					</div>
+				</div>
+			<?php } ?>
 	</article>
 </main>
 <?php do_action( 'amp_post_template_above_footer', $this ); ?>
