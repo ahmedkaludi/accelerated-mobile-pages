bd377e5973e7cc5caa7dd0e7fdc5db20ebdf9618
Syed Zabiuddin <syedzabi332@gmail.com>
2019-09-20 19:33:40 +0530

    #3644 AMP text module is not working in Widget area issue fixed

--- includes/vendor/tinymce-widget/includes/class-widget.php
+++ includes/vendor/tinymce-widget/includes/class-widget.php
@@ -54,14 +54,14 @@ if ( ! class_exists( 'WP_Widget_Black_Studio_TinyMCE' ) ) {
             $title = empty( $instance['title'] ) ? '' : $instance['title'];
 			$title = apply_filters( 'widget_title', $title , $instance, $this->id_base );
 			$text = apply_filters( 'widget_text', empty( $instance['text'] ) ? '' : $instance['text'], $instance, $this );
-            $markup_title = '<div class="amp_module_title"><span>' . $title . '</span></div>';
-            $markup_text = '<div class="amp_cb_text_content">' . $text . '</div>';
+            $markup_title = '<div class="amp_module_title"><span>' . esc_html__($title,'accelerated-mobile-pages') . '</span></div>';
+            $markup_text = '<div class="amp_cb_text_content">' . esc_html__($text,'accelerated-mobile-pages') . '</div>';
 			$hide_empty = apply_filters( 'black_studio_tinymce_hide_empty', false, $instance );
 			if ( ! ( $hide_empty && empty( $text ) ) ) {
 				$output = $before_widget;
 				$output .= $before_text . $markup_title .  $markup_text . $after_text;
 				$output .= $after_widget;
-				echo $output; // xss ok
+				echo htmlspecialchars_decode($output); // xss ok, escaped above
 			}
 			do_action( 'black_studio_tinymce_after_widget', $args, $instance );
 		}
