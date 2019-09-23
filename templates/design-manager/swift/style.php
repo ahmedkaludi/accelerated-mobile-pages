2ec524d7e370759c921356eb24811ad2403cc423
Syed Zabiuddin <syedzabi332@gmail.com>
2019-09-23 17:21:47 +0530

    #3664 table styling issue fixed

--- templates/design-manager/swift/style.php
+++ templates/design-manager/swift/style.php
@@ -507,7 +507,7 @@ if( is_page() || ampforwp_is_front_page() || ampforwp_polylang_front_page() ){?>
 	}
 <?php } ?>
 <?php } // Level Condition Ends
-if(is_page()){ ?>
+if(is_page() || ampforwp_is_front_page()){ ?>
 	.pg table {width: 100%;margin-bottom:25px;overflow-x: auto;word-break: normal;}
 	.pg td {padding: 0.5em 1em;border: 1px solid #ddd;}
 	.pg tr:nth-child(odd) td {background: #f7f7f7;}
