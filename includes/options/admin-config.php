c21e31b65b78468eeb30993b0d1704ed6f22f8dd
Mohammed Kaludi <mohammed@magazine3.com>
2019-09-25 18:59:32 +0530

    #1489 updated the data-action so it can fetch from the categories
    #1489 updated the data-action so it can fetch from the categories

--- includes/options/admin-config.php
+++ includes/options/admin-config.php
@@ -2297,7 +2297,7 @@ Redux::setSection( $opt_name, array(
                         'tooltip-subtitle' => esc_html__( 'Hide IA from all the posts of a selected category.', 'accelerated-mobile-pages' ),
                         'multi'     => true, 
                         'ajax'      => true, 
-                        'data-action'     => 'categories', 
+                        'data-action'     => 'ampforwp_categories', 
                         'data'      => 'categories',
                         'required'  => array('fb-instant-article-switch', '=', 1)
                     ),  
