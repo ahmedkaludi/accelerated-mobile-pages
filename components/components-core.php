f5cae7855527ccb4bbab8b985fddfa76b188a0a7
Wasim Akhtar <md.w.akhtar@gmail.com>
2019-10-09 17:05:18 +0530

    #3624 - Improvement

--- components/components-core.php
+++ components/components-core.php
@@ -614,8 +614,10 @@ function amp_date( $args=array() ) {
                     'ago');
          
     }
-    if(!defined('ECWD_VERSION')){
-	    $post_date = apply_filters('ampforwp_modify_post_date', $post_date);
+    $pdate = $post_date;
+    $post_date = apply_filters('ampforwp_modify_post_date', $post_date);
+    if(defined('ECWD_VERSION')){
+	   $post_date = $pdate;
 	}
    	if(isset($args['custom_format']) && $args['custom_format']!=""){
 	    $post_date = date($args['custom_format'],get_the_time('U', get_the_ID() ));
