1be6962214cb3ca8ee90b104a3dfd5107f4a6af8
RajuJeelaga <rajujeelaga@gmail.com>
2019-10-28 19:35:33 +0530

    #2810 icomoon icons are added
    #2810 icomoon icons are added

--- pagebuilder/modules/social-share-module.php
+++ pagebuilder/modules/social-share-module.php
@@ -28,102 +28,102 @@ $output = '
 	 		<ul>
 	 			{{if_condition_fb-enable==1}}
 	 			<li>
-					<a class="s_fb" target="_blank" rel=nofollow href="https://www.facebook.com/sharer.php?u={{current_permalink}}" aria-label="facebook share"></a>
+					<a class="sm_fb" target="_blank" rel=nofollow href="https://www.facebook.com/sharer.php?u={{current_permalink}}" aria-label="facebook share"></a>
 				</li>
 				{{ifend_condition_fb-enable_1}}
 				{{if_condition_tw-enable==1}}
 	 			<li>
-					<a class="s_tw" target="_blank" rel=nofollow href="https://twitter.com/intent/tweet?url={{current_permalink}}&text={{current_title}}"  aria-label="twitter share"></a>
+					<a class="sm_tw" target="_blank" rel=nofollow href="https://twitter.com/intent/tweet?url={{current_permalink}}&text={{current_title}}"  aria-label="twitter share"></a>
 				</li>
 				{{ifend_condition_tw-enable_1}}
 				{{if_condition_em-enable==1}}
 	 			<li>
-					<a class="s_em" target="_blank" rel=nofollow href="mailto:?subject={{current_title}}&body={{current_permalink}}" aria-label="email share"></a>
+					<a class="sm_em" target="_blank" rel=nofollow href="mailto:?subject={{current_title}}&body={{current_permalink}}" aria-label="email share"></a>
 				</li>
 				{{ifend_condition_em-enable_1}}
 				{{if_condition_pin-enable==1}}
 	 			<li>
-					<a class="s_pt" target="_blank" rel=nofollow href="https://pinterest.com/pin/create/button/?media={{image}}&url={{current_permalink}}&description={{current_title}}" aria-label="pinterest share"></a>
+					<a class="sm_pt" target="_blank" rel=nofollow href="https://pinterest.com/pin/create/button/?media={{image}}&url={{current_permalink}}&description={{current_title}}" aria-label="pinterest share"></a>
 				</li>
 				{{ifend_condition_pin-enable_1}}
 				{{if_condition_lnk-enable==1}}
 	 			<li>
-					<a class="s_lk" target="_blank" rel=nofollow href="https://www.linkedin.com/shareArticle?url={{current_permalink}}&title={{current_title}}" aria-label="linkedin share"></a>
+					<a class="sm_lk" target="_blank" rel=nofollow href="https://www.linkedin.com/shareArticle?url={{current_permalink}}&title={{current_title}}" aria-label="linkedin share"></a>
 				</li>
 				{{ifend_condition_lnk-enable_1}}
 				{{if_condition_wap-enable==1}}
 	 			<li>
-					<a class="s_wp" target="_blank" rel=nofollow  href="https://api.whatsapp.com/send?text={{current_permalink}}" data-action="share/whatsapp/share" aria-label="whatsapp share"></a>
+					<a class="sm_wp" target="_blank" rel=nofollow  href="https://api.whatsapp.com/send?text={{current_permalink}}" data-action="share/whatsapp/share" aria-label="whatsapp share"></a>
 				</li>
 				{{ifend_condition_wap-enable_1}}
 				{{if_condition_line-enable==1}}
 	 			<li>
-					<a title="line share" class="s_li" rel=nofollow href="{{current_permalink}}" aria-label="line share"></a>
+					<a title="line share" class="sm_li" rel=nofollow href="{{current_permalink}}" aria-label="line share"></a>
 				</li>
 				{{ifend_condition_line-enable_1}}
 				{{if_condition_vk-enable==1}}
 	 			<li>
-					<a class="s_vk" target="_blank" rel=nofollow href="http://vk.com/share.php?url={{current_permalink}}" aria-label="vk share"></a>
+					<a class="sm_vk" target="_blank" rel=nofollow href="http://vk.com/share.php?url={{current_permalink}}" aria-label="vk share"></a>
 				</li>
 				{{ifend_condition_vk-enable_1}}
 				{{if_condition_od-enable==1}}
 	 			<li>
-					<a class="s_od" target="_blank" rel=nofollow  href="https://ok.ru/dk?st.cmd=addShare&st._surl={{current_permalink}}" aria-label="odnoklassniki share"></a>
+					<a class="sm_od" target="_blank" rel=nofollow  href="https://ok.ru/dk?st.cmd=addShare&st._surl={{current_permalink}}" aria-label="odnoklassniki share"></a>
 				</li>
 				{{ifend_condition_od-enable_1}}
 				{{if_condition_rd-enable==1}}
 	 			<li>
-					<a class="s_rd" target="_blank" rel=nofollow href="https://reddit.com/submit?url={{current_permalink}}&title={{current_title}}" aria-label="reddit share"></a>
+					<a class="sm_rd" target="_blank" rel=nofollow href="https://reddit.com/submit?url={{current_permalink}}&title={{current_title}}" aria-label="reddit share"></a>
 				</li>
 				{{ifend_condition_rd-enable_1}}
 				{{if_condition_tmb-enable==1}}
 	 			<li>
-					<a class="s_tb" target="_blank" rel=nofollow href="https://www.tumblr.com/widgets/share/tool?canonicalUrl={{current_permalink}}" aria-label="tumbler share"></a>
+					<a class="sm_tb" target="_blank" rel=nofollow href="https://www.tumblr.com/widgets/share/tool?canonicalUrl={{current_permalink}}" aria-label="tumbler share"></a>
 				</li>
 				{{ifend_condition_tmb-enable_1}}
 				{{if_condition_tg-enable==1}}
 	 			<li>
-					<a class="s_tg" target="_blank" rel=nofollow href="https://telegram.me/share/url?url={{current_permalink}}&text={{current_title}}" aria-label="telegram share"></a>
+					<a class="sm_tg" target="_blank" rel=nofollow href="https://telegram.me/share/url?url={{current_permalink}}&text={{current_title}}" aria-label="telegram share"></a>
 				</li>
 				{{ifend_condition_tg-enable_1}}
 				{{if_condition_stu-enable==1}}
 	 			<li>
-					<a class="s_su" target="_blank" rel=nofollow href="http://www.stumbleupon.com/submit?url={{current_permalink}}&title={{current_title}}" aria-label="stumbleupon share"></a>
+					<a class="sm_su" target="_blank" rel=nofollow href="http://www.stumbleupon.com/submit?url={{current_permalink}}&title={{current_title}}" aria-label="stumbleupon share"></a>
 				</li>
 				{{ifend_condition_stu-enable_1}}
 				{{if_condition_wc-enable==1}}
 	 			<li>
-					<a class="s_wc" target="_blank" rel=nofollow href="http://api.addthis.com/oexchange/0.8/forward/wechat/offer?url={{current_permalink}}" aria-label="wechat share"></a>
+					<a class="sm_wc" target="_blank" rel=nofollow href="http://api.addthis.com/oexchange/0.8/forward/wechat/offer?url={{current_permalink}}" aria-label="wechat share"></a>
 				</li>
 				{{ifend_condition_wc-enable_1}}
 				{{if_condition_vb-enable==1}}
 	 			<li>
-					<a class="s_vb" target="_blank" rel=nofollow href="viber://forward?text={{current_permalink}}" aria-label="viber share"></a>
+					<a class="sm_vb" target="_blank" rel=nofollow href="viber://forward?text={{current_permalink}}" aria-label="viber share"></a>
 				</li>
 				{{ifend_condition_vb-enable_1}}
 				{{if_condition_htb-enable==1}}
 	 			<li>
-					<a class="s_hb" target="_blank" rel=nofollow href="http://b.hatena.ne.jp/entry/{{current_permalink}}&title={{current_title}}" aria-label="hatena share"></a>
+					<a class="sm_hb" target="_blank" rel=nofollow href="http://b.hatena.ne.jp/entry/{{current_permalink}}&title={{current_title}}" aria-label="hatena share"></a>
 				</li>
 				{{ifend_condition_htb-enable_1}}
 				{{if_condition_pkt-enable==1}}
 	 			<li>
-					<a class="s_pk" target="_blank" rel=nofollow href="https://getpocket.com/save?url={{current_permalink}}&title={{current_title}}" aria-label="pocket share"></a>
+					<a class="sm_pk" target="_blank" rel=nofollow href="https://getpocket.com/save?url={{current_permalink}}&title={{current_title}}" aria-label="pocket share"></a>
 				</li>
 				{{ifend_condition_pkt-enable_1}}
 				{{if_condition_yml-enable==1}}
 	 			<li>
-					<a class="s_ym" target="_blank" rel=nofollow href="http://www.yummly.com/urb/verify?url=<{{current_permalink}}&title={{current_title}}&yumtype=button" aria-label="yummly share"></a>
+					<a class="sm_ym" target="_blank" rel=nofollow href="http://www.yummly.com/urb/verify?url=<{{current_permalink}}&title={{current_title}}&yumtype=button" aria-label="yummly share"></a>
 				</li>
 				{{ifend_condition_yml-enable_1}}
 				{{if_condition_mw-enable==1}}
 	 			<li>
-					<a title="mewe share" class="s_mewe" target="_blank" rel=nofollow href="https://mewe.com/share?link={{current_permalink}}" aria-label="mewe share"></a>
+					<a title="mewe share" class="sm_mewe" target="_blank" rel=nofollow href="https://mewe.com/share?link={{current_permalink}}" aria-label="mewe share"></a>
 				</li>
 				{{ifend_condition_mw-enable_1}}
 				{{if_condition_flb-enable==1}}
 	 			<li>
-					<a title="flipboard share" class="s_flipboard" rel=nofollow href="https://share.flipboard.com/bookmarklet/popout?v={{current_title}}&url={{current_permalink}}" target="_blank" aria-label="flipboard share"></a>
+					<a title="flipboard share" class="sm_flipboard" rel=nofollow href="https://share.flipboard.com/bookmarklet/popout?v={{current_title}}&url={{current_permalink}}" target="_blank" aria-label="flipboard share"></a>
 				</li>
 				{{ifend_condition_flb-enable_1}}
 	 		</ul>
@@ -145,6 +145,60 @@ if ( $ampforwp_font_icon == 'swift-icons' ){
 	{{if_condition_tw-enable==1}}
 		.sm_tw:after{content: "\e942";}
 	{{ifend_condition_tw-enable_1}}
+	{{if_condition_em-enable==1}}
+		.sm_em:after{content: "\e930";}
+	{{ifend_condition_em-enable_1}}
+	{{if_condition_pin-enable==1}}
+		.sm_pt:after{content: "\e937";}
+	{{ifend_condition_pin-enable_1}}
+	{{if_condition_lnk-enable==1}}
+		.sm_lk:after{content: "\e934";}
+	{{ifend_condition_lnk-enable_1}}
+	{{if_condition_wap-enable==1}}
+		.sm_wp:after{content: "\e946";}
+	{{ifend_condition_wap-enable_1}}
+	{{if_condition_line-enable==1}}
+		.sm_li:after{content: "\LN";}
+	{{ifend_condition_line-enable_1}}
+	{{if_condition_vk-enable==1}}
+		.sm_vk:after{content: "\e944";}
+	{{ifend_condition_vk-enable_1}}
+	{{if_condition_od-enable==1}}
+		.sm_od:after{content: "\e936";}
+	{{ifend_condition_od-enable_1}}
+	{{if_condition_rd-enable==1}}
+		.sm_rd:after{content: "\e938";}
+	{{ifend_condition_rd-enable_1}}
+	{{if_condition_tmb-enable==1}}
+		.sm_tb:after{content: "\e940";}
+	{{ifend_condition_tmb-enable_1}}
+	{{if_condition_tg-enable==1}}
+		.sm_tg:after{content: "\e93f";}
+	{{ifend_condition_tg-enable_1}}
+	{{if_condition_stu-enable==1}}
+		.sm_su:after{content: "\e93e";}
+	{{ifend_condition_stu-enable_1}}
+	{{if_condition_wc-enable==1}}
+		.sm_wc:after{content: "\e945";}
+	{{ifend_condition_wc-enable_1}}
+	{{if_condition_vb-enable==1}}
+		.sm_vb:after{content: "\e943";}
+	{{ifend_condition_vb-enable_1}}
+	{{if_condition_htb-enable==1}}
+		.sm_hb:after{content: "\e948";}
+	{{ifend_condition_htb-enable_1}}
+	{{if_condition_pkt-enable==1}}
+		.sm_pk:after{content: "\e949";}
+	{{ifend_condition_pkt-enable_1}}
+	{{if_condition_yml-enable==1}}
+		.sm_ym:after{content: "\e948";}
+	{{ifend_condition_yml-enable_1}}
+	{{if_condition_mw-enable==1}}
+		.sm_mewe:after{content: "MW";}
+	{{ifend_condition_mw-enable_1}}
+	{{if_condition_flb-enable==1}}
+		.sm_mewe:after{content: "FB";}
+	{{ifend_condition_flb-enable_1}}
 
 	';
 }
@@ -163,7 +217,7 @@ $css = $swift_icon . $font_awesome.'
 
 ';
 return array(
-		'label' =>'Social Share Module',
+		'label' =>'Social Share',
 		'name' =>'social-share',
 		'default_tab'=> 'customizer',
 		'tabs' => array(
