120d1d9b23a7a4612d85e7ef03db69f62ce8eba4
Syed Zabiuddin <syedzabi332@gmail.com>
2019-11-19 16:27:54 +0530

    #3634 Added in 98.5

--- templates/design-manager/swift/style.php
+++ templates/design-manager/swift/style.php
@@ -488,7 +488,7 @@ if( is_page() || ampforwp_is_front_page() || ampforwp_polylang_front_page() ){?>
 		<?php }
 		else{ ?> .shr-txt {text-transform: uppercase;font-size: 12px;color: #666;font-weight: 400;margin-bottom: 12px;display: block;} <?php } ?>
         .cntn-wrp{font-size: 18px;color: #000;line-height: 1.7;}
-        .cntn-wrp p, .cntn-wrp ul, .cntn-wrp ol{margin:0px 0px 30px 0px;word-break: break-word;}
+        .cntn-wrp p, .cntn-wrp ul, .cntn-wrp ol{margin:15px 0px 30px 0px;word-break: break-word;}
     <?php } else{ ?>
         .cntn-wrp{font-size: 18px;color: #000;line-height: 1.7;}
         .sp {width: 100%;margin-top: 20px;display: inline-block;}
@@ -669,6 +669,7 @@ if($redux_builder_amp['swift-social-position'] == 'below-content'){?>
 	<?php } // H6 Ends
  // swift Content Heading Sizes Ends?>
 .cntn-wrp p, .cntn-wrp ul, .cntn-wrp ol{margin:0px 0px 30px 0px;word-break: break-word;}
+.cntn-wrp .wp-block-image,.wp-block-embed{margin:15px 0px;}
 .artl-cnt ul li, .artl-cnt ol li{list-style-type: none;position: relative;
 <?php if( true == ampforwp_get_setting('amp-rtl-select-option') ) {?> 
 padding-right:20px;
