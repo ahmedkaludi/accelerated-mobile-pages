f13d131500e366324a16ad1280bfc9c6e8040f5e
Syed Zabiuddin <syedzabi332@gmail.com>
2019-11-30 12:21:35 +0530

    #3687 Added in 98.5

--- includes/features/performance/performance-functions.php
+++ includes/features/performance/performance-functions.php
@@ -6,6 +6,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 add_filter('ampforwp_the_content_last_filter','ampforwp_minify_html_output');
 function ampforwp_minify_html_output($content_buffer){
     $content_buffer = str_replace('srcset=""', '', $content_buffer);
+    //Removed trbidi attribute #3687
+    $content_buffer = str_replace('trbidi="on"', '', $content_buffer);
+    $content_buffer = str_replace("trbidi='on'", '', $content_buffer);
     global $redux_builder_amp;
     if(!$redux_builder_amp['ampforwp_cache_minimize_mode']){
            return $content_buffer;       
