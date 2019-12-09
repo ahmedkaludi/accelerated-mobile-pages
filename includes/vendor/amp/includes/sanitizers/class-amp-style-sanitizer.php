451d171863212b8c13aee3c3002864984a379c4a
Lokesh <jasthilokesh@gmail.com>
2019-12-09 12:16:01 +0530

    #3889 Fixed image tag name issue for inline CSS

--- includes/vendor/amp/includes/sanitizers/class-amp-style-sanitizer.php
+++ includes/vendor/amp/includes/sanitizers/class-amp-style-sanitizer.php
@@ -452,6 +452,8 @@ class AMP_Style_Sanitizer extends AMP_Base_Sanitizer {
 		}
 		$class = $element->getAttribute( 'class' );
 		$tagName = $element->tagName;
+		if($element->tagName == 'img')
+			$tagName = 'amp-img';
 		$properties = $this->process_style( $value );
 
 		if ( ! empty( $properties ) ) {
