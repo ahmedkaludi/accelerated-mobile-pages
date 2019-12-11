7a72d7edd8961f0039baee32317ba1ddda300bcc
Mohammed Kaludi <mohammed@magazine3.com>
2019-12-11 15:12:53 +0530

    #3914 updated the code
    #3914 updated the code

--- components/components-core.php
+++ components/components-core.php
@@ -703,8 +703,7 @@ function amp_author_meta( $args ) {
 add_action('amp_post_template_css','ampforwp_backtotop_global_css');
 function ampforwp_backtotop_global_css(){?>
 	amp-img.amp-wp-enforced-sizes[layout=intrinsic] > img, .amp-wp-unknown-size > img { object-fit: contain; }
-<?php if( true == ampforwp_get_setting('ampforwp-footer-top') ) {
-if( true == ampforwp_get_setting('ampforwp-footer-top') ) { ?>
+<?php if( true == ampforwp_get_setting('ampforwp-footer-top') ) { ?>
   .btt{
       position: fixed;
       <?php if( (is_single() && ampforwp_get_setting('enable-single-social-icons')) || (is_page() && true == ampforwp_get_setting('ampforwp-page-sticky-social')) ){ ?>
