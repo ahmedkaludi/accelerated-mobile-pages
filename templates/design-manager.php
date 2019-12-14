8a6802766d846d89c94eff67f7090f6c40151f8b
Mohammed Kaludi <mohammed@magazine3.com>
2019-12-14 17:34:39 +0530

    #3932 debug warning fixed
    #3932 debug warning fixed

--- templates/design-manager.php
+++ templates/design-manager.php
@@ -19,10 +19,11 @@ if ( is_customize_preview() ) {
 }
 
 	$data = get_option( 'ampforwp_design',array());
+
 	// Adding default Value
-	if ($data['elements'] == '') {
+	if (empty($data['elements'])) {
 	 	$data['elements'] = "bread_crumbs:1,meta_info:1,title:1,featured_image:1,content:1,meta_taxonomy:1,social_icons:1,comments:1,related_posts:1";
-	} 
+	}
 	
 	if( isset( $data['elements'] ) || ! empty( $data['elements'] ) ){
 		$options = explode( ',', $data['elements'] );
