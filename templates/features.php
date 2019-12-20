f91e88a4314bd06687dab7c533fc99c47ca5dbf9
Wasim Akhtar <md.w.akhtar@gmail.com>
2019-12-20 15:07:18 +0530

    #3970 - Feature of RankMatch nofollow external links

--- templates/features.php
+++ templates/features.php
@@ -7964,3 +7964,23 @@ function ampforwp_remove_unused_pb_amp_script($data){
    }
    return $data;                 
 }
+
+add_filter('ampforwp_modify_the_content','ampforwp_rank_math_nofollow_to_external_link');
+function ampforwp_rank_math_nofollow_to_external_link($content){
+	if(class_exists('RankMath')){
+		$rank_math_external_link = RankMath\Helper::get_settings( 'general.nofollow_external_links' );
+		if($rank_math_external_link){
+			preg_match_all('/<a href="(.*?)">(.*?)<\/a>/', $content, $matches);
+			for($i=0;$i<count($matches[1]);$i++){
+				$url = $matches[1][$i];
+				$is_external = ampforwp_isexternal($url);
+				if($is_external){
+					$rep_url = $matches[0][$i];
+					$url = str_replace("/", "\/", $url);
+					$content = preg_replace('/(<a href="'.$url.'.*")/', '$1 rel="nofollow"', $content);
+				}
+			}
+		}
+	}
+	return $content;
+}
