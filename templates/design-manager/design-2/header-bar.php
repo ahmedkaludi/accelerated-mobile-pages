aecab3e2e38dfc84dae82ef6dfe62f17607741bb
RajuJeelaga <rajujeelaga@gmail.com>
2019-12-20 14:54:04 +0530

    #3935 Design2 header right side part styling is properly added
    #3935 Design2 header right side part styling is properly added

--- templates/design-manager/design-2/header-bar.php
+++ templates/design-manager/design-2/header-bar.php
@@ -6,12 +6,14 @@ ampforwp_pagescroll_progress_bar(); ?>
       <div id="header">
         <?php do_action('ampforwp_header_top_design2'); ?>
         <?php amp_logo(); ?>
-        <?php if(true == ampforwp_get_setting('d123-signin-button') && '1' == ampforwp_get_setting('d123_cta-responsive-view') ) { ?>
-          <div class="d1-cta-wrap">   
-              <a target="_blank" <?php ampforwp_nofollow_cta_header_link(); ?> href="<?php echo esc_url(ampforwp_get_setting('d123-signin-button-link'))?>"><?php echo esc_html__(ampforwp_get_setting('d123-signin-button-text'), 'accelerated-mobile-pages'); ?></a>
-          </div>
-        <?php } ?>
-        <?php do_action('ampforwp_header_search'); ?>
+        <div class="d2-rp">
+          <?php if(true == ampforwp_get_setting('d123-signin-button') && '1' == ampforwp_get_setting('d123_cta-responsive-view') ) { ?>
+            <div class="d1-cta-wrap">   
+                <a target="_blank" <?php ampforwp_nofollow_cta_header_link(); ?> href="<?php echo esc_url(ampforwp_get_setting('d123-signin-button-link'))?>"><?php echo esc_html__(ampforwp_get_setting('d123-signin-button-text'), 'accelerated-mobile-pages'); ?></a>
+            </div>
+          <?php } ?>
+          <?php do_action('ampforwp_header_search'); ?>
+        </div>
         <?php do_action('ampforwp_call_button');
         do_action('ampforwp_header_bottom_design2'); ?>
       </div>
