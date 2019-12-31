9dac0b9487a2b9efd1351cff375958bbc22a7333
Wasim Akhtar <md.w.akhtar@gmail.com>
2019-12-31 12:07:06 +0530

    #3826 - Fixed the issue with advanced gtm setting.
    #3826 - Fixed the issue with advanced gtm setting.

--- includes/features/analytics/analytics-functions.php
+++ includes/features/analytics/analytics-functions.php
@@ -415,7 +415,7 @@ function ampforwp_add_advance_gtm_fields( $ampforwp_adv_gtm_fields ) {
 			$ampforwp_adv_gtm_fields = preg_replace('!/\*.*?\*/!s', '', $ampforwp_adv_gtm_fields);
 			$ampforwp_adv_gtm_fields = preg_replace('/\n\s*\n/', '', $ampforwp_adv_gtm_fields);
 			$ampforwp_adv_gtm_fields = preg_replace('/\/\/(.*?)\s(.*)/m', '$2', $ampforwp_adv_gtm_fields); ?>
-			<amp-analytics id="googletagmanager-<?php echo ampforwp_get_setting('amp-gtm-id'); ?>" type="gtag" <?php if(ampforwp_get_data_consent()){?>data-block-on-consent <?php } ?> data-credentials="include" config="https://www.googletagmanager.com/amp.json?id=<?php echo esc_attr(ampforwp_get_setting('amp-gtm-id')); ?>&amp;gtm.url=SOURCE_URL"><script type="application/json"><?php echo sanitize_text_field($ampforwp_adv_gtm_fields) ?></script></amp-analytics>
+			<amp-analytics type="googleanalytics" id="gtag" <?php if(ampforwp_get_data_consent()){?>data-block-on-consent <?php } ?>><script type="application/json"><?php echo sanitize_text_field($ampforwp_adv_gtm_fields) ?></script></amp-analytics>
 			<?php
 		 }
 }
