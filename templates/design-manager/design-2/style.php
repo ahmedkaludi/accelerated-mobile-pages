2e22824350b3b45db9aa7eca906dd5524c51ee08
RajuJeelaga <rajujeelaga@gmail.com>
2020-01-03 12:13:20 +0530

    #3935 Design 2 search icon problem is fixed
    #3935 Design 2 search icon problem is fixed

--- templates/design-manager/design-2/style.php
+++ templates/design-manager/design-2/style.php
@@ -159,7 +159,7 @@ amp-lightbox{ background: rgba(0, 0, 0,0.85); }
 [class*=icono-]:after, [class*=icono-]:before { content: ''; pointer-events: none; }
 .icono-search:before{ position: absolute; left: 50%; -webkit-transform: rotate(270deg); -ms-transform: rotate(270deg); transform: rotate(270deg); width: 2px; height: 9px; box-shadow: inset 0 0 0 32px; top: 0px; border-radius: 0 0 1px 1px; left: 14px; }
 [class*=icono-] { display: inline-block; vertical-align: middle; position: relative; font-style: normal; color: #f42; text-align: left; text-indent: -9999px; direction: ltr }
-<?php if( true == ampforwp_get_setting('amp-design-1-search-feature') ) {  ?>
+<?php if( true == ampforwp_get_setting('amp-design-2-search-feature') ) {  ?>
 .icono-search { -webkit-transform: translateX(-50%); -ms-transform: translateX(-50%); transform: translateX(-50%) }
 .icono-search { border: 1px solid; width: 10px; height: 10px; border-radius: 50%; -webkit-transform: rotate(45deg); -ms-transform: rotate(45deg); transform: rotate(45deg); margin: 4px 4px 8px 8px; }
 .searchform label{ color: #f7f7f7; display: block; font-size: 10px; letter-spacing: 0.3px; line-height: 0; opacity:0.6 }
@@ -324,7 +324,7 @@ a.btt:hover {
 .nav_container a, .nav_container a:hover, .nav_container a:focus{ 
 	color:  <?php echo ampforwp_sanitize_color( $header_font_color ); ?>;
 }
-<?php if ($redux_builder_amp['ampforwp-callnow-button']) { ?>
+<?php if ( ampforwp_get_setting('ampforwp-callnow-button') ) { ?>
 	.callnow{
 	<?php if( true == ampforwp_get_setting('amp-rtl-select-option') ) { ?>
 	right:16px;
