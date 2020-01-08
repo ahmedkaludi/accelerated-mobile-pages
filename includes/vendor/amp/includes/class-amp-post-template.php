a3fa5db4215882e585346a78391841f94c97bb34
Wasim Akhtar <md.w.akhtar@gmail.com>
2020-01-08 14:53:32 +0530

    #4033 - Merged in 1.0-deploy

--- includes/vendor/amp/includes/class-amp-post-template.php
+++ includes/vendor/amp/includes/class-amp-post-template.php
@@ -379,7 +379,7 @@ class AMP_Post_Template {
 					preg_match_all('/width="(.*?)"/', $m1_content,$fimgwidth);
 					preg_match_all('/height="(.*?)"/', $m1_content,$fimgheight);
 					preg_match_all('/alt="(.*?)"/', $m1_content,$fimgalt);
-					if(isset($fimgsrc[1][0]) && isset($fimgwidth[1][0]) && isset($fimgheight[1][0])){
+					if((isset($fimgsrc[1][0]) && preg_match_all('/http/', $fimgsrc[1][0],$fbi)) && isset($fimgwidth[1][0]) && isset($fimgheight[1][0])){
 					$data['src'] 	= $fimgsrc[1][0];
 					$data['width'] 	= $fimgwidth[1][0];
 					$data['height'] = $fimgheight[1][0];
