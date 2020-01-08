bc568ab0b5db8ccb595b33c2cba722d150775925
Wasim Akhtar <md.w.akhtar@gmail.com>
2020-01-08 12:52:17 +0530

    #3983 - Merged in 1.0-deploy

--- includes/vendor/amp/includes/class-amp-post-template.php
+++ includes/vendor/amp/includes/class-amp-post-template.php
@@ -342,7 +342,12 @@ class AMP_Post_Template {
 			if($convert_to_webp && $display_webp){
 				$img_url = esc_url($src[1][0]);
 				$rep_url = esc_url($src[1][0]).".webp";
-				$content = str_replace($img_url, $rep_url, $content);
+				if(isset($headers[0])){
+					$is_webp = stripos($headers[0], "200 OK") ? TRUE : FALSE;
+					if($is_webp){
+						$content = str_replace($img_url, $rep_url, $content);
+					}
+				}
 			}
 		}
 		return $content;
