ad953e2098e0a2da8d3183ef5690c6990399e33b
Wasim Akhtar <md.w.akhtar@gmail.com>
2020-01-11 15:13:06 +0530

    #4057 - Merged in master
    Improved code for add custom component script if not added.

--- templates/features.php
+++ templates/features.php
@@ -2289,15 +2289,20 @@ function ampforwp_set_body_content_script($data){
     		for($i=0;$i<count($amp_comp);$i++){
     			$comp = $amp_comp[$i];
     			if($comp!='img'){
+    				$script_ver = 'latest';
+					if($comp == 'auto-ads' || $comp == 'ad'){
+						$script_ver = '0.1';
+					}
+    				$component_url = "https://cdn.ampproject.org/v0/amp-".esc_attr($comp)."-".esc_attr($script_ver).".js";
     				if(isset($matches[1][0])){
     					$thtml = $matches1[1];
     					if(!in_array($comp, $thtml)){
-    						$data['amp_component_scripts']["amp-".esc_attr($comp)] = "https://cdn.ampproject.org/v0/amp-".esc_attr($comp)."-latest.js"; 
+    						$data['amp_component_scripts']["amp-".esc_attr($comp)] = esc_url($component_url);
     					}else{
-    						$data['amp_component_scripts']["amp-".esc_attr($comp)] = "https://cdn.ampproject.org/v0/amp-".esc_attr($comp)."-latest.js";
+    						$data['amp_component_scripts']["amp-".esc_attr($comp)] = esc_url($component_url);
     					}
     				} else{
-    					$data['amp_component_scripts']["amp-".esc_attr($comp)] = "https://cdn.ampproject.org/v0/amp-".esc_attr($comp)."-latest.js"; 
+    					$data['amp_component_scripts']["amp-".esc_attr($comp)] = esc_url($component_url); 
     				}   
     			}
     		}
@@ -7614,21 +7619,52 @@ if(!function_exists('ampforwp_transposh_plugin_rtl_css')){
     	}
     }
 }
-add_filter('ampforwp_the_content_last_filter','ampforwp_add_required_scripts',12);
-function ampforwp_add_required_scripts($content){
-	$exclude_el_arr = array('img','state','bind-macro','pixel');
+
+add_filter('ampforwp_the_content_last_filter','ampforwp_include_required_scripts',12);
+function ampforwp_include_required_scripts($content){
 	preg_match_all('/<\/amp-(.*?)>/', $content, $matches);
 	if(isset($matches[1][0])){
 		$amp_comp = $matches[1];
+		$comp_to_remove_json = get_transient('ampforwp_amp_exclude_custom_element');
+		$comp_to_include_json = get_transient('ampforwp_amp_included_custom_element');
+		$comp_to_remove_arr = array();
+		if($comp_to_remove_json){
+			$comp_to_remove_arr = json_decode($comp_to_remove_json, true);
+		}
+		$comp_to_include_arr = array();
+		if($comp_to_include_json){
+			$comp_to_include_arr = json_decode($comp_to_include_json, true);
+		}
 		for($i=0;$i<count($amp_comp);$i++){
 			$comp = $amp_comp[$i];
-			if(!in_array($comp, $exclude_el_arr)){
+			$script_ver = 'latest';
+			if($comp == 'auto-ads' || $comp == 'ad'){
+				$script_ver = '0.1';
+			}
+			$comp_url = 'https://cdn.ampproject.org/v0/amp-'.esc_attr($comp).'-'.esc_attr($script_ver).'.js';
+			if(!in_array($comp, $comp_to_remove_arr) && !in_array($comp, $comp_to_include_arr) ){
+				$headers = get_headers($comp_url);
+				if(isset($headers[0])){
+					$is_script = stripos($headers[0], "200 OK") ? TRUE : FALSE;
+					if($is_script){
+						$comp_to_include_arr[] = $comp;
+						$inc_json = json_encode($comp_to_include_arr);
+						set_transient('ampforwp_amp_included_custom_element',$inc_json, 30 * DAY_IN_SECONDS);
+					}else{
+						$comp_to_remove_arr[] = $comp;
+						$ex_json = json_encode($comp_to_remove_arr);
+						set_transient('ampforwp_amp_exclude_custom_element',$ex_json, 30 * DAY_IN_SECONDS);
+					}
+				}
+			}
+			$comp_to_include_arr = apply_filters('ampforwp_amp_custom_element_to_include',$comp_to_include_arr);
+			if(in_array($comp, $comp_to_include_arr)){
 				if(!preg_match('/<script\scustom-element=\"amp-'.esc_attr($comp).'\"(.*?)><\/script>/', $content, $matches)){
-					$script_tag = '<head><script custom-element="amp-'.esc_attr($comp).'" src="https://cdn.ampproject.org/v0/amp-'.esc_attr($comp).'-latest.js" async></script>';
+					$script_tag = '<head><script custom-element="amp-'.esc_attr($comp).'" src="'.esc_url($comp_url).'" async></script>';
 					$content =  str_replace('<head>', $script_tag, $content);
 				}
 			}
 		}
 	}
 	return $content;
-} 
+}
