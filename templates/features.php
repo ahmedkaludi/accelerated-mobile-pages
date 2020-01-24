593fa6778ff7725db929f2b0cceb2de90c63f61f
Wasim Akhtar <md.w.akhtar@gmail.com>
2020-01-24 16:53:07 +0530

    #4069 - Merged in 1.0-deploy

--- templates/features.php
+++ templates/features.php
@@ -6370,6 +6370,7 @@ function ampforwp_sneak_peek_css(){
 	$txt_color = $redux_builder_amp['content-sneak-peek-txt-color']['color'];?>
 	.fd-h{height: <?php echo esc_attr($height); ?>;overflow: hidden;position: relative;}
     .fd-b-c{text-align: center;margin: 0px 0px 30px 0px;}
+    .fd-b-c .fd-b:hover{cursor:pointer;}
     .fd-b-c .fd-b {border:none;border-radius: 5px;color: <?php echo ampforwp_sanitize_color($txt_color); ?>;font-size: 16px;font-weight: 700;padding: 12px 32px 12px 32px;background-color: <?php echo ampforwp_sanitize_color($btn_color); ?>;
     }
     .fd-h:after {
