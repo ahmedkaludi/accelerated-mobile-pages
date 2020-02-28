c5b8e48a9d5995d836f601920026a87de191bf80
Syed Zabiuddin <syedzabi332@gmail.com>
2020-02-28 13:01:44 +0530

    #4185 table alignment fixed in design 1 2 and 3

--- components/components-core.php
+++ components/components-core.php
@@ -739,7 +739,7 @@ function ampforwp_backtotop_global_css(){?>
             table.wp-block-table.alignright,table.wp-block-table.alignleft,table.wp-block-table.aligncenter{width: auto;}
             table.wp-block-table.aligncenter{width: 50%;}
             table.wp-block-table.alignfull,table.wp-block-table.alignwide{display: table;}
-            table { display: -webkit-box; display: -ms-flexbox; display: flex; -ms-flex-wrap: wrap; flex-wrap: wrap; overflow-x: auto; }
+            table { display: inline-table; overflow-x: auto; }
             table a:link { font-weight: bold; text-decoration: none; }
             table a:visited { color: #999999; font-weight: bold; text-decoration: none; }
             table a:active, table a:hover { color: #bd5a35; text-decoration: underline; }
