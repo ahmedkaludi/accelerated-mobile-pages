e65e5c056fb5d858175473d496feaa2d144c89c4
Wasim Akhtar <md.w.akhtar@gmail.com>
2020-03-03 18:13:24 +0530

    #4247 - Fixed the issue

--- includes/admin-script.js
+++ includes/admin-script.js
@@ -118,6 +118,7 @@ jQuery(function($) {
             $(this).find('.display_header').append('<span class="search-wrapper"><input  class="redux_field_search" name="" type="text" placeholder="Search the controls"  style="display:none"/><span class="redux-amp-search-icon"><i class="dashicons-before dashicons-search"></i></span></span>');
             $('.redux-amp-search-icon').on('click',function(){
                 $('.redux_field_search').toggle('slide');
+                $('.redux_field_search').val('');
             });
             reduxOptionSearch();
         }
