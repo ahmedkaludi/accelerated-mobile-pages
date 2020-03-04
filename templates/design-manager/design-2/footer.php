ef62edef3bbd891d7474ebb8ff43b7de00259fc2
Syed Zabiuddin <syedzabi332@gmail.com>
2020-03-04 13:19:59 +0530

    #4239 code improvement

--- templates/design-manager/design-2/footer.php
+++ templates/design-manager/design-2/footer.php
@@ -15,7 +15,13 @@ global $redux_builder_amp;
                   $sidebar_output = $sanitized_sidebar->get_amp_content();
                   $sidebar_output = apply_filters('ampforwp_modify_sidebars_content',$sidebar_output);
                   $sidebar_output = ampforwp_show_yoast_seo_local_map($sidebar_output);
-                  $sidebar_output = preg_replace('/<form(.*?)>(.*?)<select name="cat"(.*?)<\/form>/s', '<form$1target="_top">$2<select name="cat"$3</form>' , $sidebar_output);
+                  $sidebar_output = preg_replace_callback('/<form(.*?)>(.*?)<\/form>/s', function($match){
+                  if(strpos($match[1], 'target=') === false){
+                    return '<form'.$match[1].' target="_top">'.$match[2].'</form>';
+                  }else{
+                    return '<form'.$match[1].'>'.$match[2].'</form>';
+                  } 
+                }, $sidebar_output); 
                 } 
                 echo do_shortcode($sidebar_output); 
                 ?>
