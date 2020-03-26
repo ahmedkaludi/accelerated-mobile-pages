9c6240a65b8bf7bac64479f7ee07953913fe23bf
Wasim Akhtar <53761154+WasimM3@users.noreply.github.com>
2020-03-26 12:23:02 +0530

    #4106 - Improved code for Comment Module

--- pagebuilder/modules/comments-module.php
+++ pagebuilder/modules/comments-module.php
@@ -1,8 +1,9 @@
 <?php
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) exit;
-function ampforwp_get_pb_comment_list(){
+function ampforwp_get_pb_comment_list($moduleFrontHtml, $moduleTemplate, $contentArray){
 	$output = '';
+	if($moduleTemplate['name']!="comments"){return $moduleFrontHtml; }
 	if (ampforwp_get_setting('wordpress-comments-support')) {
 			global $post;
 			$comments = $max_page =  "";
@@ -14,9 +15,9 @@ function ampforwp_get_pb_comment_list(){
 					'status' => 'approve',
 					'order' =>$order
 			));
-
+//{{if_id}}id="{{id}}"{{ifend_id}} {{if_user_class}}class="{{user_class}}{{ifend_user_class}}"
 			if ( $comments ) {
-				$output .= '<div id="pb-comments" class="pb-comments-wrapper">
+				$output .= '<div {{if_id}}id="{{id}}"{{ifend_id}} class="pb-comments-wrapper {{if_user_class}}{{user_class}}{{ifend_user_class}}">
 		            <h3><span>View Comments</span></h3>
 		            <ul>';
 					foreach ($comments as $key => $value) {
@@ -35,20 +36,38 @@ function ampforwp_get_pb_comment_list(){
 				$output .='</ul>'; 
 				$output .='</div>';
 			} 
-		
 		$output .='</div>';
 	}
 	return $output;
 }
-$output_html = ampforwp_get_pb_comment_list();
+add_filter("ampforwp_extension_pagebuilder_module_template",'ampforwp_get_pb_comment_list' ,10,3);
+$output_html = "{{Comment_HTML}}";
 $css = '.pb-comments-wrapper ul li{list-style-type:none;position:relative;} .pbc-auth-info{position:absolute;top:2px;left:56px;} .pbc-auth-name{margin-bottom:5px}.pbc-comment{margin-top:10px;margin-bottom:10px;} .pb-comments-wrapper h3{margin-bottom: 16px;line-height:35px;border-bottom: 1px solid #d7d7d7;}';
 return array(
 		'label' =>'Comments',
 		'name' =>'comments',
+		'default_tab'=> 'advanced',
+		'tabs' => array(
+              'advanced' => 'Advanced'
+         ),
+		
 		'fields' => array(
-						array(		
-		 						
-	 						),
+					array(
+								'type'		=>'text',
+								'name'		=>"id",
+								'label'		=>'ID',
+								'tab'		=>'advanced',
+								'default'	=>'',
+								'content_type'=>'html'
+							),
+					array(
+							'type'		=>'text',
+							'name'		=>"user_class",
+							'label'		=>'Class',
+							'tab'		=>'advanced',
+							'default'	=>'',
+							'content_type'=>'html'
+						),
 			),
 		'front_template'=> $output_html,
 		'front_css'=> $css,
