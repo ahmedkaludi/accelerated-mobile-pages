8f92f4b2f4887ab5340038741b6c51448c43b7ad
Wasim Akhtar <53761154+WasimM3@users.noreply.github.com>
2020-04-13 17:25:02 +0530

    #4343 - Merged in 1.0.40

--- includes/admin-script.js
+++ includes/admin-script.js
@@ -1068,6 +1068,7 @@ jQuery(document).ready(function($) {
         }
     },10);
     $(".redux-group-tab-link-li").click(function(){
+        var this_c_val = $(this).children('a').children('span.group_title').html();
         if($(this).hasClass('ampforwp-new-ux')){
             $('#redux-footer-sticky').hide();
             $('#redux-footer-sticky #redux-footer').addClass("hide");
@@ -1078,6 +1079,20 @@ jQuery(document).ready(function($) {
                     $('#redux-footer-sticky #redux-footer').removeClass("hide");
                     $('#redux-footer-sticky #redux-footer').css({'position': 'fixed', 'bottom': '0px', 'width': '818px', 'left': '379px', 'background': 'rgb(238, 238, 238)'});
                 }
+            }
+        }
+         // There is no save button in AMP "Basic setup" #4343
+        var selected = $(".amp-opt-change:checked").parent().find('label').attr('id');
+        if(selected=='basic'){
+            if(this_c_val!='Setup' && this_c_val!='Extensions'){
+                if($('#redux-footer-sticky #redux-footer').hasClass('hide')){
+                    $('#redux-footer-sticky').show();
+                    $('#redux-footer-sticky #redux-footer').removeClass("hide");
+                    $('#redux-footer-sticky #redux-footer').css({'position': 'fixed', 'bottom': '0px', 'width': '818px', 'left': '379px', 'background': 'rgb(238, 238, 238)'});
+                }
+            }else{
+                $('#redux-footer-sticky').hide();
+                $('#redux-footer-sticky #redux-footer').addClass("hide");
             }
         }
     });
