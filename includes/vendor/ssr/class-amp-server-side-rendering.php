05121575584473c0af2e2af44cc09e1d597be306
Wasim Akhtar <53761154+WasimM3@users.noreply.github.com>
2020-04-13 12:59:29 +0530

    #3557 - Checked conditon for amp boilerplate

--- includes/vendor/ssr/class-amp-server-side-rendering.php
+++ includes/vendor/ssr/class-amp-server-side-rendering.php
@@ -12,15 +12,34 @@ class AMP_Server_Side_Rendering {
 		return $dom->saveHTML();
 	}
 	public static function ampforwp_get_transform_attribute($dom,$xpath) {
+		$is_boilerplate = AMP_Server_Side_Rendering::ampforwp_check_element_existance($dom,$xpath);
 		$nodes = $xpath->query("//html");
 		foreach($nodes as $node) {
 		    $node->setAttribute('i-amphtml-layout', '');
-		    $node->setAttribute('i-amphtml-no-boilerplate', '');
+			if($is_boilerplate){
+		    	$node->setAttribute('i-amphtml-no-boilerplate', '');
+			}
 		    $node->setAttribute('transformed', 'self;v=1');
 		}
 		return;
 	}
+	public static function ampforwp_check_element_existance($dom,$xpath){
+		$is_boilerplate = true;
+		$elem_arr = array('amp-experiment','amp-dynamic-css-classes');
+		$nodes = $xpath->query("//script[@custom-element]");
+	    foreach ($nodes as $node) {
+	      	$element = $node->getAttribute('custom-element');
+	      	if(in_array($element, $elem_arr)){
+	      		$is_boilerplate = false;
+	      	}
+	    }
+	    return $is_boilerplate;
+	}
 	public static function ampforwp_remove_ssr_boilerplate($dom,$xpath){
+		$is_boilerplate = AMP_Server_Side_Rendering::ampforwp_check_element_existance($dom,$xpath);
+		if(!$is_boilerplate){
+			return false;
+		}
 		$nodes = $xpath->query("//style[@amp-boilerplate]");
 	    foreach ($nodes as $node) {
 	      $node->parentNode->removeChild($node);
@@ -32,10 +51,10 @@ class AMP_Server_Side_Rendering {
 		$runtime_css = get_transient('ampforwp_ssr_runtime_css');
 		if(!$runtime_css){
 			$runtime_css = AMPFORWP_PLUGIN_DIR .'includes/vendor/ssr/local_fallback/v0.css';
+			$runtime_css = file_get_contents($runtime_css);
 			set_transient('ampforwp_ssr_runtime_css',$runtime_css);
 		}
-		$css = file_get_contents($runtime_css);
-		$style = $dom->createElement('style',$css);
+		$style = $dom->createElement('style',$runtime_css);
 		$domAttribute = $dom->createAttribute('amp-runtime');
 		$style->appendChild($domAttribute);
 		$domAttribute = $dom->createAttribute('i-amphtml-version');
