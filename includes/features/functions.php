7ed8079eed75dcd08db1e5e094f76eb7cb141f29
Syed Zabiuddin <32957211+Zabi09@users.noreply.github.com>
2020-04-13 11:48:28 +0530

    #4330 Added in 1.0.40

--- includes/features/functions.php
+++ includes/features/functions.php
@@ -38,6 +38,10 @@ function ampforwp_add_admin_styling($hook_suffix){
     if( current_user_can("manage_options") && $hook_suffix=='toplevel_page_amp_options' ){
         $redux_data = $redux_builder_amp;
         wp_dequeue_script( 'insert-post-adschart-admin' );
+        if(function_exists('brokers_page_template_redirect')){
+            wp_dequeue_script( 'broker-js' );
+            wp_dequeue_script( 'broker-min-js' );
+        }
         remove_all_actions('admin_notices');
         add_action('admin_notices', 'ampforwp_dev_mode_notice');
         add_action('admin_notices', 'ampforwp_plugins_manager_notice');
