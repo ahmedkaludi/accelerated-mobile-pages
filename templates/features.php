168cb6b4ae328dbfe6b4f89544a459c7fd0be06e
Syed Zabiuddin <32957211+Zabi09@users.noreply.github.com>
2020-05-02 11:14:42 +0530

    #4423 prefix added

--- templates/features.php
+++ templates/features.php
@@ -3153,7 +3153,7 @@ function ampforwp_auto_add_amp_in_menu_link( $atts, $item, $args ) {
   			$atts['href'] = user_trailingslashit(trailingslashit( $atts['href'] ) . AMPFORWP_AMP_QUERY_VAR);
   		}	
     }
-    $atts = apply_filters('auto_add_amp_menu_url',$atts);   
+    $atts = apply_filters('ampforwp_auto_add_amp_menu_url',$atts);   
 	return $atts;
 }
 
