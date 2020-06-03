7fa15baea9d667bcc070fdd554c7f455dae5340f
Syed Zabiuddin <32957211+Zabi09@users.noreply.github.com>
2020-06-03 15:23:08 +0530

    #4515 proper function exists added

--- includes/thirdparty-compatibility.php
+++ includes/thirdparty-compatibility.php
@@ -59,7 +59,9 @@ function ampforwp_thirdparty_compatibility(){
 	if(function_exists('heateor_sss_save_default_options') && false == ampforwp_get_setting('ampforwp-sassy_social-switch') ){
 		add_filter('heateor_sss_disable_sharing','ampforwp_removing_sassy_social_share');
 	}	
-	remove_filter( 'clean_url', 'defer_parsing_of_js', 11, 1 ); 
+	if(function_exists('defer_parsing_of_js') ){
+		remove_filter( 'clean_url', 'defer_parsing_of_js', 11, 1 ); 
+	}
 }
 function ampforwp_removing_sassy_social_share(){	
 	return 1;
