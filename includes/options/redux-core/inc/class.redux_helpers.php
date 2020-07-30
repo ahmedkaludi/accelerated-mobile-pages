4b4b4014e862a94f99e7366b3bb1d4e981e337dd
Syed Zabiuddin <32957211+Zabi09@users.noreply.github.com>
2020-07-30 16:06:47 +0530

    #4671 debug warning fixed

--- includes/options/redux-core/inc/class.redux_helpers.php
+++ includes/options/redux-core/inc/class.redux_helpers.php
@@ -340,15 +340,18 @@ namespace ReduxCore\ReduxFramework;
              * @since ReduxFramework 3.0.4
              */
             public static function hex2rgba( $hex, $alpha = '' ) {
+                $r = $g = $b = '';
                 $hex = str_replace( "#", "", $hex );
                 if ( strlen( $hex ) == 3 ) {
                     $r = hexdec( substr( $hex, 0, 1 ) . substr( $hex, 0, 1 ) );
                     $g = hexdec( substr( $hex, 1, 1 ) . substr( $hex, 1, 1 ) );
                     $b = hexdec( substr( $hex, 2, 1 ) . substr( $hex, 2, 1 ) );
                 } else {
+                    if (ctype_xdigit($hex)) {
                     $r = hexdec( substr( $hex, 0, 2 ) );
                     $g = hexdec( substr( $hex, 2, 2 ) );
                     $b = hexdec( substr( $hex, 4, 2 ) );
+                    }
                 }
                 $rgb = $r . ',' . $g . ',' . $b;
 
