6dbc8b5ef873e43e52d732bc1e976f41a17c5312
Syed Zabiuddin <32957211+Zabi09@users.noreply.github.com>
2020-08-25 14:04:22 +0530

    #4715 meta added

--- templates/features.php
+++ templates/features.php
@@ -3568,6 +3568,9 @@ function ampforwp_add_meta_viewport() {
 	    	$output = '<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,user-scalable=yes">';
 	    }
 	}
+	if (strpos($_SERVER['HTTP_USER_AGENT'], 'Safari') && !strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome')) {
+		$output .= '<meta name="referrer" content="no-referrer-when-downgrade">';
+	}
 	echo apply_filters('ampforwp_modify_meta_viewport_filter',$output);
 	
 }
