57ce8be9508e5560a19aa86304da4c82fbe5dcce
Syed Zabiuddin <32957211+Zabi09@users.noreply.github.com>
2020-09-11 11:25:02 +0530

    #4667 web story support added

--- includes/features/performance/performance-functions.php
+++ includes/features/performance/performance-functions.php
@@ -40,6 +40,9 @@ function ampforwp_minify_html_output($content_buffer){
             $content_buffer = preg_replace('/<blockquote class="tiktok-embed(.*?)(https?:\/\/(?:www\.)?(?:tiktok\.com\/@(.*?)\/video\/\d+))(.*?)data-video-id="(.*?)"(.*?)<\/blockquote>/i', $sanitized_iframe, $content_buffer);    
         }         
     }
+    if(preg_match('/<amp-story-player(.*?)<\/amp-story-player>/s', $content_buffer)){
+        $content_buffer = preg_replace('/<amp-story-player(.*?)<\/amp-story-player>/s', '<amp-story-player width="360" height="600" $1</amp-story-player>', $content_buffer);
+    }
     global $redux_builder_amp;
     if(!$redux_builder_amp['ampforwp_cache_minimize_mode']){
            return $content_buffer;       
