dd03f91b3da6bfc5185d17989d95a8735839bcc1
Syed Zabiuddin <32957211+Zabi09@users.noreply.github.com>
2020-09-12 16:10:40 +0530

    #4742 improved the condition

--- includes/vendor/amp/amp.php
+++ includes/vendor/amp/amp.php
@@ -80,11 +80,24 @@ if ( ! function_exists('AMPforWP\\AMPVendor\\amp_prepare_render') ) {
 }
 if ( ! function_exists('AMPforWP\\AMPVendor\\amp_render') ) {
 	function amp_render() {
+		amp_load_classes();
+		$post_id = get_queried_object_id();
 		do_action( 'pre_amp_render_post', $post_id );
 		$post = get_queried_object();
-		if ( $post instanceof WP_Post ) {
+		if ( $post instanceof WP_Post && function_exists('amp_activate')) {
 			amp_render_post( $post );
 			exit;
+		}
+		if ( !function_exists('amp_activate')) {
+		global $ampforwpTemplate;
+		amp_add_post_template_actions();
+		$template = $ampforwpTemplate = new AMP_Post_Template( $post_id );
+		$template->load();
+		// Set Header: last modified information
+		if( is_singular() && $post_id ) {
+            header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+	    }
+		exit;
 		}
 	}
 }
