e5bbde405d3399e2fdc199bd031bf428ae11c5bd
Syed Zabiuddin <32957211+Zabi09@users.noreply.github.com>
2020-10-13 17:13:07 +0530

    #4667 removed script

--- templates/features.php
+++ templates/features.php
@@ -9143,10 +9143,3 @@ function ampforwp_jannah_video_audio_support($content){
 	    }
 	  return $content;
 }
-add_filter('ampforwp_post_template_data','ampforwp_amp_story_player_script');
-function ampforwp_amp_story_player_script($data) {
-	if ( empty( $data['amp_component_scripts']['amp-story-player'] ) ) {
-		$data['amp_component_scripts']['amp-story-player'] = 'https://cdn.ampproject.org/v0/amp-story-player-latest.js';
-	}
-	return $data;
-}
