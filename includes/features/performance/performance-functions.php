3fc269e3ace153c2fc8997c691e8132a2d8b999d
Syed Zabiuddin <32957211+Zabi09@users.noreply.github.com>
2020-10-17 17:44:01 +0530

    #4785 issue fixed

--- includes/features/performance/performance-functions.php
+++ includes/features/performance/performance-functions.php
@@ -43,6 +43,9 @@ function ampforwp_minify_html_output($content_buffer){
     if(preg_match('/<amp-story-player(.*?)<\/amp-story-player>/s', $content_buffer)){
         $content_buffer = preg_replace('/<amp-story-player(.*?)<\/amp-story-player>/s', '<amp-story-player width="360" height="600" $1</amp-story-player>', $content_buffer);
     }
+   if(preg_match('/<input(.*?)type="image"(.*?)>/', $content_buffer)){
+        $content_buffer = preg_replace('/<input(.*?)type="image"(.*?)>/', '', $content_buffer);
+    }
     global $redux_builder_amp;
     if(!$redux_builder_amp['ampforwp_cache_minimize_mode']){
            return $content_buffer;       
