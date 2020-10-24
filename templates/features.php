74e24fa5cfbf492108485f3f48bb4f9148beacd9
Syed Zabiuddin <32957211+Zabi09@users.noreply.github.com>
2020-10-24 18:41:07 +0530

    #4794 amp-story-player properly added

--- templates/features.php
+++ templates/features.php
@@ -8330,7 +8330,7 @@ function ampforwp_include_required_scripts($content){
 		}
 	}
 	//OTHER COMPONENT CHECK 
-	$other_comp_arr = array('amp-mustache'=>'amp-mustache','amp-embed'=>'amp-ad','form'=>'amp-form','amp-access'=>'amp-access','amp-fx'=>'amp-fx-collection','amp-story-player'=>'amp-story-player');
+	$other_comp_arr = array('amp-mustache'=>'amp-mustache','amp-embed'=>'amp-ad','form'=>'amp-form','amp-access'=>'amp-access','amp-fx'=>'amp-fx-collection');
 	foreach ($other_comp_arr as $key => $value) {
 		$ocomp = $value;
 		$celem = 'element';
@@ -8947,3 +8947,10 @@ function ampforwp_amp_bind_script($data) {
 	}	
 	return $data;
 }
+add_filter('ampforwp_post_template_data','ampforwp_amp_story_player_script');
+function ampforwp_amp_story_player_script($data) {	
+	if ( empty( $data['amp_component_scripts']['amp-story-player'] ) ) {	
+		$data['amp_component_scripts']['amp-story-player'] = 'https://cdn.ampproject.org/v0/amp-story-player-latest.js';	
+	}	
+	return $data;	
+} 
