f24d7107573f99f482f4e935d7b72ff03dae3cbc
Syed Zabiuddin <32957211+Zabi09@users.noreply.github.com>
2020-11-23 15:43:36 +0530

    #4833 condition updated

--- templates/features.php
+++ templates/features.php
@@ -9179,7 +9179,7 @@ function ampforwp_remove_homepage_breadcrumb($data){
 }
 function ampforwp_admin_discount_btn() {
 $result = get_option( "ampforwp_dismiss_discount_btn");
-if ($result == 'removed') {?>
+if ($result != 'removed') {?>
 	<div class="wrapper-discount">
 	<a class="admin_discount_btn" href="<?php echo admin_url('admin.php?page=amp_options&tab=31'); ?>"><span>50% OFF on AMPforWP</span></a>
 	<span id='amp-close'>x</span></div>
