d3540bffa234cde161b2775544fb2a709b886a34
Syed Zabiuddin <32957211+Zabi09@users.noreply.github.com>
2020-11-27 10:56:08 +0530

    Revert "#4841 issue fixed"
    This reverts commit 598945a137faff13a6e89d82d04b73bf3ea47685.

--- templates/features.php
+++ templates/features.php
@@ -290,6 +290,9 @@ define('AMPFORWP_COMMENTS_PER_PAGE',  ampforwp_define_comments_number() );
 			if (empty(ampforwp_get_setting('ampforwp-custom-type'))) {
 				return;
 			}
+			if(is_array(ampforwp_get_setting('ampforwp-custom-type')) && $post_type != ampforwp_get_setting('ampforwp-custom-type')[0]){
+				return;
+			}
 		}
 		$page_for_posts = intval(get_option( 'page_for_posts' ));
 		$post_id = ampforwp_get_the_ID();
