0ec0bbdfc2147cde0826f137641e2874d25fb188
Syed Zabiuddin <32957211+Zabi09@users.noreply.github.com>
2020-12-12 16:07:11 +0530

    #4867 issue properly fixed

--- includes/admin-script.js
+++ includes/admin-script.js
@@ -113,7 +113,7 @@ jQuery(function($) {
                 captureLength:0
             });
         } 
-    $('input[data-id="ampforwp-bread-crumb"]').click(function(){
+    $('input[data-id="ampforwp-bread-crumb"]').on("click", function(){
         var html = '<li id="sortee-bread_crumbs" class="sortee ui-sortable-handle" data-id="bread_crumbs"><input class="position " type="hidden" name="redux_builder_amp[d_1_3_single_components_layout][enabled][bread_crumbs]" value="BreadCrumbs">BreadCrumbs</li>';
          $("#sortee-bread_crumbs").remove();
         if($(this).prop('checked')==false){
@@ -122,7 +122,7 @@ jQuery(function($) {
             $("#d_1_3_single_components_layout_enabled").append(html);
         }
     });
-    $('input[data-id="ampforwp-single-related-posts-switch"]').click(function(){
+    $('input[data-id="ampforwp-single-related-posts-switch"]').on("click", function(){
         var html = '<li id="sortee-related_posts" class="sortee ui-sortable-handle" data-id="related_posts"><input class="position " type="hidden" name="redux_builder_amp[d_1_3_single_components_layout][enabled][related_posts]" value="Related Posts">Related Posts</li>';
          $("#sortee-related_posts").remove();
         if($(this).prop('checked')==false){
@@ -134,7 +134,7 @@ jQuery(function($) {
     $('.redux-container').each(function() {
         if (!$(this).hasClass('redux-no-sections')) {
             $(this).find('.display_header').append('<span class="search-wrapper"><input  class="redux_field_search" name="" type="text" placeholder="Search the controls" style="display:none"/><span class="redux-amp-search-icon"><i class="dashicons-before dashicons-search"></i></span></span>');
-            $('.redux-amp-search-icon').click(function(){
+            $('.redux-amp-search-icon').on("click", function(){
                 $('.redux_field_search').toggle('slide');
                 var val = $('.redux_field_search').val();
                 var display = $('.redux_field_search').css('display');
@@ -195,7 +195,7 @@ jQuery(function($) {
         }
     });
 
-    $(".redux_field_search").keypress(function (evt) {
+    $(".redux_field_search").on( "keypress", function (evt) {
         //Deterime where our character code is coming from within the event
         var charCode = evt.charCode || evt.keyCode;
         if (charCode  == 13) { //Enter key's keycode
@@ -204,7 +204,7 @@ jQuery(function($) {
     });
     //option panel Section Division
     var optionSectionDevision = function(){
-        $('.afw-accordion-header').click(function(){
+        $('.afw-accordion-header').on("click", function(){
             //Get Cookie Changes
             if ( $.cookie( "redux_current_section_customize" )){
                  var allReduxTabs = JSON.parse($.cookie( "redux_current_section_customize" ));   
@@ -283,7 +283,7 @@ jQuery(function($) {
             }
         });
 
-        jQuery( '.redux-group-tab-link-a' ).click(function(){
+        jQuery( '.redux-group-tab-link-a' ).on("click", function(){
             if(jQuery(this).parent('li').hasClass('otherSectionFields')){
                 jQuery(this).parent('li.otherSectionFields').siblings('li.otherSectionFields').hide();
                 if(!jQuery(this).parent('li').is(':visible')){
@@ -313,7 +313,7 @@ jQuery(function($) {
                 $(this).parent('div.switch-options').find('.switch-text-off').show();
             }
         });        
-        switchText.siblings('label').click(function(){
+        switchText.siblings('label').on("click", function(){
             if ( $(this).siblings('input').attr('value') == 1 ) {
                 $(this).parent().find('.switch-text-on').hide();
                 $(this).parent().find('.switch-text-off').show();
@@ -654,7 +654,7 @@ var dataTabRequired = function(){
     });
 }    
 var reduxOptionTab = function(){
-    $('.redux-tab-selector').click(function(){
+    $('.redux-tab-selector').on("click", function(){
         var tabId = $(this).attr('data-tab');
         var tabRequired = $(this).attr('data-tab-required');
         $(this).parents().find('.custom-tab-container').hide();   
@@ -683,7 +683,7 @@ var reduxOptionTab = function(){
         });//$(".amp-ls-solve") each closed
    }
 });
-$(".redux-ampforwp-ext-activate").click(function(){
+$(".redux-ampforwp-ext-activate").on("click", function(){
     var currentThis = $(this);
     var plugin_id = currentThis.attr("id");
     var secure_nonce = currentThis.parents("li").attr('data-ext-secure');
@@ -749,7 +749,7 @@ function AMPforwpreadCookie(name) {
     return null;
 }
 function deactivatelicence(){
-    $(".ampforwp-ext-refresh").click(function(){
+    $(".ampforwp-ext-refresh").on("click", function(){
     var currentThis = $(this);
     var plugin_id = currentThis.attr("id");
 
@@ -801,7 +801,7 @@ function deactivatelicence(){
 
     }
 });
-$(".redux-ampforwp-ext-deactivate").click(function(){
+$(".redux-ampforwp-ext-deactivate").on("click", function(){
     var currentThis = $(this);
     var plugin_id = currentThis.attr("id");
     var secure_nonce = currentThis.parents("li").attr('data-ext-secure');
@@ -871,7 +871,7 @@ var helpSection = function(){
 helpSection();
 
 var redux_title_modify = function(){
-    $( '.redux-group-tab-link-a' ).click(function(){
+    $( '.redux-group-tab-link-a' ).on("click", function(){
         var link = $( this );
         if ( link.parent().hasClass( 'empty_section' ) && link.parent().hasClass( 'hasSubSections' ) ) {
             var elements = $( this ).closest( 'ul' ).find( '.redux-group-tab-link-a' );
@@ -893,7 +893,7 @@ var redux_title_modify = function(){
     $('li.active .redux-group-tab-link-a').click();
     }
 
-    $("#meta-checkbox").click(function(){ 
+    $("#meta-checkbox").on("click", function(){ 
            ampforwp_check_custom_content_status($(this));
     });
     ampforwp_check_custom_content_status($("#meta-checkbox")); 
@@ -929,7 +929,7 @@ var redux_title_modify = function(){
     });
 
     if($(".amp-preview-button").length>0){
-        $(".amp-preview-button").click(function(){
+        $(".amp-preview-button").on("click", function(){
             var srcLink = $("#amp-preview-iframe").attr('data-src');
            $("#amp-preview-iframe").html("<iframe  src='"+srcLink+"'></iframe>");
         });
@@ -1085,7 +1085,7 @@ jQuery(document).ready(function($) {
             $('#redux-footer-sticky').hide();
         }
     },10);
-    $(".redux-group-tab-link-li").click(function(){
+    $(".redux-group-tab-link-li").on("click", function(){
         if($(this).hasClass('ampforwp-new-ux')){
             $('#redux-footer-sticky').hide();
             $('#redux-footer-sticky #redux-footer').addClass("hide");
@@ -1330,7 +1330,7 @@ jQuery(document).ready(function($) {
             $(".ux-setup-icon").addClass("amp-ux-warning-okay");
         }
     }
-    $("#ampforwp-prem-upg-to").click(function(){
+    $("#ampforwp-prem-upg-to").on("click", function(){
         $(".redux-group-tab-link-a").each(function(){
             var id = $(this).attr('data-key');
             var thischildelem = $(this).children('.group_title').html();
@@ -1341,7 +1341,7 @@ jQuery(document).ready(function($) {
             }
         });
     }); 
-    $("#ampforwp-goto-analytics").click(function(){
+    $("#ampforwp-goto-analytics").on("click", function(){
         $(".redux-group-tab-link-a").each(function(){
             var id = $(this).attr('data-key');
             var thischildelem = $(this).children('.group_title').html();
@@ -1356,7 +1356,7 @@ jQuery(document).ready(function($) {
             }
         });
     }); 
-     $("[data-href=ampforwp-ux-advertisement-section]").click(function(){
+     $("[data-href=ampforwp-ux-advertisement-section]").on("click", function(){
        $(".redux-group-tab-link-a").each(function(){
             var id = $(this).attr('data-key');
             var thischildelem = $(this).children('.group_title').html();
@@ -1416,7 +1416,7 @@ jQuery(document).ready(function($) {
     });
 
     // Homepage
-    $('input[id="amp-ux-homepage"]').click(function(){
+    $('input[id="amp-ux-homepage"]').on("click", function(){
         if($(this).prop("checked") == true){
             $('.amp-ux-frontpage').show();
             $(this).attr('value', 1);
@@ -1433,7 +1433,7 @@ jQuery(document).ready(function($) {
         
     });
     // Frontpage
-     $('input[id="amp-ux-frontpage"]').click(function(){
+     $('input[id="amp-ux-frontpage"]').on("click", function(){
         if($(this).prop("checked") == true){
             // FrontPage
             $("input[data-id=amp-frontpage-select-option]").prop('checked', true).trigger( 'change' );
@@ -1458,7 +1458,7 @@ jQuery(document).ready(function($) {
         }
     });
     // Posts
-    $('#amp-ux-posts').click(function(){
+    $('#amp-ux-posts').on("click", function(){
         if($(this).prop("checked") == true){
             if($('input[id="amp-on-off-for-all-posts"]').val() != 1 ) {
                 $("input[data-id=amp-on-off-for-all-posts]").prop('checked', true).trigger( 'change' );
@@ -1471,7 +1471,7 @@ jQuery(document).ready(function($) {
         }
     });
     // Pages
-    $('input[id="amp-ux-pages"]').click(function(){
+    $('input[id="amp-ux-pages"]').on("click", function(){
         if($(this).prop("checked") == true){
             if($('input[id="amp-on-off-for-all-pages"]').val() != 1 ) {
                 $("input[data-id=amp-on-off-for-all-pages]").prop('checked', true).trigger( 'change' );
@@ -1484,7 +1484,7 @@ jQuery(document).ready(function($) {
         }
     });
     // Archives
-    $('input[id="amp-ux-archives"]').click(function(){
+    $('input[id="amp-ux-archives"]').on("click", function(){
         if($(this).prop("checked") == true){
             if($('input[id="ampforwp-archive-support"]').val() != 1 ) {
                 $("input[data-id=ampforwp-archive-support]").prop('checked', true).trigger( 'change' );
@@ -1507,7 +1507,7 @@ jQuery(document).ready(function($) {
             }
     });
     // Design and Presentation Section
-    $('.media-amp-ux-opt-media' ).unbind().on(
+    $('.media-amp-ux-opt-media' ).off().on(
         'click', function( event ) {
             redux.field_objects.media.addFile( event, $( this ).parents( 'div.amp-ux-opt-media-container:first' ) );
              $('.media-button-select').on('click', function(){
@@ -1763,7 +1763,7 @@ jQuery(document).ready(function($) {
         }
     }
     // Privacy Settings Section
-    $('input[id="amp-ux-notice-switch"]').click(function(){
+    $('input[id="amp-ux-notice-switch"]').on("click", function(){
         if($(this).prop("checked") == true){
             $(this).val(1);
             if($('input[id="amp-enable-notifications"]').val() != 1 ) {
@@ -1788,7 +1788,7 @@ jQuery(document).ready(function($) {
         
     });
     
-    $('input[id="amp-ux-gdpr-switch"]').click(function(){
+    $('input[id="amp-ux-gdpr-switch"]').on("click", function(){
         if($(this).prop("checked") == true){
             $(this).val(1);
             if($('input[id="amp-gdpr-compliance-switch"]').val() != 1 ) {
@@ -1810,7 +1810,7 @@ jQuery(document).ready(function($) {
             $(this).val(0);
         }
     });
-  $(".amp-ux-section-field").click(function(){
+  $(".amp-ux-section-field").on("click", function(){
         var track = $(this).attr('data-href');
         localStorage.setItem('ampforwp_current_drawer_click',track);
     });
@@ -1838,7 +1838,7 @@ function ampforwp_check_required(value,required){
         $("[required="+required+"]").removeClass("hide");
     }
 }
-$("#ampforwp-add-more-analytics").click(function(){
+$("#ampforwp-add-more-analytics").on("click", function(){
     var analytics = $("#ampforwp-ux-analytics-more").val();
     
     if( analytics && $("[data-href="+analytics+"]").hasClass('hide')){
@@ -1859,7 +1859,7 @@ $("#ampforwp-add-more-analytics").click(function(){
     }
 });
 
-$('.ampforwp-ux-closable').click(function(){
+$('.ampforwp-ux-closable').on("click", function(){
     $(this).parent('.ampforwp-ux-sub-section').addClass('hide');
      var data_href = $(this).parent('.ampforwp-ux-sub-section').attr('data-href');
     var checked = $('#redux_builder_amp-'+data_href).children('.switch-options').children('.ios7-switch').children('.switch-on-off').prop('checked');
@@ -2280,7 +2280,7 @@ function Drawer(drawerElem) {
     }
     amp_option_panel_view_func();
 
-    $(".amp-opt-view").click(function(){
+    $(".amp-opt-view").on("click", function(){
          var thisid = $(this).attr('id');
          amp_options_hide_show(thisid);
          $(".ampforwp-option-panel-view-pop").remove();
@@ -2332,14 +2332,14 @@ function Drawer(drawerElem) {
          $("body").css({'overflow':'auto'});
          $("#1_section_group_li_a").click();
     }
-    $("[data-href='ampforwp-ux-design-section']").click(function(){
+    $("[data-href='ampforwp-ux-design-section']").on("click", function(){
         if($("[name='redux_builder_amp[amp-design-selector]']").val() == '4'){
             $('#ampforwp-easy-setup-global-color').show();
         }else{
             $('#ampforwp-easy-setup-global-color').hide();
         }
     });
-    $(".amp-opt-change").click(function(){
+    $(".amp-opt-change").on("click", function(){
         var thisid = $(this).attr('id');
         $(".amp-opt-change").each(function(){
             $(this).parent().removeClass('active');
@@ -2380,7 +2380,7 @@ function Drawer(drawerElem) {
         setTimeout(function(){ $(".amp-ux-valid-require").parent('.amp-ux-elem-field').parent('.amp-ux-section-field').removeAttr('style'); }, 500);
     });
 
-     $(".redux-action_bar #redux_save").click(function(){
+     $(".redux-action_bar #redux_save").on("click", function(){
         $("#d_1_3_single_components_layout_disabled li").each(function(){
             var this_dataid = $(this).attr('data-id');
             if('bread_crumbs'==this_dataid){
@@ -2541,7 +2541,7 @@ function DrawerIcon(icon) {
         }
     })();
 }
- $("#close-notice").click(function(){
+ $("#close-notice").on("click", function(){
         var data = {
             action: 'ampforwp_internal_feedback_dismiss',
         };
