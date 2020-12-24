7a816a293511bb0d71810ae2e2907801ce041ee8
mali <merajali294@gmail.com>
2020-12-24 19:10:41 +0530

    #4886
    Accordion added for Oxygen Pagebuilder support

--- includes/features/pagebuilders-support/pagebuilders_support.php
+++ includes/features/pagebuilders-support/pagebuilders_support.php
@@ -4,7 +4,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 function ampforwp_page_builders_support_options($opt_name){
-$divi_pb_for_ampchecker = $elemntr_pb_for_ampchecker = $avada_pb_for_ampchecker = $avia_pb_for_ampchecker = array();
+$divi_pb_for_ampchecker = $elemntr_pb_for_ampchecker = $avada_pb_for_ampchecker = $avia_pb_for_ampchecker = $oxygen_pb_for_ampchecker = array();
 if(!function_exists('amp_pagebuilder_compatibility_init')){
 $divi_pb_for_ampchecker = array( 
                 'id'   => 'divi_pb_for_amp_info_normal',
@@ -35,6 +35,15 @@ $avia_pb_for_ampchecker = array(
                 'type' => 'info',
                 'required' => array(
                     array('ampforwp-avia-pb-for-amp', '=' , true),  
+                    ),
+                 'desc' => sprintf('<div style="background: #FFF9C4;padding: 12px;line-height: 1.6;margin: -45px -14px -18px -17px;"><b>%s</b> %s <a href="https://ampforwp.com/page-builder-compatibility-for-amp/" target="_blank">%s</a> extension.<br /> <div style="margin-top:4px;">(<a href="https://ampforwp.com/page-builder-compatibility-for-amp/" target="_blank">%s</a>)</div></div>',esc_html__( 'ONE LAST STEP REQUIRED:','accelerated-mobile-pages'),esc_html__( 'This feature requires', 'accelerated-mobile-pages' ),esc_html__( 'Page Builder compatibility For AMP', 'accelerated-mobile-pages'),esc_html__( 'Click here for more info', 'accelerated-mobile-pages' )),               
+           );
+
+      $oxygen_pb_for_ampchecker = array( 
+                'id'   => 'oxygen_pb_for_amp_info_normal',
+                'type' => 'info',
+                'required' => array(
+                    array('ampforwp-oxygen-pb-for-amp', '=' , true),  
                     ),
                  'desc' => sprintf('<div style="background: #FFF9C4;padding: 12px;line-height: 1.6;margin: -45px -14px -18px -17px;"><b>%s</b> %s <a href="https://ampforwp.com/page-builder-compatibility-for-amp/" target="_blank">%s</a> extension.<br /> <div style="margin-top:4px;">(<a href="https://ampforwp.com/page-builder-compatibility-for-amp/" target="_blank">%s</a>)</div></div>',esc_html__( 'ONE LAST STEP REQUIRED:','accelerated-mobile-pages'),esc_html__( 'This feature requires', 'accelerated-mobile-pages' ),esc_html__( 'Page Builder compatibility For AMP', 'accelerated-mobile-pages'),esc_html__( 'Click here for more info', 'accelerated-mobile-pages' )),               
            );
@@ -127,6 +136,28 @@ $avia_pb_for_ampchecker = array(
   if( class_exists('Vc_Manager') || ( class_exists('ET_Builder_Plugin') || 'Divi' == $theme->name || 'Divi' == $theme->parent_theme ) || did_action( 'elementor/loaded' ) || class_exists( 'FusionBuilder' )  ){
      if(class_exists('Vc_Manager') ){
          $pb_title =  'WPBakery Page Builder Support';
+    if (is_plugin_active('oxygen/functions.php')) {
+      $pb_for_amp[] =  array(
+                  'id' => 'ampforwp-oxygen-pb-for-amp-accor',
+                  'type' => 'section',
+                  'title' => esc_html__('Oxygen Compatibility', 'accelerated-mobile-pages'),
+                  'indent' => true,
+                  'layout_type' => 'accordion',
+                  'accordion-open'=> 1, 
+              );
+      $pb_for_amp[] = array(
+                 'id'       => 'ampforwp-oxygen-pb-for-amp',
+                 'type'     => 'switch',
+                 'title'    => esc_html__('Oxygen Support','accelerated-mobile-pages'),
+                 'tooltip-subtitle' => esc_html__('Enable or Disable the Oxygen support for AMP', 'accelerated-mobile-pages'),
+                 'default'  => false
+              ); 
+    }
+    $pb_for_amp[] = $oxygen_pb_for_ampchecker;
+    $pb_title = 'Page Builder';
+    $theme = wp_get_theme(); // gets the current theme
+      if( class_exists('ET_Builder_Plugin') || 'Divi' == $theme->name || 'Divi' == $theme->parent_theme ){
+        $pb_title =  'Divi Builder Support';
       }
      if( class_exists('ET_Builder_Plugin') || 'Divi' == $theme->name || 'Divi' == $theme->parent_theme ){
           $pb_title =  'Divi Builder Support';
