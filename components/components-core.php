7f922fc7e1f0fca48837bfb22eeb62c952ea9239
Syed Zabiuddin <32957211+Zabi09@users.noreply.github.com>
2021-02-18 19:39:42 +0530

    #4877 Added in 1.0.76.3

--- components/components-core.php
+++ components/components-core.php
@@ -366,13 +366,10 @@ function amp_header_core(){
      if( true == ampforwp_get_setting('amp-rtl-select-option') ){
     	$bodyClass .= ' rtl ';
     }
-    $lightbox = $right_click = '';
+    $lightbox = '';
     if( false == ampforwp_get_setting('ampforwp-amp-img-lightbox') ){
     	$lightbox = 'data-amp-auto-lightbox-disable';
     }
-    if( true == ampforwp_get_setting('ampforwp-right-click-disable') ){
-    	$right_click = 'oncontextmenu="return false"';
-    }	
 	?><!doctype html>
 	<html <?php echo esc_attr(ampforwp_amp_nonamp_convert('amp ')); ?><?php echo AMP_HTML_Utils::build_attributes_string( $thisTemplate->get( 'html_tag_attributes' ) ); ?>>
 		<head>
@@ -402,7 +399,7 @@ function amp_header_core(){
 			</style>
 			<?php do_action('ampforwp_before_head', $thisTemplate);  ?>
 		</head>
-		<body <?php echo sanitize_text_field($right_click); ?><?php echo esc_attr($lightbox); ?> <?php ampforwp_body_class($bodyClass); ?>>
+		<body <?php echo esc_attr($lightbox); ?> <?php ampforwp_body_class($bodyClass); ?>>
 		<?php do_action('amp_start', $thisTemplate); ?>
 		<?php do_action('ampforwp_admin_menu_bar_front'); ?>
 		<?php do_action('ampforwp_body_beginning', $thisTemplate);  
@@ -734,7 +731,23 @@ function ampforwp_backtotop_global_css(){?>
 	  .wp-pagenavi a.last , .amp-archive a.first {
 	      margin-left: 10px;
 	  }
-  <?php } ?>  
+  <?php } ?> 
+  <?php if (true == ampforwp_get_setting('ampforwp-right-click-disable')) {?>
+    	.*{
+		-moz-user-select:none;
+		-webkit-user-select:none;
+		cursor: default;
+		}
+		html{
+		-webkit-touch-callout: none;
+		-webkit-user-select: none;
+		-khtml-user-select: none;
+		-moz-user-select: none;
+		-ms-user-select: none;
+		user-select: none;
+		-webkit-tap-highlight-color: rgba(0,0,0,0);
+		}
+    <?php } ?> 
 	<?php if (class_exists('UAGB_Admin')) {?>
 	.wp-block-uagb-faq svg {display: none;}
 	<?php } ?>
