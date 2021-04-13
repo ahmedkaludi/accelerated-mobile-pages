a29ce20025233f54eaa078ff5d0e74ddb79d152f
Syed Zabiuddin <32957211+Zabi09@users.noreply.github.com>
2021-04-13 13:04:17 +0530

    #4979 Added in 1.0.76.11

--- components/breadcrumb/breadcrumb.php
+++ components/breadcrumb/breadcrumb.php
@@ -142,6 +142,7 @@ function amp_breadcrumb_output(){
                             $cat_link = ampforwp_url_controller( $cat_link );
                         }
                         $cat_link = apply_filters('ampforwp_breadcrumbs_category_url', $cat_link,$post->ID);
+                        $parents = apply_filters('ampforwp_breadcrumbs_category_name', $parents);
                         $cat_display .=  '<li class="item-cat item-cat-' . esc_attr($cat_id) . '"><a class="bread-cat bread-cat-' . esc_attr($cat_id) . ' bread-cat-' . $parents. '" href="'. esc_url($cat_link).'" title="' . esc_attr($parents) . '">' . esc_html($parents) . '</a></li>';  
                     }
                     if(ampforwp_get_setting('ampforwp-bread-crumb-post')){
