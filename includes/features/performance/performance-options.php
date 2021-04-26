5bac8aa61e1ca161ce148d9dd473a3dec4fe80c6
Syed Zabiuddin <32957211+Zabi09@users.noreply.github.com>
2021-04-26 17:53:56 +0530

    #4529 Added in 1.0.76.15

--- includes/features/performance/performance-options.php
+++ includes/features/performance/performance-options.php
@@ -11,11 +11,11 @@ if ( ! defined( 'ABSPATH' ) ) {
   $cache_AD_URL = "http://ampforwp.com/amp-cache/#utm_source=options-panel&utm_medium=performance-tab&utm_campaign=AMP%20Plugin";
   $cache_desc = '<a href="'.$cache_AD_URL.'"  target="_blank"><img class="ampforwp-ad-img-banner" src="'.AMPFORWP_IMAGE_DIR . '/amp_cache_b.png" width="560" height="85" /></a>';
   }
-  // PWA Section
+  // Performance SECTION
   Redux::setSection( $opt_name, array(
-        'title'      => esc_html__( 'PWA', 'accelerated-mobile-pages' ),
-        'id'         => 'pwa-for-wp',
-        'desc'      => '',
+        'title'      => esc_html__( 'Performance', 'accelerated-mobile-pages' ),
+        'id'         => 'amp-performance',
+        'desc' => $cache_desc,
         'subsection' => true,
         'fields'     => array(
             array(
@@ -28,29 +28,16 @@ if ( ! defined( 'ABSPATH' ) ) {
               ),
 
            array(
-               'id'       => 'ampforwp_pwa_module',
-               'type'     => 'raw',
-               'title'     => esc_html__('PWA Support', 'accelerated-mobile-pages'),
-               'content'  => (!is_plugin_active('pwa-for-wp/pwa-for-wp.php')? 
-                                '<div class="install-now ampforwp-activation-call-module-upgrade button  " id="ampforwp-pwa-activation-call" data-secure="'.wp_create_nonce('verify_module').'">Activate this Module</div>'
-                            : '<div class="col-wrapper">
-                                   <a href="'.admin_url('admin.php?page=pwaforwp&reference=ampforwp').'"> <div class="ampforwp-recommendation-btn updated-message"><p>Go to PWA Settings</p></div> </a> 
-                                </div>
-                            ').'<a class="amp_recommend_learnmore" href="https://ampforwp.com/tutorials/article/what-is-pwa-for-wp-and-why-its-included-in-the-amp/" target="_blank">Learn more</a>'
+                 'id'       => 'ampforwp_pwa_module',
+                 'type'     => 'raw',
+                 'title'     => esc_html__('PWA Support', 'accelerated-mobile-pages'),
+                 'content'  => (!is_plugin_active('pwa-for-wp/pwa-for-wp.php')? 
+                                  '<div class="install-now ampforwp-activation-call-module-upgrade button  " id="ampforwp-pwa-activation-call" data-secure="'.wp_create_nonce('verify_module').'">Activate this Module</div>'
+                              : '<div class="col-wrapper">
+                                     <a href="'.admin_url('admin.php?page=pwaforwp&reference=ampforwp').'"> <div class="ampforwp-recommendation-btn updated-message"><p>Go to PWA Settings</p></div> </a> 
+                                  </div>
+                              ').'<a class="amp_recommend_learnmore" href="https://ampforwp.com/tutorials/article/what-is-pwa-for-wp-and-why-its-included-in-the-amp/" target="_blank">Learn more</a>'
            ),
-
-       )
-
-  )
-
-  );
-  // Performance SECTION
-  Redux::setSection( $opt_name, array(
-        'title'      => __( 'Performance', 'accelerated-mobile-pages' ),
-        'id'         => 'amp-performance',
-        'desc' => $cache_desc,
-        'subsection' => true,
-        'fields'     => array(
             array(
                   'id' => 'ampforwp-performance',
                   'type' => 'section',
