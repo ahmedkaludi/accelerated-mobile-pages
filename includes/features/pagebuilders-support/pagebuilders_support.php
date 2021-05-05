c17150e5652644ec0d4a2d9a1d45f838d85c757f
mali <merajali294@gmail.com>
2021-05-05 15:28:34 +0530

    #5009
    #5009 , array added for Beaver builder support

--- includes/features/pagebuilders-support/pagebuilders_support.php
+++ includes/features/pagebuilders-support/pagebuilders_support.php
@@ -4,7 +4,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 function ampforwp_page_builders_support_options($opt_name){
-$divi_pb_for_ampchecker = $elemntr_pb_for_ampchecker = $avada_pb_for_ampchecker = $avia_pb_for_ampchecker = $oxygen_pb_for_ampchecker = $ux_pb_for_ampchecker = array();
+$divi_pb_for_ampchecker = $elemntr_pb_for_ampchecker = $avada_pb_for_ampchecker = $avia_pb_for_ampchecker = $oxygen_pb_for_ampchecker = $ux_pb_for_ampchecker = $beaver_builder_pb_for_ampchecker = array();
 if(!function_exists('amp_pagebuilder_compatibility_init')){
 $divi_pb_for_ampchecker = array( 
                 'id'   => 'divi_pb_for_amp_info_normal',
@@ -55,6 +55,16 @@ $avia_pb_for_ampchecker = array(
                     ),
                  'desc' => sprintf('<div style="background: #FFF9C4;padding: 12px;line-height: 1.6;margin: -45px -14px -18px -17px;"><b>%s</b> %s <a href="https://ampforwp.com/page-builder-compatibility-for-amp/" target="_blank">%s</a> extension.<br /> <div style="margin-top:4px;">(<a href="https://ampforwp.com/page-builder-compatibility-for-amp/" target="_blank">%s</a>)</div></div>',esc_html__( 'ONE LAST STEP REQUIRED:','accelerated-mobile-pages'),esc_html__( 'This feature requires', 'accelerated-mobile-pages' ),esc_html__( 'Page Builder compatibility For AMP', 'accelerated-mobile-pages'),esc_html__( 'Click here for more info', 'accelerated-mobile-pages' )),               
             );
+
+            $beaver_builder_pb_for_ampchecker = array( 
+                'id'   => 'beaver_b_pb_for_amp_info_normal',
+                'type' => 'info',
+                'required' => array(
+                    array('ampforwp-beaver-pb-for-amp', '=' , true),  
+                    ),
+                 'desc' => sprintf('<div style="background: #FFF9C4;padding: 12px;line-height: 1.6;margin: -45px -14px -18px -17px;"><b>%s</b> %s <a href="https://ampforwp.com/page-builder-compatibility-for-amp/" target="_blank">%s</a> extension.<br /> <div style="margin-top:4px;">(<a href="https://ampforwp.com/page-builder-compatibility-for-amp/" target="_blank">%s</a>)</div></div>',esc_html__( 'ONE LAST STEP REQUIRED:','accelerated-mobile-pages'),esc_html__( 'This feature requires', 'accelerated-mobile-pages' ),esc_html__( 'Page Builder compatibility For AMP', 'accelerated-mobile-pages'),esc_html__( 'Click here for more info', 'accelerated-mobile-pages' )),               
+           );
+
 }
   if( !function_exists('amp_activate') ){
      $pb_for_amp[] =  array(
@@ -158,6 +168,26 @@ $avia_pb_for_ampchecker = array(
     }
     
     $pb_for_amp[] = $ux_pb_for_ampchecker;
+
+    if (is_plugin_active('beaver-builder-lite-version/fl-builder.php')) {
+      $pb_for_amp[] =  array(
+                  'id' => 'ampforwp-beaver-pb-for-amp-accor',
+                  'type' => 'section',
+                  'title' => esc_html__('Beaver Compatibility', 'accelerated-mobile-pages'),
+                  'indent' => true,
+                  'layout_type' => 'accordion',
+                  'accordion-open'=> 1, 
+              );
+      $pb_for_amp[] = array(
+                 'id'       => 'ampforwp-beaver-pb-for-amp',
+                 'type'     => 'switch',
+                 'title'    => esc_html__('Beaver Builder Support','accelerated-mobile-pages'),
+                 'tooltip-subtitle' => esc_html__('Enable or Disable the Beaver support for AMP', 'accelerated-mobile-pages'),
+                 'default'  => false
+              ); 
+    }
+    $pb_for_amp[] = $beaver_builder_pb_for_ampchecker;
+
     
   $pb_title = 'Page Builder';
   $theme = wp_get_theme(); // gets the current theme
