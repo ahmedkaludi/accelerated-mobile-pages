c200398eacb7cc2761d312f2c2d9404fa312d906
Syed Zabiuddin <32957211+Zabi09@users.noreply.github.com>
2021-06-24 14:24:03 +0530

    #5070 properly added amp animation

--- components/components-core.php
+++ components/components-core.php
@@ -476,46 +476,7 @@ function amp_back_to_top_link(){
     if(true == ampforwp_get_setting('ampforwp-footer-top')){?>
         <a id="scrollToTopButton" title="back to top" on="tap:backtotop.scrollTo(duration=500)" class="btt" href="#" ></a> 
         <?php 
-        global $wp;
-        $current_url = home_url(add_query_arg(array($_GET), $wp->request));
-        if(ampforwp_get_setting('ampforwp-amp-convert-to-wp')==false || (strpos( $current_url,'/amp')!=false || strpos( $current_url,'?amp')!=false)){?>
-        <amp-animation id="showAnim"
-		  layout="nodisplay">
-		  <script type="application/json">
-		    {
-		      "duration": "400ms",
-		      "fill": "both",
-		      "iterations": "1",
-		      "direction": "alternate",
-		      "animations": [{
-		        "selector": "#scrollToTopButton",
-		        "keyframes": [{
-		          "opacity": "1",
-		          "visibility": "visible"
-		        }]
-		      }]
-		    }
-		  </script>
-		</amp-animation>
-		<amp-animation id="hideAnim"
-		  layout="nodisplay">
-		  <script type="application/json">
-		    {
-		      "duration": "400ms",
-		      "fill": "both",
-		      "iterations": "1",
-		      "direction": "alternate",
-		      "animations": [{
-		        "selector": "#scrollToTopButton",
-		        "keyframes": [{
-		          "opacity": "0",
-		          "visibility": "hidden"
-		        }]
-		      }]
-		    }
-		  </script>
-		</amp-animation>
-	<?php }else if(ampforwp_get_setting('ampforwp-amp-convert-to-wp')==true){?>
+        if(ampforwp_get_setting('ampforwp-amp-convert-to-wp')==true){?>
       	<script>
       		var elem = document.getElementById('scrollToTopButton');
       		elem.addEventListener("click", function(){
@@ -896,6 +857,9 @@ function ampforwp_backtotop_global_css(){?>
     transform: rotate(180deg);
     text-align: center;
     line-height: 1.5;
+  }
+  html {
+    scroll-behavior: smooth;
   }
 <?php } }
 
