e419528ec09663480e0bf6f8ec8d3c7a69aa6069
mali <merajali294@gmail.com>
2021-10-21 14:59:56 +0530

    #5151
    Added support to show license information in panel

--- includes/options/redux-core/framework.php
+++ includes/options/redux-core/framework.php
@@ -3235,6 +3235,7 @@
                     $amp_opt = get_option("ampforwp_option_panel_view_type");
                     $opt_visible = "";
                     $opt_visible_class = "amp-full-view-options";
+                    $anchor_href = 'javascript:void(0);';
                     if(($amp_opt==1 || $amp_opt=="") && !get_theme_support('amp-template-mode')){
                         $opt_visible = 'style=display:none';
                     }
@@ -3242,8 +3243,11 @@
                          $opt_visible = '';
                          $opt_visible_class = "";
                     }
+                    if($section['title']=="Extensions"){
+                         $anchor_href = class_exists('AMPExtensionManager') ?admin_url( 'admin.php?page=amp-extension-manager' ) : 'javascript:void(0);';                         
+                    }
                     $string .= '<li '.esc_html($opt_visible).' id="' . esc_attr( $k . $suffix ) . '_section_group_li" class="'.esc_attr($opt_visible_class).' redux-group-tab-link-li '.esc_attr($addClass).'' . esc_attr( $hide_section ) . esc_attr( $section['class'] ) . esc_attr( $subsectionsClass ) . ' ' . strtolower( wp_kses_post( $section['id'] )) . '" '.ampforwp_css_sanitizer($style).'>';
-                    $string .= '<a href="javascript:void(0);" id="' . esc_attr( $k . $suffix ) . '_section_group_li_a" class="redux-group-tab-link-a" data-key="' . esc_attr( $k ) . '" data-rel="' . esc_attr( $k . $suffix ) . '">' . $extra_icon . $icon . '<span class="group_title">' . wp_kses_post( $section['title'] ) . '</span></a>';
+                    $string .= '<a href="'.$anchor_href.'" id="' . esc_attr( $k . $suffix ) . '_section_group_li_a" class="redux-group-tab-link-a" data-key="' . esc_attr( $k ) . '" data-rel="' . esc_attr( $k . $suffix ) . '">' . $extra_icon . $icon . '<span class="group_title">' . wp_kses_post( $section['title'] ) . '</span></a>';
 
                     $nextK = $k;
 
