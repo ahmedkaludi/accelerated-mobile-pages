d8763e1b60da4b3822eea3e4aeecd1ff3f9e4793
Syed Zabiuddin <32957211+Zabi09@users.noreply.github.com>
2022-02-28 15:24:08 +0530

    #5223 escaping added

--- includes/deactivate-feedback.php
+++ includes/deactivate-feedback.php
@@ -1,15 +1,15 @@
 <?php 
 $reasons = array(
-    		1 => '<li><label><input type="radio" name="ampforwp_disable_reason" value="temporary"/>' . __('It is only temporary', 'ampforwp-for-wp') . '</label></li>',
-		2 => '<li><label><input type="radio" name="ampforwp_disable_reason" value="stopped showing ads"/>' . __('I stopped showing ads on my site', 'ampforwp-for-wp') . '</label></li>',
-		3 => '<li><label><input type="radio" name="ampforwp_disable_reason" value="missing feature"/>' . __('I miss a feature', 'ampforwp-for-wp') . '</label></li>
+    	1 => '<li><label><input type="radio" name="ampforwp_disable_reason" value="temporary"/>' . esc_html__('It is only temporary', 'accelerated-mobile-pages') . '</label></li>',
+		2 => '<li><label><input type="radio" name="ampforwp_disable_reason" value="stopped showing ads"/>' . esc_html__('I stopped showing ads on my site', 'accelerated-mobile-pages') . '</label></li>',
+		3 => '<li><label><input type="radio" name="ampforwp_disable_reason" value="missing feature"/>' . esc_html__('I miss a feature', 'accelerated-mobile-pages') . '</label></li>
 		<li><input type="text" name="ampforwp_disable_text[]" value="" placeholder="Please describe the feature"/></li>',
-		4 => '<li><label><input type="radio" name="ampforwp_disable_reason" value="technical issue"/>' . __('Technical Issue', 'ampforwp-for-wp') . '</label></li>
-		<li><textarea name="ampforwp_disable_text[]" placeholder="' . __('Can we help? Please describe your problem', 'ampforwp-for-wp') . '"></textarea></li>',
-		5 => '<li><label><input type="radio" name="ampforwp_disable_reason" value="other plugin"/>' . __('I switched to another plugin', 'ampforwp-for-wp') .  '</label></li>
+		4 => '<li><label><input type="radio" name="ampforwp_disable_reason" value="technical issue"/>' . esc_html__('Technical Issue', 'accelerated-mobile-pages') . '</label></li>
+		<li><textarea name="ampforwp_disable_text[]" placeholder="' . esc_html__('Can we help? Please describe your problem', 'accelerated-mobile-pages') . '"></textarea></li>',
+		5 => '<li><label><input type="radio" name="ampforwp_disable_reason" value="other plugin"/>' . esc_html__('I switched to another plugin', 'accelerated-mobile-pages') .  '</label></li>
 		<li><input type="text" name="ampforwp_disable_text[]" value="" placeholder="Name of the plugin"/></li>',
-		6 => '<li><label><input type="radio" name="ampforwp_disable_reason" value="other"/>' . __('Other reason', 'ampforwp-for-wp') . '</label></li>
-		<li><textarea name="ampforwp_disable_text[]" placeholder="' . __('Please specify, if possible', 'ampforwp-for-wp') . '"></textarea></li>',
+		6 => '<li><label><input type="radio" name="ampforwp_disable_reason" value="other"/>' . esc_html__('Other reason', 'accelerated-mobile-pages') . '</label></li>
+		<li><textarea name="ampforwp_disable_text[]" placeholder="' . esc_html__('Please specify, if possible', 'accelerated-mobile-pages') . '"></textarea></li>',
     );
 shuffle($reasons);
 ?>
@@ -18,7 +18,7 @@ shuffle($reasons);
 <div id="ampforwp-reloaded-feedback-overlay" style="display: none;">
     <div id="ampforwp-reloaded-feedback-content">
 	<form action="" method="post">
-	    <h3><strong><?php _e('If you have a moment, please let us know why you are deactivating:', 'ampforwp-for-wp'); ?></strong></h3>
+	    <h3><strong><?php echo esc_html__('If you have a moment, please let us know why you are deactivating:', 'accelerated-mobile-pages'); ?></strong></h3>
 	    <ul>
                 <?php 
                 foreach ($reasons as $reason){
@@ -29,9 +29,9 @@ shuffle($reasons);
 	    <?php if ($email) : ?>
     	    <input type="hidden" name="ampforwp_disable_from" value="<?php echo $email; ?>"/>
 	    <?php endif; ?>
-	    <input id="ampforwp-reloaded-feedback-submit" class="button button-primary" type="submit" name="ampforwp_disable_submit" value="<?php _e('Submit & Deactivate', 'ampforwp-for-wp'); ?>"/>
-	    <a class="button"><?php _e('Only Deactivate', 'ampforwp-for-wp'); ?></a>
-	    <a class="ampforwp-for-wp-feedback-not-deactivate" href="#"><?php _e('Don\'t deactivate', 'ampforwp-for-wp'); ?></a>
+	    <input id="ampforwp-reloaded-feedback-submit" class="button button-primary" type="submit" name="ampforwp_disable_submit" value="<?php echo esc_html__('Submit & Deactivate', 'accelerated-mobile-pages'); ?>"/>
+	    <a class="button"><?php echo esc_html__('Only Deactivate', 'accelerated-mobile-pages'); ?></a>
+	    <a class="ampforwp-for-wp-feedback-not-deactivate" href="#"><?php echo esc_html__('Don\'t deactivate', 'accelerated-mobile-pages'); ?></a>
 	</form>
     </div>
 </div>
